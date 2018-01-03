8fe63391696db2613783848657dfe9ef440950b1
Jeremy Lewi <jeremy@lewi.us>
2018-01-03 13:33:03 -0800

    Create a binary to launch an Argo workflow to run our E2E tests.

--- testing/run_e2e_workflow.py
+++ testing/run_e2e_workflow.py
@@ -4,9 +4,16 @@ This script submits an Argo workflow to run the E2E tests and waits for
 it to finish. It is intended to invoked by prow jobs.
 """
 
+import argparse
 import logging
+from kubernetes import client as k8s_client
 import os
+import tempfile
+from testing import argo_client
 from py import util
+
+# The namespace used by the workflow ksonnet component.
+NAMESPACE = "kubeflow-testing"
 
 def _get_src_dir():
   return os.path.abspath(os.path.join(__file__, "..",))
@@ -16,30 +23,54 @@ def run(args):
   logging.info("Source directory: %s", src_dir)
   app_dir = os.path.join(src_dir, "testing", "test-infra")
 
+  util.configure_kubectl(args.project, args.zone, args.cluster)
+  util.load_kube_config()
+
   # Create the name for the workflow
-  worflow_name = os.getenv("JOB_NAME")
+  workflow_name = os.getenv("JOB_NAME")
   job_type = os.getenv("JOB_TYPE")
   if job_type == "presubmit":
-    worflow_name += "-{0}".format(os.getenv("PULL_NUMBER"))
-  elif job_type == "postsubmit"):
-    worflow_name += "-{0}".format(os.getenv("PULL_BASE_SHA"))
+    workflow_name += "-{0}".format(os.getenv("PULL_NUMBER"))
+  elif job_type == "postsubmit":
+    workflow_name += "-{0}".format(os.getenv("PULL_BASE_SHA"))
 
   workflow_name += "-{0}".format(os.getenv("BUILD_NUMBER"))
 
   util.run(["ks", "param", "set", "name", workflow_name], cwd=app_dir)
+  util.load_kube_config()
+
+  api_client = k8s_client.ApiClient()
 
   # Set the prow environment variables.
   prow_env = []
-  for v in ["JOB_NAME", "JOB_TYPE", "BUILD_ID", "BUILD_NUMBER",
-            "PULL_BASE_SHA", "PULL_NUMBER", "PULL_PULL_SHA", "REPO_OWNER",
-            "REPO_NAME"]:
+
+  names = ["JOB_NAME", "JOB_TYPE", "BUILD_ID", "BUILD_NUMBER",
+           "PULL_BASE_SHA", "PULL_NUMBER", "PULL_PULL_SHA", "REPO_OWNER",
+           "REPO_NAME"]
+  names.sort()
+  for v in names:
     if not os.getenv(v):
       continue
     prow_env.append("{0}={1}".format(v, os.getenv(v)))
 
-  util.run(["ks", "param", "set", "prow_env", prow_env], cwd=app_dir)
+  util.run(["ks", "param", "set", "prow_env", ",".join(prow_env)], cwd=app_dir)
+
+  # TODO(jlewi): Should we set the namespace using ks param set so that it
+  # always matches the namespace used by run_e2e_workflow.py?
 
   util.run(["ks", "apply", "prow", "-c", "workflows"], cwd=app_dir)
+
+  success = False
+  try:
+    results = argo_client.wait_for_workflow(api_client, NAMESPACE, workflow_name)
+    if results["status"]["phase"] == "Succeeded":
+      success = True
+  except util.TimeoutError:
+    success = False
+  finally:
+    # TODO(jlewi): upload build log to Gubernator and create finished.json
+    # in gubernator. Currently we create finished.json in the workflow.
+    pass
 
 def main(unparsed_args=None):  # pylint: disable=too-many-locals
   logging.getLogger().setLevel(logging.INFO) # pylint: disable=too-many-locals
@@ -75,11 +106,8 @@ def main(unparsed_args=None):  # pylint: disable=too-many-locals
   # to gubernator.
   root_logger = logging.getLogger()
 
-  test_log = os.path.join(os.path.join(args.artifacts_dir, "artifacts"),
-                          "logs", "prow_artifacts." + args.func.__name__ +
-                          ".log")
-  if not os.path.exists(os.path.dirname(test_log)):
-    os.makedirs(os.path.dirname(test_log))
+  with tempfile.NamedTemporaryFile(prefix="tmpRunE2eWorkflow", suffix="log") as hf:
+    test_log = hf.name
 
   file_handler = logging.FileHandler(test_log)
   root_logger.addHandler(file_handler)
---
+++ testing/run_e2e_workflow_test.py
@@ -0,0 +1,49 @@
+import json
+import os
+import unittest
+import mock
+from testing import run_e2e_workflow
+import tempfile
+
+from google.cloud import storage  # pylint: disable=no-name-in-module
+
+class TestRunE2eWorkflow(unittest.TestCase):
+  @mock.patch("testing.run_e2e_workflow.argo_client.wait_for_workflow")
+  @mock.patch("testing.run_e2e_workflow.util.load_kube_config")
+  @mock.patch("testing.run_e2e_workflow.util.configure_kubectl")
+  @mock.patch("testing.run_e2e_workflow.util.run")
+  def testMainPresubmit(self, mock_run, mock_configure, _, mock_wait):  # pylint: disable=no-self-use
+    """Test create started for presubmit job."""
+
+    os.environ["REPO_OWNER"] = "fake_org"
+    os.environ["REPO_NAME"] = "fake_name"
+    os.environ["PULL_NUMBER"] = "77"
+    os.environ["PULL_PULL_SHA"] = "123abc"
+    os.environ["JOB_NAME"] = "kubeflow-presubmit"
+    os.environ["JOB_TYPE"] = "presubmit"
+    os.environ["BUILD_NUMBER"] = "1234"
+
+    args = ["--project=some-project", "--cluster=some-cluster",
+            "--zone=us-east1-d"]
+    run_e2e_workflow.main(args)
+
+    mock_configure.assert_called_once_with("some-project", "us-east1-d",
+                                           "some-cluster",)
+    self.assertItemsEqual(
+      ["ks", "param", "set", "name", "kubeflow-presubmit-77-1234"],
+      mock_run.call_args_list[0][0][0])
+
+    self.assertItemsEqual(
+      ["ks", "param", "set", "prow_env",
+       "BUILD_NUMBER=1234,JOB_NAME=kubeflow-presubmit,JOB_TYPE=presubmit"
+       ",PULL_NUMBER=77,PULL_PULL_SHA=123abc,REPO_NAME=fake_name"
+       ",REPO_OWNER=fake_org"],
+      mock_run.call_args_list[1][0][0])
+
+    self.assertItemsEqual(
+      ["ks", "apply", "prow", "-c", "workflows"],
+      mock_run.call_args_list[2][0][0])
+
+
+if __name__ == "__main__":
+  unittest.main()
