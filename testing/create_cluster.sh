ec09e4117d48fc97d99cc658baa51a545485aa4f
Jeremy Lewi <jeremy@lewi.us>
2018-01-03 10:57:13 -0800

    Remove create_cluster.sh

--- testing/create_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Create the GKE cluster used for testing Kubeflow deployments.
-# This should be a one-time thing because we reuse the same cluster for all tests.
-gcloud --project=mlkube-testing container clusters create \
-	--zone=us-east1-d  \
-	--machine-type=n1-standard-8 \
-	--cluster-version=1.8.4-gke.1 \
-	kubeflow-testing
