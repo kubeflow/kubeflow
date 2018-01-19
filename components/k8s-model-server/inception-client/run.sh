cae312e3ab9887e29a70ec6dcfbcdbf759c2509a
Neeraj Kashyap <nkashy1@gmail.com>
2018-01-19 15:21:42 -0800

    Updated run.sh so that local and k8s runs work
    $SERVER and $PORT are read from command line but, if unspecified, default to k8s environment variables.

--- components/k8s-model-server/inception-client/run.sh
+++ components/k8s-model-server/inception-client/run.sh
@@ -14,14 +14,15 @@
 
 #!/bin/bash
 
-SERVER=$INCEPTION_SERVICE_HOST
-PORT=$INCEPTION_SERVICE_PORT
+SERVER=$1
+PORT=$2
 
 if [ -z $SERVER ] ; then
-  echo "run.sh Error: Please provide server address as first parameter"
-  exit 1
+  SERVER=$INCEPTION_SERVICE_HOST
 fi
 
-if [ -z $PORT ] ; then PORT=9000 ; fi
+if [ -z $PORT ] ; then
+  PORT=$INCEPTION_SERVICE_PORT
+fi
 
 python label.py -s $SERVER -p $PORT /data/*.jpg
