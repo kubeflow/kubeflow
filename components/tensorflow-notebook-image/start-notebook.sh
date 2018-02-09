d5caf230ff50260c1a6565db35edeeddd5d407e6
Hitoshi Mitake <mitake.hitoshi@gmail.com>
2018-02-09 13:34:23 +0900

    add correct boilerplates (#204)

--- components/tensorflow-notebook-image/start-notebook.sh
+++ components/tensorflow-notebook-image/start-notebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2017 Google Inc. All rights reserved.
+# Copyright 2017 The Kubeflow Authors All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
