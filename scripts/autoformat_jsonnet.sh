7bdbc560198868acdb46d5d85bb0277b4b0c9159
Ankush Agarwal <agwl@google.com>
2018-02-23 08:50:01 -0800

    Update jsonnet autoformat script
    Enforce consistent string quote styling
    and comment styling

--- scripts/autoformat_jsonnet.sh
+++ scripts/autoformat_jsonnet.sh
@@ -17,5 +17,12 @@
 # Script to autoformat libsonnet files.
 # Assumes jsonnet is on the path.
 set -ex
-find ./ -name "*.libsonnet" -exec jsonnet fmt {} -i --indent 2 ";"
-find ./ -name "*.jsonnet" -exec jsonnet fmt {} -i --indent 2 ";"
+
+# 2 spaces vertical indentation
+# Use double quotes for strings
+# Use // for comments
+find -E $(pwd) -iregex ".*\.(libsonnet|jsonnet)$" -exec \
+  jsonnet fmt {} -i \
+  --string-style d \
+  --comment-style s \
+  --indent 2 ";"
