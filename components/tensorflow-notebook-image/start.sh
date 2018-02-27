e4ba669ef04b41f9b82e52f6d3adc0f6dcd0e0a7
Michal (inc0) Jastrzebski <inc007@gmail.com>
2018-02-27 15:04:32 -0800

    Fix #285 and make few tweaks to notebook Dockerfile
    Besides adding chown /home/$USER also move non-install layers lower in
    Dockerfile to make better use of docker cache during edits

--- components/tensorflow-notebook-image/start.sh
+++ components/tensorflow-notebook-image/start.sh
@@ -24,14 +24,11 @@ if [ $(id -u) == 0 ] ; then
     if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
         echo "Set user UID to: $NB_UID"
         usermod -u $NB_UID $NB_USER
-        # Careful: $HOME might resolve to /root depending on how the
-        # container is started. Use the $NB_USER home path explicitly.
-        for d in "$CONDA_DIR" "$JULIA_PKGDIR" "/home/$NB_USER"; do
-            if [[ ! -z "$d" && -d "$d" ]]; then
-                echo "Set ownership to uid $NB_UID: $d"
-                chown -R $NB_UID "$d"
-            fi
-        done
+    fi
+
+    # Ensure pv directory belongs to user
+    if [ -d "/home/$NB_USER" ]; then
+        chown -R $NB_USER /home/$NB_USER
     fi
 
     # Change GID of NB_USER to NB_GID if NB_GID is passed as a parameter
