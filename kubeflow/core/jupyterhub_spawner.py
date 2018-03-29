a6bb940752cbc1e94385c3e876f12ac00af7d112
Ankush Agarwal <agwl@google.com>
2018-03-29 04:44:03 -0700

    Fix typos in tensorflow notebook images in spawner (#526)

--- kubeflow/core/jupyterhub_spawner.py
+++ kubeflow/core/jupyterhub_spawner.py
@@ -12,12 +12,12 @@ class KubeFormSpawner(KubeSpawner):
     <label for='image'>Image</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     <input list="image" name="image" placeholder='repo/image:tag'>
     <datalist id="image">
-      <option value="gcr.io/kubeflow-images-staging/tensorflow-1-4.1-notebook-cpu:v20180327-6bb4058">
-      <option value="gcr.io/kubeflow-images-staging/tensorflow-1-4.1-notebook-gpu:v20180327-6bb4058">
-      <option value="gcr.io/kubeflow-images-staging/tensorflow-1-5.1-notebook-cpu:v20180327-6bb4058">
-      <option value="gcr.io/kubeflow-images-staging/tensorflow-1-5.1-notebook-gpu:v20180327-6bb4058">
-      <option value="gcr.io/kubeflow-images-staging/tensorflow-1-6.1-notebook-cpu:v20180327-6bb4058">
-      <option value="gcr.io/kubeflow-images-staging/tensorflow-1-6.1-notebook-gpu:v20180327-6bb4058">
+      <option value="gcr.io/kubeflow-images-staging/tensorflow-1.4.1-notebook-cpu:v20180327-6bb4058">
+      <option value="gcr.io/kubeflow-images-staging/tensorflow-1.4.1-notebook-gpu:v20180327-6bb4058">
+      <option value="gcr.io/kubeflow-images-staging/tensorflow-1.5.1-notebook-cpu:v20180327-6bb4058">
+      <option value="gcr.io/kubeflow-images-staging/tensorflow-1.5.1-notebook-gpu:v20180327-6bb4058">
+      <option value="gcr.io/kubeflow-images-staging/tensorflow-1.6.0-notebook-cpu:v20180327-6bb4058">
+      <option value="gcr.io/kubeflow-images-staging/tensorflow-1.6.0-notebook-gpu:v20180327-6bb4058">
     </datalist>
     <br/><br/>
 
@@ -117,4 +117,3 @@ if pvc_mount:
         'name': 'volume-{username}{servername}'
       }
     ]
-
