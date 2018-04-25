02b7d5a0137fe60b56dc56d8b91650d9841394a0
Michal (inc0) Jastrzebski <inc007@gmail.com>
2018-04-25 16:25:20 +0200

    move app to pvc

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -9,6 +9,7 @@ set -x
 
 # If it's run by deployment job, don't switch users
 if [ -v "DEPLOY_JOB" ]; then
+    cd /kubeflow
     # Remove fake kubeconfig and start k8s proxy
     rm ~/.kube/config
     kubectl proxy --port=8111 &
@@ -16,10 +17,13 @@ if [ -v "DEPLOY_JOB" ]; then
     # Recreate env since we have proper k8s creds
     ks env rm default
     ks env add default --server=http://127.0.0.1:8111
-
     ks env set default --namespace ${NAMESPACE}
-    ks generate kubeflow-core kubeflow-core
 
+    if [ ! -d /opt/kubeflow/app/kubeflow ]; then
+        ks generate kubeflow-core kubeflow-core
+        cp -R /kubeflow /opt/kubeflow/app
+    fi
+    cd /opt/kubeflow/app/kubeflow
     sleep 1000000000
 fi
 groupadd -g ${GROUP_ID} ${GROUP}
