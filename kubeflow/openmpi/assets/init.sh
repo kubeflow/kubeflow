1813a70604beba0bd8dc1c7513d03bd4480760d6
Jie Zhang <jiez@uber.com>
2018-05-04 11:33:58 -0700

    [openmpi] Wait for nvidia driver to be installed before running MPI job
    For auto-scaling GPU node pool, the nvidia driver will be installed by [nvidia-driver-installer](https://cloud.google.com/kubernetes-engine/docs/concepts/gpus#installing_drivers).
    The MPI job should be kicked off after the driver is present in the container.

--- kubeflow/openmpi/assets/init.sh
+++ kubeflow/openmpi/assets/init.sh
@@ -2,9 +2,11 @@ set -exv
 
 OPENMPI_DIR=/kubeflow/openmpi
 BACKOFF_SECS=10
+TIMEOUT_EXIT_CODE=124
 
 wait_mpi_ready() {
-  local max_retries=$1
+  local workers=$1
+  local max_retries=$2
   local retries=0
 
   until mpiexec -n ${workers} --hostfile ${OPENMPI_DIR}/assets/hostfile --allow-run-as-root -q sh -c 'echo $(hostname) is ready'; do
@@ -12,14 +14,23 @@ wait_mpi_ready() {
 
     retries=$(expr ${retries} + 1)
     if [ -n "${max_retries}" ] && [ ${retries} -ge ${max_retries} ]; then
-      exit 124
+      exit ${TIMEOUT_EXIT_CODE}
     fi
   done
 }
 
-wait_controller_term() {
-  until [ -f ${OPENMPI_DIR}/data/.openmpi-controller/term.sig ]; do
+wait_controller_signal() {
+  local signal=$1
+  local max_retries=$2
+  local retries=0
+
+  until [ -f ${OPENMPI_DIR}/data/.openmpi-controller/${signal} ]; do
     sleep ${BACKOFF_SECS}
+
+    retries=$(expr ${retries} + 1)
+    if [ -n "${max_retries}" ] && [ ${retries} -ge ${max_retries} ]; then
+      exit ${TIMEOUT_EXIT_CODE}
+    fi
   done
 }
 
@@ -32,7 +43,7 @@ role="$1"
 workers="$2"
 exec="$3"
 timeout_secs="$4"
-retries=$(expr ${timeout_secs} / ${BACKOFF_SECS})
+max_retries=$(expr ${timeout_secs} / ${BACKOFF_SECS})
 
 # Set up openmpi
 mkdir -p /root/.openmpi
@@ -45,16 +56,22 @@ cp ${OPENMPI_DIR}/secrets/id_rsa.pub /root/.ssh
 cp ${OPENMPI_DIR}/secrets/authorized_keys /root/.ssh
 cp ${OPENMPI_DIR}/assets/ssh_config /root/.ssh/config
 
-# Start sshd in daemon mode
-/usr/sbin/sshd -e -f ${OPENMPI_DIR}/assets/sshd_config
-
 exit_code=0
 if [ "${role}" = "master" ]; then
+  # Wait until workers are ready.
+  wait_mpi_ready ${workers} ${max_retries}
+
   # Run the exec command in master
-  wait_mpi_ready ${retries}
   sh -c "${exec}" || exit_code=$?
 else
-  wait_controller_term
+  # Wait until controller finishes initialization.
+  wait_controller_signal SIGCONT ${max_retries}
+
+  # Start sshd in daemon mode
+  /usr/sbin/sshd -e -f ${OPENMPI_DIR}/assets/sshd_config
+
+  # Block forever until controller terminates.
+  wait_controller_signal SIGTERM
 fi
 
 exit ${exit_code}
