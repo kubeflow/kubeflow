39d8940043d2c6ab01b2a12c1d09a4bb7c75e9e3
Lun-Kai Hsu <lunkai@google.com>
2018-05-09 18:01:51 -0700

    address comments

--- components/build_image.py
+++ components/build_image.py
@@ -1,5 +1,11 @@
+"""Script to build images.
+
+For example,
+python build_image.py tf_serving --tf_version=1.6 --platform=gpu
+"""
 import argparse
 import datetime
+from itertools import chain
 import logging
 import os
 import subprocess
@@ -11,6 +17,7 @@ import yaml
 def run(command, cwd=None, env=None, polling_interval=datetime.timedelta(seconds=1)):
   """Run a subprocess.
   Copied from kubeflow/test so it's easier to run locally.
+  TODO(lunkai): refactor to dedup.
   Any subprocess output is emitted through the logging modules.
   Returns:
     output: A string containing the output.
@@ -34,7 +41,7 @@ def run(command, cwd=None, env=None, polling_interval=datetime.timedelta(seconds
   output = []
   while process.poll() is None:
     process.stdout.flush()
-    for line in iter(process.stdout.readline, ''):
+    for line in iter(process.stdout.readline, b''):
       output.append(line.strip())
       logging.info(line.strip())
 
@@ -52,6 +59,17 @@ def run(command, cwd=None, env=None, polling_interval=datetime.timedelta(seconds
 
   return "\n".join(output)
 
+def get_build_args(config):
+  """
+  Make the list of params for docker build from config.
+
+  For example, if the config is {"a": 1, "b": 2}
+  This should return
+  ["--build-arg", "a=1", "--build-arg", "b=2"]
+  """
+  config_list = [key + "=" + val for key, val in config.items()]
+  return list(chain.from_iterable([["--build-arg", x] for x in config_list]))
+
 def build_tf_serving(args):
   context_dir = "k8s-model-server/images"
   version = args.tf_version if args.platform == "cpu" else args.tf_version + "gpu"
@@ -59,21 +77,15 @@ def build_tf_serving(args):
   config_file = os.path.join(context_dir, "versions", version, "version-config.json")
   with open(config_file) as f:
     config = yaml.load(f)
-  if args.platform == "cpu":
-    command = [
-        "docker", "build", "--pull",
-        "--build-arg",  "TF_SERVING_PACKAGE={}".format(config['package']),
-        "-t", "{}/tensorflow-serving-{}:{}".format(args.registry, version, args.tag),
-        "-f", "Dockerfile.cpu", "."]
-  else:  # gpu
-    command = [
-        "docker", "build", "--pull",
-        "--build-arg", "BASE_IMAGE={}".format(config['base_image']),
-        "--build-arg", "TF_VERSION={}".format(config['tf_version']),
-        "--build-arg", "CUDA_VERSION={}".format(config['cuda_version']),
-        "--build-arg", "CUDNN_VERSION={}".format(config['cudnn_version']),
-        "-t", "{}/tensorflow-serving-{}:{}".format(args.registry, version, args.tag),
-        "-f", "Dockerfile.gpu", "."]
+
+  build_args = get_build_args(config)
+
+  command = list(chain(
+      ["docker", "build", "--pull"],
+      build_args,
+      ["-t", "{}/tensorflow-serving-{}:{}".format(args.registry, version, args.tag),
+       "-f", "Dockerfile.{}".format(args.platform), "."]
+  ))
   run(command, cwd=context_dir)
 
 def main():
