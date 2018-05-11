e9ce3d2fdef1bd43adad2f7c57005186bb6758ec
Ankush Agarwal <agwl@google.com>
2018-05-11 16:42:13 -0700

    Use test_helper to simplify test_jsonnet.py

--- testing/test_jsonnet.py
+++ testing/test_jsonnet.py
@@ -30,13 +30,10 @@ from __future__ import print_function
 
 import logging
 import os
-import time
-import glob
 
 import argparse
 
-from kubeflow.testing import test_util
-from kubeflow.testing import util
+from kubeflow.testing import test_helper, util
 
 # We should test all files which end in .jsonnet or .libsonnet
 # except ksonnet prototype definitions - they require additional
@@ -50,7 +47,8 @@ def should_test(f):
     raise ValueError('Invalid file : {}'.format(f))
   return parts[-2] != 'prototypes'
 
-def run(test_files_dirs, jsonnet_path_args, t):
+
+def run(test_files_dirs, jsonnet_path_args, test_case):
   # Go through each jsonnet file in test_files_dirs and run jsonnet eval
   for test_files_dir in test_files_dirs:
     for root, _, files in os.walk(test_files_dir):
@@ -61,17 +59,12 @@ def run(test_files_dirs, jsonnet_path_args, t):
           try:
             util.run(['jsonnet', 'eval', full_path] + jsonnet_path_args, cwd=os.path.dirname(full_path))
           except Exception as e:
-            t.failure = '{} test failed'.format(test_file)
+            test_case.add_failure_info('{} test failed'.format(test_file))
             logging.error('%s test failed. See Subprocess output for details.', test_file)
-            raise
 
 
-def main():  # pylint: disable=too-many-locals
-  logging.getLogger().setLevel(logging.INFO) # pylint: disable=too-many-locals
-  # create the top-level parser
-  parser = argparse.ArgumentParser(
-    description="jsonnet tests.")
-
+def parse_args():
+  parser = argparse.ArgumentParser()
   parser.add_argument(
     "--test_files_dirs",
     default="",
@@ -82,41 +75,17 @@ def main():  # pylint: disable=too-many-locals
     default="",
     type=str,
     help="Comma separated directories used by jsonnet to find additional libraries")
-  parser.add_argument(
-    "--artifacts_dir",
-    default="",
-    type=str,
-    help="Directory to use for artifacts that should be preserved after "
-         "the test runs. Defaults to test_dir if not set.")
+  args, _ = parser.parse_known_args()
+  return args
 
-  args = parser.parse_args()
+
+def test_jsonnet(test_case): # pylint: disable=redefined-outer-name
+  args = parse_args()
 
   if not args.test_files_dirs:
     raise ValueError('--test_files_dirs needs to be set')
 
-  test_log = os.path.join(args.artifacts_dir, "logs",
-                          "test_jsonnet.log.txt")
-  if not os.path.exists(os.path.dirname(test_log)):
-    os.makedirs(os.path.dirname(test_log))
-
-  root_logger = logging.getLogger()
-  file_handler = logging.FileHandler(test_log)
-  root_logger.addHandler(file_handler)
-  # We need to explicitly set the formatter because it will not pick up
-  # the BasicConfig.
-  formatter = logging.Formatter(fmt=("%(levelname)s|%(asctime)s"
-                                     "|%(pathname)s|%(lineno)d| %(message)s"),
-                                datefmt="%Y-%m-%dT%H:%M:%S")
-  file_handler.setFormatter(formatter)
-  logging.info("Logging to %s", test_log)
-
-
-  t = test_util.TestCase()
-  t.class_name = "Kubeflow"
-  t.name = "test-jsonnet"
-
   test_files_dirs = args.test_files_dirs.split(',')
-  start = time.time()
 
   jsonnet_path_args = []
   if len(args.jsonnet_path_dirs) > 0:
@@ -124,21 +93,12 @@ def main():  # pylint: disable=too-many-locals
       jsonnet_path_args.append('--jpath')
       jsonnet_path_args.append(jsonnet_path_dir)
 
-  try:
-    run(test_files_dirs, jsonnet_path_args, t)
-  finally:
-    t.time = time.time() - start
-    junit_path = os.path.join(
-        args.artifacts_dir, "junit_kubeflow-test-jsonnet.xml")
-    logging.info("Writing test results to %s", junit_path)
-    test_util.create_junit_xml_file([t], junit_path)
+  run(test_files_dirs, jsonnet_path_args, test_case)
 
 
 if __name__ == "__main__":
-  logging.basicConfig(level=logging.INFO,
-                      format=('%(levelname)s|%(asctime)s'
-                              '|%(pathname)s|%(lineno)d| %(message)s'),
-                      datefmt='%Y-%m-%dT%H:%M:%S',
-                      )
-  logging.getLogger().setLevel(logging.INFO)
-  main()
+  test_case = test_helper.TestCase(
+    name='test_jsonnet', test_func=test_jsonnet)
+  test_suite = test_helper.init(
+    name='', test_cases=[test_case])
+  test_suite.run()
