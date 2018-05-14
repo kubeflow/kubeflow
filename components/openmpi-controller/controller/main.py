ff5be38d992ba2e3db8c792901509d29d9ebab1d
Jie Zhang <jiez@uber.com>
2018-05-14 12:02:26 -0700

    [openmpi] Download and upload data in the controller (#797)
    Fix #713

--- components/openmpi-controller/controller/main.py
+++ components/openmpi-controller/controller/main.py
@@ -9,12 +9,20 @@ def main():
     parser.add_argument('--master', type=str, required=True)
     parser.add_argument('--num-gpus', type=int, default=0)
     parser.add_argument('--timeout-secs', type=int, default=300)
+    parser.add_argument('--download-data-from', type=str)
+    parser.add_argument('--download-data-to', type=str)
+    parser.add_argument('--upload-data-from', type=str)
+    parser.add_argument('--upload-data-to', type=str)
     args = parser.parse_args()
 
     with Controller(namespace=args.namespace,
                     master=args.master,
                     num_gpus=args.num_gpus,
-                    timeout_secs=args.timeout_secs) as ctl:
+                    timeout_secs=args.timeout_secs,
+                    download_data_from=args.download_data_from,
+                    download_data_to=args.download_data_to,
+                    upload_data_from=args.upload_data_from,
+                    upload_data_to=args.upload_data_to) as ctl:
         ctl.wait_ready()
         ctl.wait_done()
 
