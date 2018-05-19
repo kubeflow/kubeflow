18f1e1a9a40721195fd59b01db1f0209d5dd8231
Shingo Omura <everpeace@gmail.com>
2018-05-19 09:16:50 +0900

    [openmpi] support non-root users run jobs (introducing `runAsUser`, `runAsGroup`, `supplementalGroups`) (#820)
    * openmpi: support non-root user in `init.sh`
    
    * openmpi: change openmpi-controller path outside of /root so non-root users can read
    
    * openmpi: support `runAsUser`, `runAsGroup`, `supplementalGroups`.

--- kubeflow/openmpi/assets/init.sh
+++ kubeflow/openmpi/assets/init.sh
@@ -1,9 +1,14 @@
 set -exv
 
 OPENMPI_DIR=/kubeflow/openmpi
+SSHD_CONFIG=${OPENMPI_DIR}/assets/sshd_config
 MPIEXEC_TIMEOUT_ON_WAIT_MPI_READY=10
 BACKOFF_SECS=10
 TIMEOUT_EXIT_CODE=124
+
+run_by_non_root() {
+  [ "$(id -u)" != "0" ]
+}
 
 wait_mpi_ready() {
   local workers=$1
@@ -47,15 +52,29 @@ timeout_secs="$4"
 max_retries=$(expr ${timeout_secs} / ${BACKOFF_SECS})
 
 # Set up openmpi
-mkdir -p /root/.openmpi
-cp ${OPENMPI_DIR}/assets/mca-params.conf /root/.openmpi
+mkdir -p ${HOME}/.openmpi
+cp ${OPENMPI_DIR}/assets/mca-params.conf ${HOME}/.openmpi
 
 # Set up ssh
-mkdir -p /root/.ssh
-cp ${OPENMPI_DIR}/secrets/id_rsa /root/.ssh
-cp ${OPENMPI_DIR}/secrets/id_rsa.pub /root/.ssh
-cp ${OPENMPI_DIR}/secrets/authorized_keys /root/.ssh
-cp ${OPENMPI_DIR}/assets/ssh_config /root/.ssh/config
+mkdir -p ${HOME}/.ssh
+cp ${OPENMPI_DIR}/secrets/id_rsa ${HOME}/.ssh
+chmod 400 ${HOME}/.ssh/id_rsa
+cp ${OPENMPI_DIR}/secrets/id_rsa.pub ${HOME}/.ssh
+cp ${OPENMPI_DIR}/secrets/authorized_keys ${HOME}/.ssh
+cp ${OPENMPI_DIR}/assets/ssh_config ${HOME}/.ssh/config
+
+# when this runs by non-root user.
+# we have to create ephemeral hostkeys
+if run_by_non_root ; then
+  mkdir -p ${HOME}/.sshd
+  cp ${OPENMPI_DIR}/assets/sshd_config ${HOME}/.sshd/sshd_config
+  SSHD_CONFIG=${HOME}/.sshd/sshd_config
+
+  ssh-keygen -f ${HOME}/.sshd/host_rsa_key -C '' -N '' -t rsa
+  ssh-keygen -f ${HOME}/.sshd/host_dsa_key -C '' -N '' -t dsa
+  echo "HostKey ${HOME}/.sshd/host_rsa_key" >> ${SSHD_CONFIG}
+  echo "HostKey ${HOME}/.sshd/host_dsa_key" >> ${SSHD_CONFIG}
+fi
 
 exit_code=0
 if [ "${role}" = "master" ]; then
@@ -69,7 +88,7 @@ else
   wait_controller_signal SIGCONT ${max_retries}
 
   # Start sshd in daemon mode
-  /usr/sbin/sshd -e -f ${OPENMPI_DIR}/assets/sshd_config
+  /usr/sbin/sshd -e -f ${SSHD_CONFIG}
 
   # Block forever until controller terminates.
   wait_controller_signal SIGTERM
