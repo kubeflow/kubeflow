1730fbbd2f40c8a91f3b8246d77cd64c26c77bb5
Jeremy Lewi <jeremy@lewi.us>
2018-05-23 14:38:35 +0200

    Deploying the bootstrapper via deployment manager. (#823)
    * This config creates the K8s resources needed to run the bootstrapper
    * Enable the ResourceManager API; this is used to get IAM policies
    * Add IAM roles to the cloudservices account. This is needed so that
      the deployment manager has sufficient RBAC permissions to do what it needs
      to.
    
    * Delete initialNodeCount and just make the default node pool a 1 CPU node pool.
    
    * The bootstrapper isn't running successfully; it looks like its trying
      to create a pytorch component but its using an older version of the registry
      which doesn't include the pytorch operator.
    
    * Set delete policy on K8s resources to ABANDON otherwise we get internal errors.
    * We can use actions to enable APIs and then we won't try to delete
      the API when the deployment is deleted which causes errors.
    
    fix #833

--- docs/gke/configs/env-kubeflow.sh
+++ docs/gke/configs/env-kubeflow.sh
@@ -18,6 +18,10 @@ export NAMESPACE=kubeflow
 
 # Set config file to the YAML file defining your deployment manager configs.
 export CONFIG_FILE=cluster-${PROJECT}.yaml
+export PROJECT_CONFIG_FILE=project-${PROJECT}.yaml
+
+# Get the project number
+export PROJECT_NUMBER=`gcloud projects describe ${PROJECT} --format='value(project_number)'`
 
 # ksonnet environment
 #export ENV=${PROJECT}
