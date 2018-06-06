810f7738fd91fbbe65aabfd4d16550ec90a7a971
kunmingg <37601826+kunmingg@users.noreply.github.com>
2018-06-05 20:51:58 -0700

    keep bootstrapper pod alive when error occurs (#921)
    * keep bootstrapper pod alive when running inside k8s
    
    * Edit message when error happens
    
    * handle review feedback

--- bootstrap/cmd/bootstrap/main.go
+++ bootstrap/cmd/bootstrap/main.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/kubeflow/kubeflow/bootstrap/cmd/bootstrap/app"
 	"github.com/kubeflow/kubeflow/bootstrap/cmd/bootstrap/app/options"
+	"time"
 )
 
 func init() {
@@ -43,6 +44,14 @@ func main() {
 	}
 
 	if err := app.Run(s); err != nil {
-		log.Fatalf("%v\n", err)
+		if s.InCluster && s.KeepAlive {
+			log.Errorf("Bootstrapper failed with error: %v\n", err)
+			log.Infof("Keeping pod alive so user can ssh in and check error status.")
+			for {
+				time.Sleep(time.Minute)
+			}
+		} else {
+			log.Fatalf("%v\n", err)
+		}
 	}
 }
