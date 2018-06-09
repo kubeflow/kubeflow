2e7b66bea1efbbc4749837a064e959dcae27a94e
kunmingg <37601826+kunmingg@users.noreply.github.com>
2018-06-08 19:08:02 -0700

    hide registries config in bootstrapper image to save user effort (#951)
    * hide registries config in bootstrapper image to save user effort
    
    * typo
    
    * dedup registries when load config

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -24,7 +24,7 @@ def main(unparsed_args=None):
     help="Docker build opts.")
   parser.add_argument(
     "--config",
-    default="config/default.yaml",
+    default="image_registries.yaml",
     type=str,
     help="Relative path to bootstrapper config file specify which registries to include.")
   parser.add_argument(
@@ -44,19 +44,23 @@ def main(unparsed_args=None):
   with open(os.path.join(FILE_PATH, args.config), 'r') as conf_input:
     conf = yaml.load(conf_input)
 
+  test_reg_name, test_reg_path = "", ""
+  if args.test_registry != "" and len(args.test_registry.split(":")) == 2:
+    test_reg_name = args.test_registry.split(":")[0]
+    test_reg_path = args.test_registry.split(":")[1]
+
   for reg in conf['registries']:
-    if "repo" in reg and "branch" in reg:
+    if test_reg_name == reg["name"]:
+      src_registry = os.path.join(test_reg_path, reg["path"])
+      reg_path = os.path.join(tmp_dir, *os.path.join(reg["name"], reg["path"]).split('/')[:-1])
+      if not os.path.exists(reg_path):
+        os.makedirs(reg_path)
+      print("cp -r %s %s" % (src_registry, reg_path))
+      os.system("cp -r %s %s" % (src_registry, reg_path))
+    else:
       reg_path = os.path.join(tmp_dir, reg["name"])
       print("Adding registry %s from %s %s" % (reg["name"], reg["repo"], reg["branch"]))
       os.system("git clone --depth 1 --branch %s %s %s" % (reg["branch"], reg["repo"], reg_path))
-    else:
-      if args.test_registry != "" and args.test_registry.split(":")[0] == reg["name"]:
-        src_registry = os.path.join(args.test_registry.split(":")[1], reg["path"])
-        reg_path = os.path.join(tmp_dir, *os.path.join(reg["name"], reg["path"]).split('/')[:-1])
-        if not os.path.exists(reg_path):
-          os.makedirs(reg_path)
-        print("cp -r %s %s" % (src_registry, reg_path))
-        os.system("cp -r %s %s" % (src_registry, reg_path))
 
   os.system("docker build %s -t %s %s --build-arg registries=%s" %
             (args.build_opts, args.image, FILE_PATH, REG_FOLDER))
