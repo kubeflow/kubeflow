338cd137a156de2b30af9914657513d10913f77c
kunming <kunming@google.com>
2018-06-15 16:33:52 -0700

    update deploy name for e2e test

--- testing/deploy_kubeflow.py
+++ testing/deploy_kubeflow.py
@@ -86,7 +86,7 @@ def deploy_kubeflow(test_case):
   util.run(apply_command, cwd=app_dir)
 
   # Verify that the TfJob operator is actually deployed.
-  tf_job_deployment_name = "tf-job-operator"
+  tf_job_deployment_name = "tf-job-operator-v1alpha2"
   logging.info("Verifying TfJob controller started.")
   util.wait_for_deployment(api_client, namespace, tf_job_deployment_name)
 
