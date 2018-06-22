2123026fd4dc6aa84a91c10505f40ce2472af66b
Jeremy Lewi <jeremy@lewi.us>
2018-06-22 04:53:07 -0700

    Tooling to make it easier to tag images and update the ksonnet prototypes (#1066)
    * Provide some python scripts to match images and then apply a tag to
      them like "v0.2.0". This makes it possible to easily apply a new release
      tag to set of images like the Jupyter images.
    
    * Creeate a shell script to use sed and other twos to update images
      in our ksonnet prototypes.
    
    * Add instructions for doing this.
    
    * I used the scripts to add the v0.2.0 tag to our Jupyter images.
    
    Related to #1060

--- releasing/apply_image_tags.py
+++ releasing/apply_image_tags.py
@@ -2,6 +2,7 @@
 
 import argparse
 import logging
+import re
 import yaml
 
 from kubeflow.testing import util
@@ -18,17 +19,33 @@ def main(unparsed_args=None):  # pylint: disable=too-many-locals
     type=str,
     help="Yaml file containing the tags to attach.")
 
+  parser.add_argument(
+    "--pattern",
+    default="",
+    type=str,
+    help=("Regex pattern e.g. .*tensorflow.*notebook.*:v20180619.* "
+          "to select the images to apply."))
   args = parser.parse_args()
 
   with open(args.images_file) as hf:
     config = yaml.load(hf)
 
+  name_pattern, tag_pattern = args.pattern.split(":")
+  name_re = re.compile(name_pattern)
+  tag_re = re.compile(tag_pattern)
+
   for image in config["images"]:
-    for tag in image["tags"]:
-      # TODO(jlewi): This appears to be really slow even when we aren't
-      # moving the image. Much slower than doing it in the UI
-      util.run(["gcloud", "container", "images", "add-tag", "--quiet",
-                image["image"], tag])
+    name = image["name"]
+    if not name_re.match(name):
+      continue
+    for v in image["versions"]:
+      for tag in v["tags"]:
+        if not tag_re.match(tag):
+          continue
+        source = name + "@" + v["digest"]
+        dest = name + ":" + tag
+        util.run(["gcloud", "container", "images", "add-tag", "--quiet",
+                  source, dest])
 
   logging.info("Done.")
 
