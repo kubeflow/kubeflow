9eca36452f2b707bb02d292f67cd214d8b5fd033
Jeremy Lewi <jeremy@lewi.us>
2018-07-07 11:49:57 -0700

    Tag the latest Jupyter images with v0.2.1
    * These images includes fixes to TFMA in the Jupyter images.
    
    * Related to #1129 - Upgrade images for 0.2.1
    
    * Related to #1130 - Fix TFMA
    
    * Upgrade the images in the ksonnet.

--- releasing/update_images.sh
+++ releasing/update_images.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$( cd ${DIR}/.. && pwd )"
 
 # Update the TFJob operator image
-python scripts/update_prototype.py \
+python ${ROOT_DIR}/scripts/update_prototype.py \
 	--file=${ROOT_DIR}/kubeflow/core/prototypes/all.jsonnet \
 	--values=tfJobImage=gcr.io/kubeflow-images-public/tf_operator:v0.2.0
