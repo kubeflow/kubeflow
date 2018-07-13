f1b96b09933e798f54bc120dd1721ba700d69ec3
Ankush Agarwal <agwl@google.com>
2018-07-13 14:55:28 -0700

    Delete docs/gke/configs and move tests to scripts/gke/configs (#1195)
    add retry logic
    
    typo

--- scripts/gke/teardown.sh
+++ scripts/gke/teardown.sh
@@ -12,6 +12,18 @@ ZONE=${ZONE:-$(gcloud config get-value compute/zone 2>/dev/null)}
 ZONE=${ZONE:-"us-central1-a"}
 
 cd "${KUBEFLOW_DM_DIR}"
+
+# Delete kubeflow namespace - this deletes all the ingress objects
+# in the namespace which deletes the associated GCP resources
+set +e
+kubectl delete ns/kubeflow
+while kubectl get ns/kubeflow; do
+  echo "kubeflow namespace not yet deleted. sleeping 10 seconds..."
+  sleep 10
+done
+echo "kubeflow namespace successfully deleted."
+set -e
+
 # We need to run an update because for deleting IAM roles,
 # we need to obtain a fresh copy of the IAM policy. A stale
 # copy of IAM policy causes issues during deletion.
