e925911c4765e7b89e76ff7211fab084e784e467
Ankush Agarwal <agwl@google.com>
2018-07-13 04:19:36 -0700

    add retry logic

--- testing/deploy_kubeflow_gcp.sh
+++ testing/deploy_kubeflow_gcp.sh
@@ -3,7 +3,7 @@
 # config at scripts/gke. It takes the following flags:
 # NAME, TEST_DIR
 
-set -xe
+set -x
 
 NAME="${1}"
 TEST_DIR="${2}"
@@ -13,13 +13,23 @@ if [[ -n "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
 fi
 
 cd "${TEST_DIR}"
-ln -s src/kubeflow/kubeflow kubeflow_repo
+
 export CLIENT_ID=dummy \
   CLIENT_SECRET=dummy \
   DEPLOYMENT_NAME=${NAME} \
   PROJECT=kubeflow-ci \
   ZONE=us-east1-d \
+  KUBEFLOW_REPO="${TEST_DIR}/src/kubeflow/kubeflow"
   SETUP_PROJECT=false \
   SKIP_METRICS_COLLECTION=true \
   IAP_IAM_ENTRY="serviceAccount:kubeflow-testing@kubeflow-ci.iam.gserviceaccount.com"
+
 bash src/kubeflow/kubeflow/scripts/gke/deploy.sh
+
+# If deploy.sh fails, tear down deployment so that it can be retried
+
+if [[ $? -gt 0 ]]; then
+  bash src/kubeflow/kubeflow/scripts/gke/teardown.sh
+  rm -rf "${TEST_DIR}/${DEPLOYMENT_NAME}_deployment_manager_configs"
+  rm -rf "${TEST_DIR}/${DEPLOYMENT_NAME}_ks_app"
+fi
