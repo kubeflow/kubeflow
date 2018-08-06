e6b9905d45ee6d71f09b9d66e6a080395cd9baea
Jeremy Lewi <jeremy@lewi.us>
2018-08-06 11:56:17 -0700

    Add retries to the command.

--- testing/workflows/run.sh
+++ testing/workflows/run.sh
@@ -9,4 +9,20 @@ gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS
 echo Working Directory=$(pwd)
 # Execute the actual command.
 # TODO(jlewi): We should add retries on error.
-"$@"
+
+# Retry up to 3 times
+for i in `seq 1 3`;
+do
+	set +e
+	"$@"
+	result=$?
+	set -e
+	if [[ ${result} -eq 0 ]]; then
+		echo command ran successfully
+		exit 0
+	fi
+
+	echo Command failed: "$@" 
+done
+echo command didn't succeed
+exit 1
