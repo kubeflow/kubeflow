6598c809eacbf6eb9998840df00dc1be2b49f69b
Jeremy Lewi <jeremy+github@lewi.us>
2018-08-07 10:14:27 -0700

    Turn deploy.sh into kfctl.sh (#1308)
    * Create kfctl.sh which is reentrant and usable for different platforms.
    
    kfctl.sh works as follows
    
    kfctl.sh init myapp --platform gcp
    cd myapp
    kfctl.sh generate all
    kfctl.sh apply all
    
    * All configuration is persisted inside files inside myapp.
    
    * The scripts were becoming more complicated because we are creating
      more environment variables to control things like whether or not to use
      GCFS.
    
    * Those environment variables are creating an unnecessary layer of indirection;
      if users want to configure things like private cluster, or GCFS, they
      should just edit the ksonnet or DM configs.
    
    * To support that we need to make the following flow easier
    
      create DM and ksonnet configs
      user manually edits configs
      user deploys
    
    * Furthermore, these environment variables weren't being persisted anywhere
      so repeatibility on subsequent invocations was a major problem.
    
    * Stop copying the Kubeflow repo into the user's directory; instead just
      execute kfctl.sh from a directory containing the GCP configs and ksonnet
      registry. Those files are copied when needed to the user's app directory.
    
    * Create DM configs for creating a network and GCFS. This makes it much
      easier for users to customize it for their needs (i.e. to create a non-legacy
      network).
    
      kfctl.sh apply will automatically create the GCF and network deployments
      if the corresponding YAMl files are present in the gcp_config directory.
    
    * Create a separate script download.sh to download the needed files from Kubeflow
      (e.g. ksonnet registry & scripts).
    
      This does two things
    
        1. Code reuse between scripts for different platforms so we don't
           duplicate the download code.
    
        2. download.sh doesn't invoke kfctl.sh. kfctl.sh is executed from the
           downloaded files. This ensures kfctl.sh is in sync with the version
           of the repo that is actually fetched.
    
    * Create a common function to create the ksonnet application.
      Different platforms can reuse this function. This ensures there
      is a common experience across platforms.
    
      After creating the ksonnet application; different platforms can customize
      it.
    
    * Refactor deploy.sh to be kfctl.sh and have similar semantics to kubectl/ks.
    
    * Run everthing from the directory containing kfctl.sh and copy artifacts to the
      directory the user creates as needed
    
    * Improve scripts to delete GCP deployments
      * If delete fails; run delete with policy abandon and manually delete
        resources
      * Delete IAM resource bindings explicitly
    
    * Add a python test for the download.
    
    * Create a new E2E workflow test for kfctl.sh
    
    * Fix IAP entry so it works with service account.
    
    * When deleting service accounts check if service account doesn't exist; this
      way we won't print out confusing error messages when we try to delete
      the SA.
    
    * We keep gke/deploy.sh and gke/teardown.sh because they are used by the
      existing e2e test and this PR is already quite large so we will refactor
      the test to use kfctl.sh in a subsequent step.
    
    * Don't set a zone in cluster-kubeflow.yaml; This is misleading because the zone
      gets overwritten by the scripts. We need to pick a zone which has
      Avx2. So we can't use us-central1-a anymore.
    
    * Autoformat jsonnet.
    
    * * kfctl needs a unique directory for kubeconfig so that it doesn't overlap
      with other workflows.

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # Util functions to be used by scripts in this directory
 
+function usage() {
+    echo "usage: kfctl <command> <what>"
+    echo "where command is one of"
+    echo "init - initialize something"
+    echo "apply  -- apply some config"
+    echo "delete - delete some components"
+    echo
+    echo "what is one of"    
+    echo "project - the GCP project"
+    echo "platform - platform resources (e.g. GCP, minikube); basically non K8s resources"
+    echo "k8s - kubernetes resources"    
+    echo "help - print this message"       
+}
+
+
 function check_install() {
   if ! which "${1}" &>/dev/null; then
     echo "You don't have ${1} installed. Please install ${1}."
@@ -13,4 +28,46 @@ function check_variable() {
     echo "'${2}' environment variable is not set. Please set it using export ${2}=value."
     exit 1
   fi
+}
+
+function createKsApp() {
+  # Create the ksonnet application.
+  # All deployments should call this function to create a common ksonnet app.
+  # They can then customize it as necessary.
+  pushd .
+  # Create the ksonnet app
+  cd $(dirname "${KUBEFLOW_KS_DIR}")
+  ks init $(basename "${KUBEFLOW_KS_DIR}")
+  cd "${KUBEFLOW_KS_DIR}"
+
+  # Remove the default environment; The cluster might not exist yet
+  # So we might be pointing to the wrong  cluster.
+  ks env rm default
+
+  # Add the local registry
+  ks registry add kubeflow "${KUBEFLOW_REPO}/kubeflow"
+
+  # Install all required packages
+  ks pkg install kubeflow/argo
+  ks pkg install kubeflow/core
+  ks pkg install kubeflow/examples
+  ks pkg install kubeflow/katib
+  ks pkg install kubeflow/mpi-job
+  ks pkg install kubeflow/pytorch-job
+  ks pkg install kubeflow/seldon
+  ks pkg install kubeflow/tf-serving
+
+  # Generate all required components
+  ks generate pytorch-operator pytorch-operator
+  # TODO(jlewi): Why are we overloading the ambassador images here?
+  ks generate ambassador ambassador
+  ks generate jupyterhub jupyterhub
+  ks generate centraldashboard centraldashboard
+  ks generate tf-job-operator tf-job-operator
+
+  ks generate argo argo
+
+  # Enable collection of anonymous usage metrics
+  # Skip this step if you don't want to enable collection.
+  ks generate spartakus spartakus --usageId=$(uuidgen) --reportUsage=${COLLECT_METRICS}
 }
--- testing/workflows/environments/kubeflow-testing/params.libsonnet
+++ testing/workflows/environments/kubeflow-testing/params.libsonnet
@@ -1,10 +1,16 @@
 local params = import "../../components/params.libsonnet";
+
 params {
   components+: {
-    // Insert component parameter overrides here. Ex:
-    // guestbook +: {
-    //   name: "guestbook-dev",
-    //   replicas: params.global.replicas,
-    // },
+    kfctl_Test+: {
+      namespace: "kubeflow-test-infra",
+      name: "jlewi-kfctl-test-1308-0539",
+      prow_env: "JOB_NAME=kfctl-test,JOB_TYPE=presubmit,REPO_NAME=kubeflow,REPO_OWNER=kubeflow,BUILD_NUMBER=0539,PULL_NUMBER=1308",
+    },
+    kfctl_test+: {
+      namespace: "kubeflow-test-infra",
+      name: "jlewi-kfctl-test-1308-0806-150201",
+      prow_env: "JOB_NAME=kfctl-test,JOB_TYPE=presubmit,REPO_NAME=kubeflow,REPO_OWNER=kubeflow,BUILD_NUMBER=0806-150201,PULL_NUMBER=1308",
+    },
   },
 }
