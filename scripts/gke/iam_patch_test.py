709d59e6a7a755772a05335beccb290262f42700
Ankush Agarwal <agwl@google.com>
2018-08-22 22:04:52 -0700

    Python script to declaratively manage IAM binding patches

---
+++ scripts/gke/iam_patch_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Unit tests for iam_patch.py
+import iam_patch
+
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_apply_iam_bindings_patch_add(self):
+        current_policy = {
+        }
+        bindings_patch = {
+            "gcpIamPolicy": {
+                "members": {
+                  "admin": ["admin-sa"]
+                },
+                "bindings": {
+                    "admin": ["roles/source.admin"]
+                }
+            }
+        }
+        expected = {'roles/source.admin': set(['admin-sa'])}
+        result = iam_patch.apply_iam_bindings_patch(current_policy, bindings_patch, "add")
+        self.assertEqual(result, expected)
+
+    def test_apply_iam_bindings_patch_remove(self):
+        current_policy = {'roles/source.admin': set(['admin-sa'])}
+        bindings_patch = {
+            "gcpIamPolicy": {
+                "members": {
+                  "admin": ["admin-sa"]
+                },
+                "bindings": {
+                    "admin": ["roles/source.admin"]
+                }
+            }
+        }
+        expected = {'roles/source.admin': set()}
+        result = iam_patch.apply_iam_bindings_patch(current_policy, bindings_patch, "remove")
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
