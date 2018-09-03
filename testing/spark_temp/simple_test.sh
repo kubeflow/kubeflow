90ba0261258ce771534f33a423805121a84ae490
Holden Karau <holden@pigscanfly.ca>
2018-09-03 10:06:43 -0700

    Add two hacked up temporary test scripts for use during dev (TODO refactor later into proper workflow)

---
+++ testing/spark_temp/simple_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -x
+set -o pipefail
+
+mkdir kf_test
+pushd kf_test
+
+ks init coffee_is_amazing
+pushd coffee_is_amazing
+#export KUBEFLOW_VERSION=v0.2.0-rc.1
+#ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${KUBEFLOW_VERSION}/kubeflow
+ks registry add kubeflow /home/hkarau/repos/kubeflow/kubeflow
+ks pkg install kubeflow/core
+ks pkg install kubeflow/spark
+ks env add gke
+ks param set --env gke kubeflow-core \
+  cloud "gke"
+ks param set --env gke kubeflow-core \
+   tfAmbassadorServiceType "LoadBalancer"
+# I forgot to generate and that's why it was rough
+# ks generate core kubeflow-core --name=kubeflow-core --cloud=gke
+ks generate spark-operator spark-operator --name=spark-operator
+# Not to self KS doesn't give any error when the component doesn't exist
+ks apply gke -c spark-operator --verbose
+
+ks generate spark-job spark-pi --name=spark-operator --applicationResource="local:///opt/spark/examples/jars/spark-examples_2.11-2.3.1.jar" --mainClass=org.apache.spark.examples.SparkPi
+
+ks apply gke -c spark-pi
+
+
+#popd
+#popd
