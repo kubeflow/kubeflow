c35c084d610989edf36445d377df6a4cc7aacd43
adselvar <adhita94@gmail.com>
2018-09-24 15:07:53 -0700

    Image build for centralui

--- components/centraldashboard/build_image.sh
+++ components/centraldashboard/build_image.sh
@@ -4,20 +4,25 @@
 # This is intended to be invoked as a step in Argo to build the docker image.
 #
 # build_image.sh ${DOCKERFILE} ${IMAGE} ${TAG} (optional | ${TEST_REGISTRY})
-set -ex
+set -o errexit
+set -o nounset
+set -o pipefail
 
 DOCKERFILE=$1
 IMAGE=$2
 TAG=$3
+PROJECT="${GCP_PROJECT}"
+REGISTRY="${GCP_REGISTRY}"
+CONTEXT_DIR=$(dirname "$DOCKERFILE")
+
 
 # Wait for the Docker daemon to be available.
 until docker ps
 do sleep 3
 done
 
-docker build -f ${DOCKERFILE} -t ${IMAGE}:${TAG} .
-
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-gcloud docker -- push "${IMAGE}:${TAG}"
-docker tag "${IMAGE}:${TAG}" "${IMAGE}:latest"
-gcloud docker -- push "${IMAGE}:latest"
+
+cd $CONTEXT_DIR
+
+gcloud builds submit --tag=${REGISTRY}/${IMAGE}:${TAG} --project=${PROJECT} .
