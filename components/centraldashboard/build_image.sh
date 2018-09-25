087934030bb7c190564171a058f4737a5cc672cb
adselvar <adhita94@gmail.com>
2018-09-24 19:32:01 -0700

    remove unbound variables

--- components/centraldashboard/build_image.sh
+++ components/centraldashboard/build_image.sh
@@ -3,16 +3,12 @@
 # A simple script to build the Docker images.
 # This is intended to be invoked as a step in Argo to build the docker image.
 #
-# build_image.sh ${DOCKERFILE} ${IMAGE} ${TAG} (optional | ${TEST_REGISTRY})
-set -o errexit
-set -o nounset
-set -o pipefail
+# build_image.sh ${DOCKERFILE} ${IMAGE} ${TAG}
+set -ex
 
 DOCKERFILE=$1
 IMAGE=$2
 TAG=$3
-PROJECT="${GCP_PROJECT}"
-REGISTRY="${GCP_REGISTRY}"
 CONTEXT_DIR=$(dirname "$DOCKERFILE")
 
 
@@ -25,4 +21,5 @@ gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS
 
 cd $CONTEXT_DIR
 
-gcloud builds submit --tag=${REGISTRY}/${IMAGE}:${TAG} --project=${PROJECT} .
+echo "Building centraldashboard using gcloud build"
+gcloud builds submit --tag=${IMAGE}:${TAG} .
