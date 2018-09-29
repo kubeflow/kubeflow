ae7eba66a19e43988e9e7422b84eab038abc8f9d
Jeremy Lewi <jeremy@lewi.us>
2018-09-29 11:49:49 -0700

    Add a comment.

--- scripts/kfctl.sh
+++ scripts/kfctl.sh
@@ -29,6 +29,7 @@ createEnv() {
   # a file for consistency across runs.
   echo PLATFORM=${PLATFORM} >> ${ENV_FILE}
   DEFAULT_KUBEFLOW_REPO="$( cd "${DIR}/.." >/dev/null && pwd )"
+  # Remove trailing slash from the repo.
   KUBEFLOW_REPO=${KUBEFLOW_REPO%/}
   echo KUBEFLOW_REPO=${KUBEFLOW_REPO:-"${DEFAULT_KUBEFLOW_REPO}"} >> ${ENV_FILE}
   echo KUBEFLOW_VERSION=${KUBEFLOW_VERSION:-"master"} >> ${ENV_FILE}
