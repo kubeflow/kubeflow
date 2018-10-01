775a194ec4bda8d09975773fcf9c04064bf83151
Kunming Qu <37601826+kunmingg@users.noreply.github.com>
2018-10-01 16:04:57 -0700

    upgrade argo version to v2.2.0 (#1690)

--- scripts/gke/use_gcr_for_all_images.sh
+++ scripts/gke/use_gcr_for_all_images.sh
@@ -19,9 +19,9 @@
 set -x
 
 if ks component list | awk '{print $1}' | grep -q "^argo$" ; then
-  ks param set argo workflowControllerImage gcr.io/kubeflow-images-public/argoproj/workflow-controller:v2.1.1
-  ks param set argo uiImage gcr.io/kubeflow-images-public/argoproj/argoui:v2.1.1
-  ks param set argo executorImage gcr.io/kubeflow-images-public/argoproj/argoexec:v2.1.1
+  ks param set argo workflowControllerImage gcr.io/kubeflow-images-public/argoproj/workflow-controller:v2.2.0
+  ks param set argo uiImage gcr.io/kubeflow-images-public/argoproj/argoui:v2.2.0
+  ks param set argo executorImage gcr.io/kubeflow-images-public/argoproj/argoexec:v2.2.0
 fi
 
 if ks component list | awk '{print $1}' | grep -q "^cert-manager$" ; then
