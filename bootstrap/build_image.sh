447788ab48802801c4dc47c759c068d5964ed05e
kunming <kunming@google.com>
2018-10-09 15:25:25 -0700

    fix build_image.sh more

--- bootstrap/build_image.sh
+++ bootstrap/build_image.sh
@@ -15,14 +15,14 @@ BUILDER_IMG=gcr.io/kubeflow-images-public/bootstrapper-builder
 BUILDER_IMG_VERSION=$(head -1 ${CONTEXT_DIR}/glide.lock | cut -d ' ' -f 2)
 
 # pull builder image from GCR or build it from local if required one doesn't exist.
-docker pull ${BUILDER_IMG}:${BUILDER_IMG_VERSION} || docker build -t ${BUILDER_IMG}:${BUILDER_IMG_VERSION} -f ${CONTEXT_DIR}/Dockerfile.Builder .
+docker pull ${BUILDER_IMG}:${BUILDER_IMG_VERSION} || docker build -t ${BUILDER_IMG}:${BUILDER_IMG_VERSION} -f ${CONTEXT_DIR}/Dockerfile.Builder ${CONTEXT_DIR}
 
 # Wait for the Docker daemon to be available.
 until docker ps
 do sleep 3
 done
 
-python ${CONTEXT_DIR}/build.py --build_args=BUILDER_IMG=${BUILDER_IMG},BUILDER_IMG_VERSION=${BUILDER_IMG_VERSION} --image=${IMAGE}:${TAG} --config=${CONTEXT_DIR}/image_registries.yaml --test_registry=${4-}
+python ${CONTEXT_DIR}/build.py --build_args=BUILDER_IMG=${BUILDER_IMG},BUILDER_IMG_VERSION=${BUILDER_IMG_VERSION} --image=${IMAGE}:${TAG} --test_registry=${4-}
 
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud docker -- push "${IMAGE}:${TAG}"
