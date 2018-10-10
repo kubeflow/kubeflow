e47a05a1ddf4dc7cfd6ac2e3ba685ea0d6ee8cad
kunming <kunming@google.com>
2018-10-10 09:38:53 -0700

    switch to available GKE version

--- bootstrap/cmd/bootstrap/app/gcpUtils.go
+++ bootstrap/cmd/bootstrap/app/gcpUtils.go
@@ -57,7 +57,7 @@ func (s *ksServer)InsertDeployment(ctx context.Context, req CreateRequest) error
 		dmconf.Resources[0].Properties["users"] = []string { "user:" + req.Email }
 		dmconf.Resources[0].Properties["isWebapp"] = true
 		// TODO: use get-server-config
-		dmconf.Resources[0].Properties["cluster-version"] = "1.10.7-gke.2"
+		dmconf.Resources[0].Properties["cluster-version"] = "1.10.6-gke.2"
 	}
 	confByte, err := yaml.Marshal(dmconf)
 	if err != nil {
