9825daaead673ee08f2b8771a3fc05b0bf9a20ac
kunming <kunming@google.com>
2018-10-11 13:54:21 -0700

    remove deploy app test from presubmit run

--- bootstrap/test_delete.sh
+++ bootstrap/test_delete.sh
@@ -15,7 +15,7 @@ gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS
 
 gcloud container clusters get-credentials ${CLUSTER} --zone ${ZONE} --project kubeflow-ci
 # delete test namespace
-# kubectl delete namespace ${NAMESPACE}
+kubectl delete namespace ${NAMESPACE}
 
 for i in 1 2 3 4 5
 do gcloud -q deployment-manager deployments delete ${DEPLOYMENT} --project=kubeflow-ci-deploy && break || sleep 30
