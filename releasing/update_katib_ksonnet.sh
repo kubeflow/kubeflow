5f4242e58bf6e501cfac5bf48a1d8e732713f9ae
Xiaolin Zhang <leoncamel@gmail.com>
2018-10-15 13:42:09 +0800

    Cherry pick #1727
    * Add katib's hyperband/bayesianoptimization suggestion images
    * Reformat jsonnet
    * Add default params for base-images

--- releasing/update_katib_ksonnet.sh
+++ releasing/update_katib_ksonnet.sh
@@ -10,9 +10,11 @@ export PYTHONPATH=${PYTHONPATH}:${ROOT_DIR}/../git_kubeflow-testing/py
 RELEASE=v0.1.2-alpha-34-gb46378c
 
 VALUES="modeldbFrontendImage=gcr.io/kubeflow-images-public/katib/katib-frontend:${RELEASE}"
-VALUES="${VALUES},suggestionRandomImage=gcr.io/kubeflow-images-public/katib/katib-suggestion-random:${RELEASE}"
-VALUES="${VALUES},suggestionGridImage=gcr.io/kubeflow-images-public/katib/katib-suggestion-grid:${RELEASE}"
-VALUES="${VALUES},vizierCoreImage=gcr.io/kubeflow-images-public/katib/katib-vizier-core:${RELEASE}"
+VALUES="${VALUES},suggestionRandomImage=gcr.io/kubeflow-images-public/katib/suggestion-random:${RELEASE}"
+VALUES="${VALUES},suggestionGridImage=gcr.io/kubeflow-images-public/katib/suggestion-grid:${RELEASE}"
+VALUES="${VALUES},suggestionHyperbandImage=gcr.io/kubeflow-images-public/katib/suggestion-hyperband:${RELEASE}"
+VALUES="${VALUES},suggestionBayesianOptimizationImage=gcr.io/kubeflow-images-public/katib/suggestion-bayesianoptimization:${RELEASE}"
+VALUES="${VALUES},vizierCoreImage=gcr.io/kubeflow-images-public/katib/vizier-core:${RELEASE}"
 # Update the TFJob operator image
 python ${ROOT_DIR}/scripts/update_prototype.py \
 	--file=${ROOT_DIR}/kubeflow/katib/prototypes/all.jsonnet \
