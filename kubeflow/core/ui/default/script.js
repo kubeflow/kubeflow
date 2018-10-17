c6f5d89563355d8fbe80d6b8422699a7435166b8
Ioannis Androulidakis <ioannis@arrikto.com>
2018-10-17 18:45:54 +0300

    Introduce YAML config file for the default JH Spawner UI
    * Add `config.yaml` under `kubeflow/core/ui/default`, where the
      admin can configure the default UI in a descriptive manner
      (e.g. readOnly fields, default values)
    * Parse the YAML configuration file inside `spawner.py` and pass it to
      Jinja as a parameter
    * Add logic in `kubeflow/core/ui/default/script.js` to initialize the
      Spawner form fields with the default values defined in `config.yaml`
    * Set any empty Spawner form fields to the default values specified in
      `config.yaml`
    * Update `jupyterhub.libsonnet` to pass the YAML configuration of
      JH Spawner UI as data, via the configMap
    * Remove `registry` and `repoName` ksonnet parameters, along with
      respective definitions, checks and assignments in `jupyterhub.jsonnet`,
      `jupyterhub_config.py` and `spawner.py`
    
    Signed-off-by: Ioannis Androulidakis <ioannis@arrikto.com>
    Signed-off-by: Ilias Tsitsimpis <iliastsi@arrikto.com>

--- kubeflow/core/ui/default/script.js
+++ kubeflow/core/ui/default/script.js
@@ -3,4 +3,60 @@ $(function() {
   $('#toggle_advanced_options').on('click', function(e) {
     $('#advanced_fields').toggle();
   });
+
+  // Fill the form with values defined in the YAML config file
+  setDefaultFormValues();
+
+  // Set tooltip to disabled form fields
+  setTooltipsOnDisabled();
 });
+
+// Set default values to form fields
+function setDefaultFormValues() {
+
+  // Set default Container Image - Disable if specified
+  formDefaults.image.options.forEach(function(item) {
+    $('#image_list').append($('<option/>').attr('value', item));
+  });
+
+  $('#image').val(formDefaults.image.value)
+  if (formDefaults.image.readOnly) {
+    $('#image').attr('disabled', true);
+  }
+
+  // Set default CPU - Disable if specified
+  $('#cpu').val(formDefaults.cpu.value);
+  if (formDefaults.cpu.readOnly) {
+    $('#cpu').attr('disabled', true);
+  }
+
+  // Set default Memory - Disable if specified
+  $('#memory').val(formDefaults.memory.value);
+  if (formDefaults.memory.readOnly) {
+    $('#memory').attr('disabled', true);
+  }
+
+  // Set default Extra Resources - Disable if specified
+  $('#extraResources').val(formDefaults.extraResources.value);
+  if (formDefaults.extraResources.readOnly) {
+    $('#extraResources').attr('disabled', true);
+  }
+}
+
+// Helper function to set a tooltip to admin-disabled Spawner form fields
+function setTooltipsOnDisabled() {
+  // Get all disabled Elements
+  var disabledElements = $('*:disabled');
+
+  // Keep only disabled Elements that do not already have a tooltip
+  var filtered = disabledElements.filter(function() {
+    return !($(this).attr('data-toggle'))
+  });
+
+  // Set tooltip
+  filtered.attr({
+    'data-toggle': 'tooltip',
+    'data-placement': 'top',
+    'title': 'This option has been set by your administrator'
+  });
+}
