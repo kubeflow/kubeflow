c081acc13caa3ea3bd4160377db6b11a1fc79876
Holden Karau <holden@pigscanfly.ca>
2018-12-28 11:58:01 -0800

    s/core/common/ and /var/log/syslog to README

--- testing/spark_temp/simple_test.sh
+++ testing/spark_temp/simple_test.sh
@@ -11,12 +11,12 @@ pushd coffee_is_amazing
 #export KUBEFLOW_VERSION=v0.2.0-rc.1
 #ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${KUBEFLOW_VERSION}/kubeflow
 ks registry add kubeflow ~/repos/kubeflow/kubeflow
-ks pkg install kubeflow/core
+ks pkg install kubeflow/common
 ks pkg install kubeflow/spark
 ks env add gke
-ks param set --env gke kubeflow-core \
+ks param set --env gke kubeflow-common \
   cloud "gke"
-ks param set --env gke kubeflow-core \
+ks param set --env gke kubeflow-common \
    tfAmbassadorServiceType "LoadBalancer"
 # I forgot to generate and that's why it was rough
 # ks generate core kubeflow-core --name=kubeflow-core --cloud=gke
@@ -26,7 +26,7 @@ ks apply gke -c spark-operator --verbose
 
 ks generate spark-job spark-pi --name=spark-operator --applicationResource="local:///opt/spark/examples/jars/spark-examples_2.11-2.3.1.jar" --mainClass=org.apache.spark.examples.SparkPi
 
-ks generate spark-job spark-wc --name=spark-operator --applicationResource="local:///opt/spark/examples/jars/spark-examples_2.11-2.3.1.jar" --mainClass=org.apache.spark.examples.DFSReadWriteTest --jobArguments="/var/log/syslog,gs://boo-stuff/temp_out_abc"
+ks generate spark-job spark-wc --name=spark-operator --applicationResource="local:///opt/spark/examples/jars/spark-examples_2.11-2.3.1.jar" --mainClass=org.apache.spark.examples.DFSReadWriteTest --jobArguments="/opt/spark/README.md,gs://boo-stuff/temp_out_abc"
 
 ks apply gke -c spark-pi -c spark-wc
 
