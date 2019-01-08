314144c8b96a6e48e56a6cfa7129211417909e1d
cheyang <cheyang@163.com>
2019-01-08 15:48:18 +0800

    update comment

--- scripts/util.sh
+++ scripts/util.sh
@@ -136,7 +136,7 @@ customizeKsAppWithDockerImage() {
   fi
 
   # The katib images like gcr.io/kubeflow-images-public/katib/tfevent-metrics-collector:v0.4.0 uses sub namespace kubeflow-images-public/katib in 
-  # gcr.io repo, but it's supported by other docker image repo. We need to consider how to support it in other docker repos.
+  # gcr.io repo, but it's not supported by other docker image repo. We need to consider how to support it in other docker repos.
   if [[ ! -z "$DOCKER_REGISTRY_KATIB_NAMESPACE" ]]; then
     find ${KUBEFLOW_KS_DIR} -name "*.libsonnet" -o -name "*.jsonnet" | xargs sed -i -e "s%kubeflow-images-public/katib%$DOCKER_REGISTRY_KATIB_NAMESPACE%g"
   fi
