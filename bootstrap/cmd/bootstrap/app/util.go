1c11e79d4225701df313421bdf3aebc8f9c2d826
Lun-Kai Hsu <lunkai@google.com>
2019-01-30 12:34:48 -0800

    bootstrapper install istio (#2315)
    * bootstraper install istio
    
    * fix
    
    * fix
    
    * various fix
    
    * update istio manifest
    
    * make configureCluster create the admin role binding

---
+++ bootstrap/cmd/bootstrap/app/util.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"io"
+	"os"
+)
+
+func copyFile(src, dst string) error {
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	return err
+}
