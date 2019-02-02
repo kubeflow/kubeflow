f6d6c20f6b396378198685a3a46568b1d3ec7358
IronPan <yangpa@google.com>
2019-02-02 10:51:12 -0800

    revert the change to delete storage DM

--- scripts/kfctl.sh
+++ scripts/kfctl.sh
@@ -497,7 +497,6 @@ main() {
         if [[ -d "${KUBEFLOW_DM_DIR}" ]]; then
           pushd ${KUBEFLOW_DM_DIR}
           ${DIR}/gke/delete_deployment.sh --project=${PROJECT} --deployment=${DEPLOYMENT_NAME} --zone=${ZONE}
-          ${DIR}/gke/delete_deployment.sh --project=${PROJECT} --deployment="${DEPLOYMENT_NAME}-storage" --zone=${ZONE}
           popd
         fi
       fi
