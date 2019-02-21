96759acb4c2492b9e37871cede9507e78b042658
Zahra Abbasi <zahraabbasi@google.com>
2019-02-21 12:39:47 -0800

    added jupyter launch command in Dockerfile and undo changes of start-notebook.sh as it referenced to build kubeflow/components/contrib images

--- components/tensorflow-notebook-image/start-notebook.sh
+++ components/tensorflow-notebook-image/start-notebook.sh
@@ -15,5 +15,10 @@
 
 set -e
 
-jupyter notebook --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+  # launched by JupyterHub, use single-user entrypoint
+  exec /usr/local/bin/start-singleuser.sh $*
+else
+  . /usr/local/bin/start.sh jupyter notebook $*
+fi
 
