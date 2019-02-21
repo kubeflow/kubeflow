55286257230dc54acecc24b66be4840f7a5b788c
Gabriel Wen <gabrielwen@google.com>
2019-02-21 13:55:37 -0800

    add config types

---
+++ bootstrap/config/types.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+)
+
+type NameValue struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type Parameters map[string][]NameValue
+
+type Config struct {
+	Repo string `json:"repo,omitempty"`
+	Components []string `json:"components,omitempty"`
+	ProtoParams Parameters `json:"protoParams,omitempty"`
+	CompParams Parameters `json:"compParams,omityempty"`
+	Platform []string `json:"platform,omitempty"`
+}
