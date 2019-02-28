2200316113cb4b86e1ae16325286b4bed322f678
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-02-28 16:34:33 -0500

    Add ESLint to enforce Google JS style in public/ (#2579)
    * Add ESLint to enforce Google JS style in public/
    
    * Switch to 4 space tabs

--- components/centraldashboard/public/components/main-page.js
+++ components/centraldashboard/public/components/main-page.js
@@ -1,4 +1,4 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
 
 import '@polymer/app-layout/app-drawer/app-drawer.js';
 import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
@@ -27,72 +27,88 @@ import '@polymer/neon-animation/animations/fade-out-animation.js';
 import css from './main-page.css';
 import template from './main-page.pug';
 
+/**
+ * Entry point for application UI.
+ */
 export class MainPage extends PolymerElement {
-
     static get template() {
         return html([`<style>${css.toString()}</style> ${template()}`]);
     }
 
-    /**
-      * Object describing property-related metadata used by Polymer features
-      */
     static get properties() {
         return {
             links: {
                 type: Array,
                 value: [
-                    { text: 'Home', defaultPage: true, hasDivider: true },
-                    { link: 'https://www.kubeflow.org/docs/about/kubeflow/', text: 'Kubeflow docs' },
-                    { link: '/jupyter/', text: 'JupyterHub' },
-                    { link: '/tfjobs/ui/', text: 'TFJob Dashboard' },
-                    { link: '/katib/', text: 'Katib Dashboard' },
-                    { link: '/pipeline/', text: 'Pipeline Dashboard' },
-                ]
+                    {text: 'Home', defaultPage: true, hasDivider: true},
+                    {
+                        link: 'https://www.kubeflow.org/docs/about/kubeflow/',
+                        text: 'Kubeflow docs',
+                    },
+                    {link: '/jupyter/', text: 'JupyterHub'},
+                    {link: '/tfjobs/ui/', text: 'TFJob Dashboard'},
+                    {link: '/katib/', text: 'Katib Dashboard'},
+                    {link: '/pipeline/', text: 'Pipeline Dashboard'},
+                ],
             },
             gettingStartedItems: {
                 type: Array,
                 value: [
-                    { text: 'Build a model in a notebook', icon: 'donut-large' },
-                    { text: 'Launch a pipeline', icon: 'donut-large' },
-                    { text: 'Deploy a solution', icon: 'donut-large' },
-                    { text: 'Compare run output', icon: 'donut-large' },
-                    { text: 'Share solution on AI Hub', icon: 'donut-large' },
-                ]
+                    {text: 'Build a model in a notebook', icon: 'donut-large'},
+                    {text: 'Launch a pipeline', icon: 'donut-large'},
+                    {text: 'Deploy a solution', icon: 'donut-large'},
+                    {text: 'Compare run output', icon: 'donut-large'},
+                    {text: 'Share solution on AI Hub', icon: 'donut-large'},
+                ],
             },
             quickLinks: {
                 type: Array,
                 value: [
-                    { text: 'Open docs', link: 'https://www.kubeflow.org/docs/started/getting-started/' },
-                    { text: 'Open Github', link: 'https://github.com/kubeflow/kubeflow' },
-                ]
+                    {
+                        text: 'Open docs',
+                        link: 'https://www.kubeflow.org/docs/started/getting-started/',
+                    },
+                    {
+                        text: 'Open Github',
+                        link: 'https://github.com/kubeflow/kubeflow',
+                    },
+                ],
             },
-            sidebarItemIndex: { type: Number, value: 0 },
-            primaryViewIndex: { type: Number, value: 0 },
-            homeOrIframeViewIndex: { type: Number, value: 0 },
-            url: { type: String, value: "" },
-            buildVersion: { type: String, value: VERSION},
-            _devMode: { type: Boolean, value: false },
-        }
-    };
+            sidebarItemIndex: {type: Number, value: 0},
+            primaryViewIndex: {type: Number, value: 0},
+            homeOrIframeViewIndex: {type: Number, value: 0},
+            url: {type: String, value: ''},
+            buildVersion: {type: String, value: VERSION},
+            _devMode: {type: Boolean, value: false},
+        };
+    }
 
     openExternalLink(href) {
-        const a = document.createElement('a')
-        a.href = href
-        a.target = '_blank'
-        a.click()
+        const a = document.createElement('a');
+        a.href = href;
+        a.target = '_blank';
+        a.click();
     }
+
     openQuickLink(e) {
         const link = e.model.item.link;
-        this.openExternalLink(link)
+        this.openExternalLink(link);
     }
+
     openLink(e) {
-        const { link, defaultPage } = e.model.item
-        this.homeOrIframeViewIndex = defaultPage ? 0 : 1
-        if (defaultPage) return
-        this.url = link
+        const {link, defaultPage} = e.model.item;
+        this.homeOrIframeViewIndex = defaultPage ? 0 : 1;
+        if (defaultPage) return;
+        this.url = link;
     }
-    toggleSidebar() { this.$.MainDrawer.toggle() }
-    isZero(i) { return i === 0 }
+
+    toggleSidebar() {
+        this.$.MainDrawer.toggle();
+    }
+
+    isZero(i) {
+        return i === 0;
+    }
 }
 
 window.customElements.define('main-page', MainPage);
