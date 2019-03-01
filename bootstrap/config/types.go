04ee1ab3149b3ad9f2d5524ac4dab7b8d9eac8b6
Gabriel Wen <gabrielwen@google.com>
2019-02-28 16:06:42 -0800

    doc

--- bootstrap/config/types.go
+++ bootstrap/config/types.go
@@ -13,9 +13,14 @@ type Parameters map[string][]NameValue
 type Config struct {
 	// Name of repository.
 	Repo        string     `json:"repo,omitempty"`
+	// List of default components.
 	Components  []string   `json:"components,omitempty"`
+	// List of default packages.
 	Packages    []string   `json:"packages,omitempty"`
+	// Parameters to be passed into component prototypes.
 	ProtoParams Parameters `json:"protoParams,omitempty"`
+	// Parameters to be set to components using ks param set.
 	CompParams  Parameters `json:"compParams,omityempty"`
+	// Platform type.
 	Platform    string     `json:"platform,omitempty"`
 }
