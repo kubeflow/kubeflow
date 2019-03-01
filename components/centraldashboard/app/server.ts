07eecd2f989a57ecd746f6b8b0d8b2fc0dff5306
Apoorv Verma [AP] <avdaredevil@gmail.com>
2019-03-01 00:57:27 -0800

    TS Changes

--- components/centraldashboard/app/server.ts
+++ components/centraldashboard/app/server.ts
@@ -1,9 +1,9 @@
-import express from "express"
-import {resolve} from "path";
-import process from "process"
+import express from "express";
+import { resolve } from "path";
+import process from "process";
 
 const app: express.Application = express();
-const {PORT, PORT_1} = process.env
+const {PORT, PORT_1} = process.env;
 const port: number = Number(PORT) || Number(PORT_1) || 8082;
 const frontEnd: string = resolve(__dirname, "public");
 
