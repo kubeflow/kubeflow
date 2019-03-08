aec73d7e79e61ddcfc31fa240d2d8a0b52f8835d
Jeremy Lewi <jeremy@lewi.us>
2019-03-08 09:17:09 -0800

    A script to reset the basic auth secret.

---
+++ scripts/create_password_secret.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Helper script to create a secret containing the passord
+set -ex
+user=$1
+
+# bcrypt encoded password
+password=$2
+
+PASSWORDB64=`echo "${password}" | base64`
+kubectl -n kubeflow create secret generic kubeflow-login --from-literal=username=${USER} --from-literal=passwordhash=${PASSWORDB64}
+
+# Delete the pod to load the new secret
+kubectl -n kubeflow delete pods -l app=basic-auth-login
