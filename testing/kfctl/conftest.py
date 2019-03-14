59a3d3dc7eacfc49771c41bde5c25ec940349d4c
Jeremy Lewi <jeremy@lewi.us>
2019-03-14 11:28:44 -0700

    Improve logging in kfctl with go binary    * Log the name and status of GCP DM operations    * Print out DM creation errors so users see things like quota issues.
    Start updating the E2E test to actually deploy Kubeflow.

---
+++ testing/kfctl/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+def pytest_addoption(parser):
+  parser.addoption(
+      "--app_path", action="store", default="",
+      help="Path where the KF application should be stored")
+
+  parser.addoption(
+      "--project", action="store", default="kubeflow-ci-deployment",
+      help="GCP project to deploy Kubeflow to")
+
+@pytest.fixture
+def app_path(request):
+  return request.config.getoption("--app_path")
+
+@pytest.fixture
+def project(request):
+  return request.config.getoption("--project")
--- testing/kfctl/kfctl_go_test.py
+++ testing/kfctl/kfctl_go_test.py
@@ -1,10 +1,17 @@
 import datetime
 import logging
 import os
+<<<<<<< HEAD
 import tempfile
+=======
+import subprocess
+import requests
+>>>>>>> b550a275... Improve logging in kfctl with go binary
 import uuid
 from retrying import retry
 
+import datetime
+import tempfile
 import pytest
 
 from kubeflow.testing import util
@@ -15,11 +22,29 @@ from kubeflow.testing import util
 def build(build_dir):
   util.run(["make", "build-kfctl"], cwd=build_dir)
 
-def test_build_kfctl_go():
+def test_build_kfctl_go(app_path, project):
+  if not app_path:
+    logging.info("--app_path not specified")
+    stamp = datetime.datetime.now().strftime("%H%M")
+    app_path = os.path.join(tempfile.gettempdir(),
+                            "kfctl-{0}-{1}".format(stamp,
+                                                   uuid.uuid4().hex[0:4]))
+  logging.info("Using app path %s", app_path)
   this_dir = os.path.dirname(__file__)
   root = os.path.abspath(os.path.join(this_dir, "..", ".."))
   build_dir = os.path.join(root, "bootstrap")
   build(build_dir)
+
+  kfctl_path = os.path.join(build_dir, "bin", "kfctl")
+
+  util.run([kfctl_path, "init", app_path, "-V", "--platform=gcp",
+            "--use_basic_auth", "--skip-init-gcp-project",
+            "--project=" + project])
+
+  util.run([kfctl_path, "generate", "-V", "all", "--email=test@kubeflow.org"],
+            cwd=app_path)
+
+  util.run([kfctl_path, "apply", "-V", "all"], cwd=app_path)
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO,
