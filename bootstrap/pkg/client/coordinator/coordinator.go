de3c90ba17c1d4c89e1c3573b1ebe16dc4715648
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-03-15 12:39:57 -0700

    fix on unmarshal

--- bootstrap/pkg/client/coordinator/coordinator.go
+++ bootstrap/pkg/client/coordinator/coordinator.go
@@ -259,14 +259,14 @@ and must start and end with an alphanumeric character`, appName)
 	return pApp, nil
 }
 
-func unmarshalAppYaml(cfgfile string, spec *cltypes.ClientSpec) error {
+func unmarshalAppYaml(cfgfile string, client *cltypes.Client) error {
 	if _, err := os.Stat(cfgfile); err == nil {
 		log.Infof("reading from %v", cfgfile)
 		buf, bufErr := ioutil.ReadFile(cfgfile)
 		if bufErr != nil {
 			return fmt.Errorf("couldn't read %v. Error: %v", cfgfile, bufErr)
 		}
-		err := yaml.Unmarshal(buf, spec)
+		err := yaml.Unmarshal(buf, client)
 		if err != nil {
 			return fmt.Errorf("could not unmarshal %v. Error: %v", cfgfile, err)
 		}
@@ -290,7 +290,7 @@ func LoadKfApp(options map[string]interface{}) (kftypes.KfApp, error) {
 		Spec: cltypes.ClientSpec{
 		},
 	}
-	err = unmarshalAppYaml(cfgfile, &client.Spec)
+	err = unmarshalAppYaml(cfgfile, client)
 	if err != nil {
 		return nil, fmt.Errorf("could not unmarshal %v. Error: %v", cfgfile, err)
 	}
