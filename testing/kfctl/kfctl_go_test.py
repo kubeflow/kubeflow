e1061a0db2cb2e2a6897df81340befa3325d00a4
Jeremy Lewi <jeremy@lewi.us>
2019-03-14 20:58:34 -0700

    Fix test.

--- testing/kfctl/kfctl_go_test.py
+++ testing/kfctl/kfctl_go_test.py
@@ -16,13 +16,6 @@ def build(build_dir):
   util.run(["make", "build-kfctl"], cwd=build_dir)
 
 def test_build_kfctl_go():
-  if not app_path:
-    logging.info("--app_path not specified")
-    stamp = datetime.datetime.now().strftime("%H%M")
-    app_path = os.path.join(tempfile.gettempdir(),
-                            "kfctl-{0}-{1}".format(stamp,
-                                                   uuid.uuid4().hex[0:4]))
-  logging.info("Using app path %s", app_path)
   this_dir = os.path.dirname(__file__)
   root = os.path.abspath(os.path.join(this_dir, "..", ".."))
   build_dir = os.path.join(root, "bootstrap")
