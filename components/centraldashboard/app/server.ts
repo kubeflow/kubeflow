f6f7a1a66109463b9a9c9cc7c06f1814d692c81f
Jason Prodonovich <prodonjs@google.com>
2019-03-17 22:41:29 -0400

    Add Namespaces and Activities from API server. Adds error handling and logging for K8s service calls. Seperate activities-list from activity-view and mirror mock styles

--- components/centraldashboard/app/server.ts
+++ components/centraldashboard/app/server.ts
@@ -1,51 +1,22 @@
-import express from "express";
-import { resolve } from "path";
-import process from "process";
+import {KubeConfig} from '@kubernetes/client-node';
+import express from 'express';
+import {resolve} from 'path';
 
+import {api} from './api';
+
+const {PORT_1} = process.env;  // PORT_1 was defined in an earlier version
+const port: number = Number(PORT_1) || 8082;
+
+const frontEnd: string = resolve(__dirname, 'public');
 const app: express.Application = express();
-const {PORT, PORT_1} = process.env;
-const port: number = Number(PORT) || Number(PORT_1) || 8082;
-const frontEnd: string = resolve(__dirname, "public");
-
-interface Activity {
-    time: Date;
-    event: string;
-    isError: boolean;
-    source: string;
-}
-const actvities = _generateActivities();
-function _generateActivities(): Activity[] {
-    const activities: Activity[] = [];
-    const systems: string[] = [
-        "serving-system",
-        "gpu-system",
-        "training-system"
-    ];
-    const now = Date.now();
-    for (let i = 0; i < 100; i++) {
-        activities.push({
-            time: new Date(now - (Math.random() * 86400000)),
-            event: `Event #${i}`,
-            isError: Math.random() * 10 <= 1, // 1/10 probability
-            source: systems[Math.floor(Math.random() * 3)]
-        });
-    }
-    return activities.sort((a, b) => b.time.getTime() - a.time.getTime());
-}
+const kubeConfig = new KubeConfig();
 
 app.use(express.json());
 app.use(express.static(frontEnd));
-
-app.get("/api", (req: express.Request, res: express.Response) => {
-    console.info(`Request ${req.url} received`);
-    res.send("Hello World");
+app.use('/api', api(kubeConfig));
+app.get('/*', (_: express.Request, res: express.Response) => {
+  res.sendFile(resolve(frontEnd, 'index.html'));
 });
-app.get("/api/activities", (req: express.Request, res: express.Response) => {
-    res.send(actvities);
-});
-app.get("/*", (req: express.Request, res: express.Response) => {
-    res.sendFile(resolve(frontEnd, "index.html"));
-});
-
-app.listen(port,
+app.listen(
+    port,
     () => console.info(`Server listening on port http://localhost:${port}`));
