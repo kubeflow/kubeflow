7b82955287e9e8ebfa1a20a1bf8c0028f19d85be
Gabriel Wen <gabrielwen@google.com>
2019-03-21 10:41:21 -0700

    remove unused codes

--- bootstrap/pkg/kfapp/ksonnet/ksonnet.go
+++ bootstrap/pkg/kfapp/ksonnet/ksonnet.go
@@ -33,7 +33,6 @@ import (
 	"io/ioutil"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	"os"
@@ -247,114 +246,7 @@ func (ksApp *ksApp) components() (map[string]*kfdefs.KsComponent, error) {
 	return comps, nil
 }
 
-func (ksApp *ksApp) deleteGlobalResources(config *rest.Config) error {
-	apiextclientset := kftypes.GetApiExtClientset(config)
-	do := &metav1.DeleteOptions{}
-	lo := metav1.ListOptions{
-		LabelSelector: kftypes.DefaultAppLabel + "=" + ksApp.Name,
-	}
-	crdsErr := apiextclientset.CustomResourceDefinitions().DeleteCollection(do, lo)
-	if crdsErr != nil {
-		return fmt.Errorf("couldn't delete customresourcedefinitions Error: %v", crdsErr)
-	}
-	crdsByName := []string{
-		"compositecontrollers.metacontroller.k8s.io",
-		"controllerrevisions.metacontroller.k8s.io",
-		"decoratorcontrollers.metacontroller.k8s.io",
-		"applications.app.k8s.io",
-	}
-	for _, crd := range crdsByName {
-		do := &metav1.DeleteOptions{}
-		dErr := apiextclientset.CustomResourceDefinitions().Delete(crd, do)
-		if dErr != nil {
-			log.Errorf("could not delete %v Error %v", crd, dErr)
-		}
-	}
-	clientset := kftypes.GetClientset(config)
-	crbsErr := clientset.RbacV1().ClusterRoleBindings().DeleteCollection(do, lo)
-	if crbsErr != nil {
-		return fmt.Errorf("couldn't get list of clusterrolebindings Error: %v", crbsErr)
-	}
-	crbName := "meta-controller-cluster-role-binding"
-	dErr := clientset.RbacV1().ClusterRoleBindings().Delete(crbName, do)
-	if dErr != nil {
-		log.Errorf("could not delete %v Error %v", crbName, dErr)
-	}
-	crsErr := clientset.RbacV1().ClusterRoles().DeleteCollection(do, lo)
-	if crsErr != nil {
-		return fmt.Errorf("couldn't delete clusterroles Error: %v", crsErr)
-	}
-	return nil
-}
-
 func (ksApp *ksApp) Delete(resources kftypes.ResourceEnum) error {
-	// ctx := context.Background()
-	// var config *rest.Config
-	// if ksApp.Spec.Platform == "gcp" {
-	// 	c, err := kfctlutils.BuildConfigForGcp(ctx, ksApp.Spec.Project, ksApp.Spec.Zone, ksApp.Name)
-	// 	if err != nil {
-	// 		return fmt.Errorf("Error getting rest.Config info for %v/%v/%v: %v",
-	// 			ksApp.Spec.Project, ksApp.Spec.Zone, ksApp.Name, err)
-	// 	}
-	// 	config = c
-	// } else {
-	// 	config = kftypes.GetConfig()
-	// }
-	// err := ksApp.deleteGlobalResources(config)
-	// if err != nil {
-	// 	log.Errorf("there was a problem deleting global resources: %v", err)
-	// }
-
-	// envSetErr := ksApp.envSet(ksApp.KsEnvName, config.Host)
-	// if envSetErr != nil {
-	// 	return fmt.Errorf("couldn't create ksonnet env %v Error: %v", ksApp.KsEnvName, envSetErr)
-	// }
-
-	// namespace := ksApp.ObjectMeta.Namespace
-	// var clientConfig *clientcmdapi.Config
-	// if ksApp.Spec.Platform == "gcp" {
-	// 	c, err := kfctlutils.CreateKubeconfig(ctx, ksApp.Spec.Project, ksApp.Spec.Zone, ksApp.Name, namespace)
-	// 	if err != nil {
-	// 		return fmt.Errorf("couldn't generate kubeconfig for GCP: %v", err)
-	// 	}
-	// 	clientConfig = c
-	// } else {
-	// 	clientConfig = kftypes.GetKubeConfig()
-	// }
-	// components := []string{"application", "metacontroller"}
-	// err = actions.RunDelete(map[string]interface{}{
-	// 	actions.OptionApp: ksApp.KApp,
-	// 	actions.OptionClientConfig: &client.Config{
-	// 		Overrides: &clientcmd.ConfigOverrides{},
-	// 		Config:    clientcmd.NewDefaultClientConfig(*clientConfig, &clientcmd.ConfigOverrides{}),
-	// 	},
-	// 	actions.OptionEnvName:        ksApp.KsEnvName,
-	// 	actions.OptionComponentNames: components,
-	// 	actions.OptionGracePeriod:    int64(10),
-	// })
-	// if err != nil {
-	// 	log.Infof("there was a problem deleting %v: %v", components, err)
-	// }
-	// log.Infof("try to delete namespace: %v", namespace)
-	// clientset := kftypes.GetClientset(config)
-	// ns, nsMissingErr := clientset.CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
-	// if nsMissingErr == nil {
-	// 	log.Infof("namespace %v is found, deleting", namespace)
-	// 	nsErr := clientset.CoreV1().Namespaces().Delete(ns.Name, metav1.NewDeleteOptions(int64(100)))
-	// 	if nsErr != nil {
-	// 		return fmt.Errorf("couldn't delete namespace %v Error: %v", namespace, nsErr)
-	// 	}
-	// }
-
-	// name := "meta-controller-cluster-role-binding"
-	// log.Infof("deleting %v", name)
-	// crb, crbErr := clientset.RbacV1().ClusterRoleBindings().Get(name, metav1.GetOptions{})
-	// if crbErr == nil {
-	// 	crbDeleteErr := clientset.RbacV1().ClusterRoleBindings().Delete(crb.Name, metav1.NewDeleteOptions(int64(5)))
-	// 	if crbDeleteErr != nil {
-	// 		return fmt.Errorf("couldn't delete clusterrolebinding %v Error: %v", name, crbDeleteErr)
-	// 	}
-	// }
 	return nil
 }
 
