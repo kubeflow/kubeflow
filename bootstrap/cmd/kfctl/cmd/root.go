15aad9bf896a4c789909cf4d7af41d4819955bbc
Kam Kasravi <kamkasravi@yahoo.com>
2019-04-22 09:41:44 -0700

    Value of kfctl version is hardcoded and not representative of the actual build (#2988)
    * Value of kfctl version is hardcoded and not representative of the actual build
    
    * remove install of upx (was going to be used to reduce exe size). Do this under a separate issue. Update TAG format

--- bootstrap/cmd/kfctl/cmd/root.go
+++ bootstrap/cmd/kfctl/cmd/root.go
@@ -48,9 +48,16 @@ var rootCmd = &cobra.Command{
 to an existing k8 cluster.`,
 }
 
+var (
+	// VERSION is set during build
+	VERSION string
+)
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(version string) {
+	VERSION = version
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
