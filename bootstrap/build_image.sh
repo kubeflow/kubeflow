605fc0b348eb33b7e72d001d604a1b8a9feecd11
Lun-Kai Hsu <lunkai@google.com>
2019-04-26 11:21:43 -0700

    fix build image

--- bootstrap/build_image.sh
+++ bootstrap/build_image.sh
@@ -19,16 +19,29 @@ done
 
 go version
 
-GO111MODULE=on 
+#GO111MODULE=on 
 #go build -gcflags 'all=-N -l' -o bin/bootstrapper cmd/bootstrap/main.go
-make build-bootstrap
+#make build-bootstrap
+
+# rm -rf reg_tmp
+# mkdir -p reg_tmp/kubeflow
+# cp -r ../kubeflow reg_tmp/kubeflow
+# cp -r ../deployment reg_tmp/kubeflow
+# cp -r ../dependencies reg_tmp/kubeflow
+# docker build -t ${IMAGE}:$TAG --build-arg registries=reg_tmp --target=bootstrap .
 
 rm -rf reg_tmp
 mkdir -p reg_tmp/kubeflow
 cp -r ../kubeflow reg_tmp/kubeflow
 cp -r ../deployment reg_tmp/kubeflow
 cp -r ../dependencies reg_tmp/kubeflow
-docker build -t ${IMAGE}:$TAG --build-arg registries=reg_tmp --target=bootstrap .
+mkdir -p reg_tmp/kubeflow/bootstrap
+cp -r k8sSpec reg_tmp/kubeflow/bootstrap
+docker build \
+  --build-arg registries=reg_tmp \
+  --target=bootstrap \
+  --tag $(IMAGE):$(TAG) .
+@echo Built $(IMAGE):$(TAG)
 
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud docker -- push "${IMAGE}:${TAG}"
