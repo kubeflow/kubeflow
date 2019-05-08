ed872747d3622596e2522123e8583ea7e752fe20
Gabriel Wen <gabrielwen@google.com>
2019-05-08 11:17:19 -0700

    fix invalid access

--- components/jupyter-web-app/kubeflow_jupyter/default/app.py
+++ components/jupyter-web-app/kubeflow_jupyter/default/app.py
@@ -50,11 +50,11 @@ def post_notebook(namespace):
   utils.set_notebook_cpu_ram(notebook, body)
 
   # Enable SHM
-  if body["shm_enable"] == "1":
+  if body.get("shm_enable", "") == "1":
       utils.enable_shm(notebook)
 
   # Workspace Volume
-  if body["ws_type"] == "New":
+  if body.get("ws_type", "") == "New":
     try:
       api.create_workspace_pvc(body)
     except ApiException as e:
