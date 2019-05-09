1a83f3f7a528f42ca1bb8ee43c34c3848a2fab75
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-05-08 20:25:09 -0700

    fix for erroring on paramName

--- bootstrap/v2/pkg/kfapp/kustomize/kustomize.go
+++ bootstrap/v2/pkg/kfapp/kustomize/kustomize.go
@@ -625,11 +625,6 @@ func MergeKustomization(compDir string, targetDir string, kfDef *cltypes.KfDef, 
 						params[i] = paramName + "=" + kfDef.Namespace
 					case "project":
 						params[i] = paramName + "=" + kfDef.Spec.Project
-					default:
-						return &kfapis.KfError{
-							Code:    int(kfapis.INTERNAL_ERROR),
-							Message: fmt.Sprintf("could not resolve %v in %v", paramName, paramFile),
-						}
 					}
 				}
 			}
