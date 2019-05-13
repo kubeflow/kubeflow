bbc76e832a892db82229359fa81536c2e60ace2b
Jason Prodonovich <prodonjs@google.com>
2019-05-13 16:39:41 -0400

    Proof-of-concept implementation with Stackdriver.
    - Uses Stackdriver Monitoring API as a implementation of a
      MetricsService interface that could be extended for other platforms
      that can be used to obtain time-series metrics for resource utilization.

--- components/centraldashboard/app/api.ts
+++ components/centraldashboard/app/api.ts
@@ -1,11 +1,12 @@
-import {KubeConfig} from '@kubernetes/client-node';
 import express from 'express';
 
 import {KubernetesService} from './k8s_service';
+import {Interval, MetricsService} from './metrics_service';
 
 /** API routes exposing information from Kubernetes */
-export function api(kubeConfig: KubeConfig): express.Router {
-  const k8sService = new KubernetesService(kubeConfig);
+export function api(
+    k8sService: KubernetesService,
+    metricsService?: MetricsService): express.Router {
   return express.Router()
       .get(
           '/platform-info',
@@ -16,6 +17,31 @@ export function api(kubeConfig: KubeConfig): express.Router {
           '/namespaces',
           async (_: express.Request, res: express.Response) => {
             res.json(await k8sService.getNamespaces());
+          })
+      .get(
+          '/metrics/:type((node|podcpu|podmem))',
+          async (req: express.Request, res: express.Response) => {
+            let interval = Interval.Last15m;
+            if (Interval[req.query.interval] !== undefined) {
+              interval = Number(Interval[req.query.interval]);
+            }
+            if (metricsService) {
+              switch (req.params.type) {
+                case 'node':
+                  res.json(
+                      await metricsService.getNodeCpuUtilization(interval));
+                  break;
+                case 'podcpu':
+                  res.json(await metricsService.getPodCpuUtilization(interval));
+                  break;
+                case 'podmem':
+                  res.json(await metricsService.getPodMemoryUsage(interval));
+                  break;
+                default:
+              }
+            } else {
+              res.sendStatus(405);
+            }
           })
       .get(
           '/activities/:namespace',
