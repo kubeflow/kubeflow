a8140138bd03dadc88111caec086e25c4c04e89d
Apoorv Verma [AP] <avdaredevil@gmail.com>
2019-05-21 18:08:48 -0400

    Dynamic icon generated svg_set now created, kubeflow logo will not be in it as it's not a square

--- components/centraldashboard/public/components/resources/kubeflow-icons.js
+++ components/centraldashboard/public/components/resources/kubeflow-icons.js
@@ -6,7 +6,20 @@ import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-iconset-svg/iron-iconset-svg.js';
 
 import {html, htmlLiteral} from '@polymer/polymer/lib/utils/html-tag.js';
-import bolt from '../../assets/icons/ic_bolt.svg';
+
+/**
+ * Converts an imported folder into a Object.entries() type of
+ *  [[key, val], ...] list.
+ * @param {object} context
+ * @return {[...[string, string]]}
+ */
+function createFileMap(context) {
+    const keys = context.keys();
+    const values = keys.map(context);
+    const _v = (v) => stripSVG(v.default || v);
+    const _name = (n) => n.split(/\/|\\/).slice(-1)[0].replace('.svg', '');
+    return keys.map((k, i) => [_name(k), _v(values[i])]);
+}
 
 /**
  * Takes SVG source returns its contents.
@@ -16,14 +29,23 @@ import bolt from '../../assets/icons/ic_bolt.svg';
 function stripSVG(svg) {
     const content = svg
         .replace(/\r?\n|(?=>)\s+(?=<)/g, ' ')
-        .replace(/.*?<svg.*?>(.+?)<\/svg>/, '$1');
+        .replace(/^.*?<svg.*?>(.+?)<\/svg>.*$/, '$1');
     if (content == svg) throw Error('Invalid SVG data');
-    return htmlLiteral([content]);
+    return content;
 }
+
+// Uses Webpack specific syntax to require all favicons
+// eslint-disable-next-line no-undef
+const importData = require.context('../../assets/icons', true, /\.svg$/);
+const fileData = createFileMap(importData).map(([name, data]) =>
+    `<g id="${name}">${data}</g>`
+).join('');
+// eslint-disable-next-line no-console
+console.log(createFileMap(importData));
 
 const template = html`<iron-iconset-svg name="kubeflow" size="24">
 <svg><defs>
-<g id="bolt">${stripSVG(bolt)}</g>
+${htmlLiteral([fileData])}
 </defs></svg>
 </iron-iconset-svg>`;
 
