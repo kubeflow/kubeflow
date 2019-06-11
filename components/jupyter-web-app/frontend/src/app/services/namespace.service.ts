7393d29fe3692d721fa64f1540546da27de66497
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2019-06-11 14:08:03 +0300

    Communicate with Centraldashboard
    * The NamespaceService will listen for namespace changes from
    the centraldashboard
    * Hide the namespace selector if inside an iframe
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/jupyter-web-app/frontend/src/app/services/namespace.service.ts
+++ components/jupyter-web-app/frontend/src/app/services/namespace.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { ReplaySubject, Subscription, fromEvent } from "rxjs";
+
+declare global {
+  interface Window {
+    centraldashboard: any;
+  }
+}
 
 @Injectable()
 export class NamespaceService {
   // Observable string sources
-  private selectedNamespaceSource = new BehaviorSubject<string>("kubeflow");
+  private selectedNamespaceSource = new ReplaySubject<string>();
 
   // Observable string streams
   selectedNamespace$ = this.selectedNamespaceSource.asObservable();
+
+  constructor() {
+    fromEvent(window, "DOMContentLoaded").subscribe(_ => {
+      if (
+        window.centraldashboard &&
+        window.centraldashboard.CentralDashboardEventHandler
+      ) {
+        // Init method will invoke the callback with the event handler instance
+        // and a boolean indicating whether the page is iframed or not
+        window.centraldashboard.CentralDashboardEventHandler.init(
+          (cdeh, isIframed) => {
+            // Binds a callback that gets invoked anytime the Dashboard's
+            // namespace is changed
+            cdeh.onNamespaceSelected = this.updateSelectedNamespace.bind(this);
+          }
+        );
+      }
+    });
+  }
 
   // GETers
   getSelectedNamespace() {
@@ -17,5 +42,12 @@ export class NamespaceService {
   // Service message commands
   updateSelectedNamespace(namespace: string) {
     this.selectedNamespaceSource.next(namespace);
+  }
+
+  dashboardConnected() {
+    return (
+      window.parent.location.pathname !== window.location.pathname &&
+      window.centraldashboard
+    );
   }
 }
