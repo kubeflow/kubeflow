fd02effa818ae36b2700c7feff7a186858ad5350
adselvar <adhita94@gmail.com>
2019-06-20 12:32:29 -0700

    set kfctl version for tests from kubeflow/kubeflow

--- testing/kfctl/kfctl_go_test.py
+++ testing/kfctl/kfctl_go_test.py
@@ -83,8 +83,9 @@ def test_build_kfctl_go(app_path, project, use_basic_auth, use_istio):
     init_args.append("--use_istio=false")
 
   version = "master"
-  if os.getenv("PULL_NUMBER"):
-    version = "pull/{0}".format(os.getenv("PULL_NUMBER"))
+  if os.getenv("REPO_NAME") != "manifests":
+    if os.getenv("PULL_NUMBER"):
+      version = "pull/{0}".format(os.getenv("PULL_NUMBER"))
   pull_sha = "@0caa70b4518859c0678d0d0e12c11a7e35345c5a"
   if os.getenv("REPO_NAME") == "manifests":
     if os.getenv("PULL_PULL_SHA"):
