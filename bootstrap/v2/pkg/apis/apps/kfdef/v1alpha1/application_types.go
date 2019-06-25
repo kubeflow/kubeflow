b07eaf04a875724e234cfecdcfc7e691732a62d6
Jeremy Lewi <jeremy@lewi.us>
2019-06-25 15:36:53 -0700

    Remove pluginparameters and some other code.

--- bootstrap/v2/pkg/apis/apps/kfdef/v1alpha1/application_types.go
+++ bootstrap/v2/pkg/apis/apps/kfdef/v1alpha1/application_types.go
@@ -17,16 +17,13 @@ package v1alpha1
 import (
 	"fmt"
 	"github.com/ghodss/yaml"
-	gogetter "github.com/hashicorp/go-getter"
 	"github.com/kubeflow/kubeflow/bootstrap/config"
-	kfapis "github.com/kubeflow/kubeflow/bootstrap/pkg/apis"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"k8s.io/api/v2/core/v1"
 	metav1 "k8s.io/apimachinery/v2/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/v2/pkg/runtime"
 	"os"
-	"path"
 )
 
 const (
@@ -84,16 +81,9 @@ var DefaultRegistry = RegistryConfig{
 //
 type Plugin struct {
 	Name       string            `json:"name,omitempty"`
-	Parameters []PluginParameter `json:"pluginParameters,omitempty"`
 
 	// TODO(jlewi): Should we be using runtime.Object or runtime.RawExtension
 	Spec *runtime.RawExtension `json:"spec,omitempty"`
-}
-
-type PluginParameter struct {
-	Name      string     `json:"name,omitempty"`
-	Value     string     `json:"value,omitempty"`
-	SecretRef *SecretRef `json:"secretRef,omitempty"`
 }
 
 // SecretRef is a reference to a secret
@@ -274,74 +264,6 @@ type KfDefList struct {
 func GetDefaultRegistry() *RegistryConfig {
 	newReg := DefaultRegistry
 	return &newReg
-}
-
-const DefaultCacheDir = ".cache"
-
-// DownalodAndLoadConfigFile constructs a kfApp given the path to a YAML file
-// specifying a YAML config file.
-// configFile is the path to the YAML file containing the KfDef spec. Can be an URI supported by hashicorp
-// go-getter.
-// appDir is the directory to use as the application dir. It is also the directory to download
-// the config to.
-func DownloadAndLoadConfigFile(configFile string, appDir string) (*KfDef, error) {
-	if configFile == "" {
-		return nil, fmt.Errorf("config file must be the URI of a KfDef spec")
-	}
-
-	if appDir == "" {
-		return nil, fmt.Errorf("appdir must be the directory where the app should be stored")
-	}
-
-	if _, err := os.Stat(appDir); os.IsNotExist(err) {
-		log.Infof("Creating directory %v", appDir)
-		appdirErr := os.MkdirAll(appDir, os.ModePerm)
-		if appdirErr != nil {
-			log.Errorf("couldn't create directory %v Error %v", appDir, appdirErr)
-			return nil, appdirErr
-		}
-	} else {
-		log.Infof("App directory exists %v", appDir)
-	}
-
-	// Open config file
-	//
-	// TODO(jlewi): Should we use hashicorp go-getter.GetAny here? We use that to download
-	// the tarballs for the repos. Maybe we should use that here as well to be consistent.
-	appFile := path.Join(appDir, KfConfigFile)
-
-	if _, err := os.Stat(appFile); err == nil {
-		log.Infof("%v exists not refetching", appFile)
-	} else {
-		log.Infof("Downloading %v to %v", configFile, appFile)
-		err := gogetter.GetFile(appFile, configFile)
-		if err != nil {
-			return nil, &kfapis.KfError{
-				Code:    int(kfapis.INVALID_ARGUMENT),
-				Message: fmt.Sprintf("could not fetch specified config %s: %v", configFile, err),
-			}
-		}
-	}
-	// Read contents
-	configFileBytes, err := ioutil.ReadFile(appFile)
-	if err != nil {
-		return nil, &kfapis.KfError{
-			Code:    int(kfapis.INTERNAL_ERROR),
-			Message: fmt.Sprintf("could not read from config file %s: %v", configFile, err),
-		}
-	}
-	// Unmarshal content onto KfDef struct
-	kfDef := &KfDef{}
-	if err := yaml.Unmarshal(configFileBytes, kfDef); err != nil {
-		return nil, &kfapis.KfError{
-			Code:    int(kfapis.INTERNAL_ERROR),
-			Message: fmt.Sprintf("could not unmarshal config file onto KfDef struct: %v", err),
-		}
-	}
-
-	// appDir should be stored in the resulting KfDef
-	kfDef.Spec.AppDir = appDir
-	return kfDef, nil
 }
 
 // GetSecret returns the specified secret or an error if the secret isn't specified.
