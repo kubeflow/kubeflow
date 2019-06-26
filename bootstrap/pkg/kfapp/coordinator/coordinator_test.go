6e78e4dda6c058d7d9b0d57ba0ce93342db1f597
Jeremy Lewi <jeremy@lewi.us>
2019-06-25 23:07:17 -0700

    Add a unittest for CreateKfAppCfgFile.

--- bootstrap/pkg/kfapp/coordinator/coordinator_test.go
+++ bootstrap/pkg/kfapp/coordinator/coordinator_test.go
@@ -3,12 +3,89 @@ package coordinator
 import (
 	"encoding/json"
 	kftypes "github.com/kubeflow/kubeflow/bootstrap/pkg/apis/apps"
+	kftypesv2 "github.com/kubeflow/kubeflow/bootstrap/v2/pkg/apis/apps"
 	kfdefsv2 "github.com/kubeflow/kubeflow/bootstrap/v2/pkg/apis/apps/kfdef/v1alpha1"
 	config "github.com/kubeflow/kubeflow/bootstrap/config"
+	"io/ioutil"
 	metav1 "k8s.io/apimachinery/v2/pkg/apis/meta/v1"
+	"os"
+	"path"
 	"reflect"
 	"testing"
 )
+
+func Test_CreateKfAppCfgFile(t *testing.T) {
+	type testCase struct {
+		Input    kfdefsv2.KfDef
+		DirExists bool
+		CfgFileExists bool
+		ExpectError  bool
+	}
+
+	cases := []testCase{
+		{
+			Input: kfdefsv2.KfDef{},
+			DirExists: false,
+			CfgFileExists: false,
+			ExpectError: false,
+		},
+		{
+			Input: kfdefsv2.KfDef{},
+			DirExists: true,
+			CfgFileExists: false,
+			ExpectError: false,
+		},
+		{
+			Input: kfdefsv2.KfDef{},
+			DirExists: true,
+			CfgFileExists: true,
+			ExpectError: true,
+		},
+	}
+
+	for _, c := range cases {
+
+		tDir, err := ioutil.TempDir("","")
+
+		if err != nil {
+			t.Fatalf("Could not create temporary directory; %v", err)
+		}
+
+		if !c.DirExists {
+			err := os.RemoveAll(tDir)
+			if err != nil {
+				t.Fatalf("Could not delete %v; error %v", tDir, err)
+			}
+		}
+
+
+		if c.CfgFileExists {
+			existingCfgFile := path.Join(tDir, kftypesv2.KfConfigFile)
+			err := ioutil.WriteFile(existingCfgFile, []byte("hello world"), 0644)
+
+			if err != nil {
+				t.Fatalf("Could not write %v; error %v", existingCfgFile, err)
+			}
+		}
+
+		c.Input.Spec.AppDir = tDir
+		cfgFile, err := CreateKfAppCfgFile(&c.Input)
+
+		pCase, _ := Pformat(c)
+		hasError := err == nil
+		if  hasError != c.ExpectError {
+			t.Errorf("Test case %v;\n CreateKfAppCfgFile returns error; got %v want %v", pCase, hasError, c.ExpectError)
+		}
+
+		expectFile := path.Join(tDir, kftypesv2.KfConfigFile)
+
+		if !c.ExpectError {
+			if expectFile != cfgFile {
+				t.Errorf("Test case %v;\n CreateKfAppCfgFile returns cfgFile; got %v want %v", pCase, cfgFile, expectFile)
+			}
+		}
+	}
+}
 
 func Test_backfillKfDefFromOptions(t *testing.T) {
 	type testCase struct {
