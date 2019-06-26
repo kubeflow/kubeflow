a828e7d036bec52d8b1e25d4cb164590146501af
Jeremy Lewi <jeremy@lewi.us>
2019-06-26 15:57:19 -0700

    Update KfDef.Spec & Status to support storing information needed by kfctl (#3548)
    * Update KfDef.Spec & Status to support storing information needed by kfctl.
    
    kfctl needs to support multiple repositories since the configuration is
    now split across kubeflow/kubeflow and kubeflow/manifests
    
    We should use KfDef.Status to pass information through the kfctl libraries
    that is temporary such as keeping track of locations where code is cached.
    
    Prior to this repo the fields Repo and ManifestsRepo were reset to
    point to the local cachedir. This means those links were no longer valid
    if the app was moved to a different folder or machine
    
    With this PR we add Spec.Repos to keep track of source of truth
    and Status.ReposCache to store information about the local cache
    
    This way the app can always rebuild the cache if needed.
    Use hashicorp's go-getter to provide a consistent syntax for downloading the
    repos. go-getter supports fetching URIs and specifying lots of variants (e.g pull) no need to invent our own syntax.
    
    Define a SyncCache method on KfDef that will sync multiple repositories.
    
    Refactor NewKfApp
    
    We want to cleanly support two different entry points
    
    For CLI we want to fetch the app.yaml from some repo; overwrite some
    values based on CLI options Save the KfDef and build a new one
    
    For click to deploy we want to supply the complete KfDev.yaml
    
    We refactor the code into a NewKfAppFromKfDef which can be reused in
    both cases
    
    Make repos a list and allow specifying URIs that go-getter will fetch
    
    Also allow specifying the root path inside the untarred file. We do
    this because github appears to create different subdirectories in the
    tarballs in unpredictable ways.
    kfctl_gcp_iap.0.6.yaml is intended to be a single static config for
    initiating GCP.
    
    Relates to #3419 (provide a fixed static set of YAML files for KfDef)
    Define KfDef.SyncCache to provide a systematic way to synchronize all
    repos. This will allow us to get rid of the code in kustomize.go to
    give special treatment to kustomize.
    
    Related to #3491 (kfctl discovery logic is flawed)
    
    * Add KfDef.Spec.Repos and KfDef.Status.ReposCache
    
      * Repos adds support for specifying multiple repositories that need
        to be downloaded.
    
      * Add support for specifying a relative path within the
    downloaded folder to use as the root; we might be able to use this to
    fix #3491
      * Allow specifying full URI and paths within the repo. We will use hashicorp's go-getter syntax to consistently support local paths, git URLs, etc...
    
     * We introduce in ReposCache to store the location of the local cache
         * Currently the Repo fields in spec are changed to keep track of the local
           cache
         * This means we no longer store the source of truth so we can't regenerate
           the cache if the cache is deleted.
         * Relates to #2959 kfctl doesn't regenerate .cache if it is missing
    
    * Add a plugins struct to store parameters related to a plugin like GCP.
    
      * The parameters are stored as a runtime.rawextension; this way each
        plugin can define its own arbitrary struct
    
    * Add a secrets field which allows secrets (e.g. basic auth password) to be
      stored as references (currently only environment variables)
    
      * This allows information about required secrets to be stored in the spec
        without storing the actual secret and checking it into source control
    
      * If a plugin needs to get some value from a secret it can use a secret ref.
    
    * Remove pluginparameters and some other code.
    
    * SetSecret need not return an error.
    
    * Use deepdopy

--- bootstrap/v2/pkg/apis/apps/kfdef/v1alpha1/application_types.go
+++ bootstrap/v2/pkg/apis/apps/kfdef/v1alpha1/application_types.go
@@ -15,36 +15,116 @@
 package v1alpha1
 
 import (
+	"fmt"
+	"github.com/ghodss/yaml"
 	"github.com/kubeflow/kubeflow/bootstrap/config"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
 	"k8s.io/api/v2/core/v1"
 	metav1 "k8s.io/apimachinery/v2/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/v2/pkg/runtime"
+	"os"
+)
+
+const (
+	KfConfigFile = "app.yaml"
 )
 
 // KfDefSpec holds common attributes used by each platform
 type KfDefSpec struct {
 	config.ComponentConfig `json:",inline"`
-	AppDir                 string `json:"appdir,omitempty"`
-	Version                string `json:"version,omitempty"`
-	MountLocal             bool   `json:"mountLocal,omitempty"`
-	Project                string `json:"project,omitempty"`
-	Email                  string `json:"email,omitempty"`
-	IpName                 string `json:"ipName,omitempty"`
-	Hostname               string `json:"hostname,omitempty"`
-	Zone                   string `json:"zone,omitempty"`
-	UseBasicAuth           bool   `json:"useBasicAuth"`
-	SkipInitProject        bool   `json:"skipInitProject,omitempty"`
-	UseIstio               bool   `json:"useIstio"`
-	EnableApplications     bool   `json:"enableApplications"`
-	ServerVersion          string `json:"serverVersion,omitempty"`
-	DeleteStorage          bool   `json:"deleteStorage,omitempty"`
-	PackageManager         string `json:"packageManager,omitempty"`
-	ManifestsRepo          string `json:"manifestsRepo,omitempty"`
+	// TODO(jlewi): Why is AppDir a part of the spec? AppDir is currently used
+	// to refer to the location on disk where all the manifests are stored.
+	// But that should be treated as a local cache and not part of the spec.
+	// For example, if the app is checked out on a different machine the AppDir will change.
+	// AppDir. AppDir is stored in KfDefSpec because we pass a KfDef around to
+	// as a way to pass the information to all the KfApps that need to know the local AppDir.
+	// A better solution might be to store AppDir in KfDef.Status to better reflect its
+	// ephemeral nature and match K8s semantics.
+	AppDir     string `json:"appdir,omitempty"`
+	Version    string `json:"version,omitempty"`
+	MountLocal bool   `json:"mountLocal,omitempty"`
+
+	// TODO(jlewi): Project, Email, IpName, Hostname, Zone and other
+	// GCP specific values should be moved into GCP plugin.
+	Project            string   `json:"project,omitempty"`
+	Email              string   `json:"email,omitempty"`
+	IpName             string   `json:"ipName,omitempty"`
+	Hostname           string   `json:"hostname,omitempty"`
+	Zone               string   `json:"zone,omitempty"`
+	UseBasicAuth       bool     `json:"useBasicAuth"`
+	SkipInitProject    bool     `json:"skipInitProject,omitempty"`
+	UseIstio           bool     `json:"useIstio"`
+	EnableApplications bool     `json:"enableApplications"`
+	ServerVersion      string   `json:"serverVersion,omitempty"`
+	DeleteStorage      bool     `json:"deleteStorage,omitempty"`
+	PackageManager     string   `json:"packageManager,omitempty"`
+	ManifestsRepo      string   `json:"manifestsRepo,omitempty"`
+	Repos              []Repo   `json:"repos,omitempty"`
+	Secrets            []Secret `json:"secrets,omitempty"`
+	Plugins            []Plugin `json:"plugins,omitempty"`
 }
 
 var DefaultRegistry = RegistryConfig{
 	Name: "kubeflow",
 	Repo: "https://github.com/kubeflow/kubeflow.git",
 	Path: "kubeflow",
+}
+
+// Plugin can be used to customize the generation and deployment of Kubeflow
+// TODO(jlewi): Should Plugin contain K8s TypeMeta so that we can use ApiVersion and Kind
+// to identify what it refers to?
+//
+// We disable deep-copy-gen because it chokes on type interface{}.
+// What are the implications of that? Will we eventually need to write our own DeepCopy
+// method based on marshling the object to bytes?
+//
+type Plugin struct {
+	Name       string            `json:"name,omitempty"`
+
+	// TODO(jlewi): Should we be using runtime.Object or runtime.RawExtension
+	Spec *runtime.RawExtension `json:"spec,omitempty"`
+}
+
+// SecretRef is a reference to a secret
+type SecretRef struct {
+	// Name of the secret
+	Name string `json:"name,omitempty"`
+}
+
+// Repo provides information about a repository providing config (e.g. kustomize packages,
+// Deployment manager configs, etc...)
+type Repo struct {
+	// Name is a name to identify the repository.
+	Name string `json:"name,omitempty"`
+	// URI where repository can be obtained.
+	// Can use any URI understood by go-getter:
+	// https://github.com/hashicorp/go-getter/blob/master/README.md#installation-and-usage
+	Uri string `json:"uri,omitempty"`
+
+	// Root is the relative path to use as the root.
+	Root string `json:"root,omitempty"`
+}
+
+// Secret provides information about secrets needed to configure Kubeflow.
+// Secrets can be provided via references e.g. a URI so that they won't
+// be serialized as part of the KfDefSpec which is intended to be written into source control.
+type Secret struct {
+	Name         string        `json:"name,omitempty"`
+	SecretSource *SecretSource `json:"secretSource,omitempty"`
+}
+
+type SecretSource struct {
+	LiteralSource *LiteralSource `json:"literalSource,omitempty"`
+	EnvSource     *EnvSource     `json:"envSource,omitempty"`
+}
+
+type LiteralSource struct {
+	Value string `json:"value,omitempty"`
+}
+
+type EnvSource struct {
+	Name string `json:"Name,omitempty"`
 }
 
 // RegistryConfig is used for two purposes:
@@ -134,6 +214,12 @@ type AppConfig struct {
 // KfDefStatus defines the observed state of KfDef
 type KfDefStatus struct {
 	Conditions []KfDefCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,6,rep,name=conditions"`
+	// ReposCache is used to cache information about local caching of the URIs.
+	ReposCache map[string]RepoCache `json:"reposCache,omitempty"`
+}
+
+type RepoCache struct {
+	LocalPath string `json:"localPath,string"`
 }
 
 type KfDefConditionType string
@@ -178,4 +264,171 @@ type KfDefList struct {
 func GetDefaultRegistry() *RegistryConfig {
 	newReg := DefaultRegistry
 	return &newReg
+}
+
+// GetSecret returns the specified secret or an error if the secret isn't specified.
+func (d *KfDef) GetSecret(name string) (string, error) {
+	for _, s := range d.Spec.Secrets {
+		if s.Name != name {
+			continue
+		}
+		if s.SecretSource.LiteralSource != nil {
+			return s.SecretSource.LiteralSource.Value, nil
+		}
+		if s.SecretSource.EnvSource != nil {
+			return os.Getenv(s.SecretSource.EnvSource.Name), nil
+		}
+
+		return "", fmt.Errorf("No secret source provided for secret %v", name)
+	}
+	return "", fmt.Errorf("No secret in KfDef named %v", name)
+}
+
+// SetSecret sets the specified secret; if a secret with the given name already exists it is overwritten.
+func (d *KfDef) SetSecret(newSecret Secret) {
+	for i, s := range d.Spec.Secrets {
+		if s.Name == newSecret.Name {
+			d.Spec.Secrets[i] = newSecret
+			return
+		}
+	}
+
+	d.Spec.Secrets = append(d.Spec.Secrets, newSecret)
+}
+
+// GetPluginSpec will try to unmarshal the spec for the specified plugin to the supplied
+// interface. Returns an error if the plugin isn't defined or if there is a problem
+// unmarshaling it.
+func (d *KfDef) GetPluginSpec(pluginName string, s interface{}) error {
+	for _, p := range d.Spec.Plugins {
+		if p.Name != pluginName {
+			continue
+		}
+
+		// To deserialize it to a specific type we need to first serialize it to bytes
+		// and then unserialize it.
+		specBytes, err := yaml.Marshal(p.Spec)
+
+		if err != nil {
+			log.Errorf("Could not marshal plugin %v args; error %v", pluginName, err)
+			return err
+		}
+
+		err = yaml.Unmarshal(specBytes, s)
+
+		if err != nil {
+			log.Errorf("Could not unmarshal plugin %v to the provided type; error %v", pluginName, err)
+		}
+		return nil
+	}
+
+	return NewPluginNotFound(pluginName)
+}
+
+// GetPluginSpec will try to unmarshal the spec for the specified plugin to the supplied
+// interface. Returns an error if the plugin isn't defined or if there is a problem
+// unmarshaling it
+//
+// TODO(jlewi): The reason this function exists is because for types like Gcp in gcp.go
+// we embed KfDef into the Gcp struct so its not actually a type KfDef. In the future
+// we will probably refactor KfApp into an appropriate plugin in type an stop embedding
+// KfDef in it.
+func (s *KfDefSpec) GetPluginSpec(pluginName string, pluginSpec interface{}) error {
+	d := &KfDef{
+		Spec: *s,
+	}
+	return d.GetPluginSpec(pluginName, pluginSpec)
+}
+
+// SetPluginSpec sets the requested parameter. The plugin is added if it doesn't already exist.
+func (d *KfDef) SetPluginSpec(pluginName string, spec interface{}) error {
+	// Convert spec to RawExtension
+
+	r := &runtime.RawExtension{}
+
+	// To deserialize it to a specific type we need to first serialize it to bytes
+	// and then unserialize it.
+	specBytes, err := yaml.Marshal(spec)
+
+	if err != nil {
+		log.Errorf("Could not marshal spec; error %v", err)
+		return err
+	}
+
+	err = yaml.Unmarshal(specBytes, r)
+
+	if err != nil {
+		log.Errorf("Could not unmarshal plugin to RawExtension; error %v", err)
+	}
+
+	index := -1
+
+	for i, p := range d.Spec.Plugins {
+		if p.Name == pluginName {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		// Plugin in doesn't exist so add it
+		log.Infof("Adding plugin %v", pluginName)
+
+		d.Spec.Plugins = append(d.Spec.Plugins, Plugin{
+			Name: pluginName,
+		})
+
+		index = len(d.Spec.Plugins) - 1
+	}
+
+	d.Spec.Plugins[index].Spec = r
+
+	return nil
+}
+
+// WriteToFile write the KfDef to a file.
+// WriteToFile will strip out any literal secrets before writing it
+func (d *KfDef) WriteToFile(path string) error {
+
+	stripped := d.DeepCopy()
+
+	secrets := make([]Secret, 0)
+
+	for _, s := range stripped.Spec.Secrets {
+		if s.SecretSource.LiteralSource != nil {
+			log.Warnf("Stripping literal secret %v from KfDef before serializing it", s.Name)
+			continue
+		}
+		secrets = append(secrets, s)
+	}
+
+	stripped.Spec.Secrets = secrets
+
+	// Rewrite app.yaml
+	buf, bufErr := yaml.Marshal(stripped)
+	if bufErr != nil {
+		log.Errorf("Error marshaling kfdev; %v", bufErr)
+		return bufErr
+	}
+	log.Infof("Writing stripped KfDef to %v", path)
+	return ioutil.WriteFile(path, buf, 0644)
+}
+
+type PluginNotFound struct {
+	Name string
+}
+
+func (e *PluginNotFound) Error() string {
+	return fmt.Sprintf("Missing plugin %v", e.Name)
+}
+
+func NewPluginNotFound(n string) *PluginNotFound {
+	return &PluginNotFound{
+		Name: n,
+	}
+}
+
+func IsPluginNotFound(e error) bool {
+	_, ok := e.(*PluginNotFound)
+	return ok
 }
