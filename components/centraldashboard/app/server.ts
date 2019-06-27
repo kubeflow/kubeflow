60fd6473a65bacd12da25542c6b20464bef99a32
Apoorv Verma [AP] <avdaredevil@gmail.com>
2019-06-26 17:40:12 -0700

    Massive Refactor: - profile_route_handlers.ts added to provide middleware for auth / profiles - API.ts changed significantly to rely on `req.user` - All routes have access to the user object - Conditionally load the registration page if no profiles exist for user - The point above is WIP

--- components/centraldashboard/app/server.ts
+++ components/centraldashboard/app/server.ts
@@ -6,8 +6,9 @@ import {Api} from './api';
 import {KubernetesService} from './k8s_service';
 import {getMetricsService} from './metrics_service_factory';
 import {DefaultApi} from './clients/profile_controller';
+import {getProfileContext, handleAuth} from './profile_route_handlers';
 
-const DEFAULT_NAMESPACE = 'kubeflow';
+const DEFAULT_NAMESPACE = process.env.KUBEFLOW_DEFAULT_NAMESPACE || 'kubeflow';
 
 async function main() {
   const {PORT_1} = process.env;  // PORT_1 was defined in an earlier version
@@ -18,10 +19,13 @@ async function main() {
   const k8sService = new KubernetesService(new KubeConfig());
   const metricsService = await getMetricsService(k8sService);
   const profileController = new DefaultApi(`http://kfam.${DEFAULT_NAMESPACE}.svc.cluster.local/kfam/`);
+  const registrationPage = resolve(frontEnd, 'register.html');
 
   app.use(express.json());
   app.use(express.static(frontEnd));
+  app.use(getProfileContext(profileController)); // Detects the auth status for the current user
   app.use('/api', new Api(k8sService, metricsService, profileController).routes());
+  app.use(handleAuth(registrationPage)); // Check if user is connected to an owned profile, if not register them
   app.get('/*', (_: express.Request, res: express.Response) => {
     res.sendFile(resolve(frontEnd, 'index.html'));
   });
