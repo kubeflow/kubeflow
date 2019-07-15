a1d48c9ec5f45c6aa103af3e838a93263b8578cb
Jeremy Lewi <jeremy@lewi.us>
2019-07-15 16:43:55 -0700

    Update the loadtest script.

--- testing/run_deploy_app.sh
+++ testing/run_deploy_app.sh
@@ -8,19 +8,32 @@
 # service account "load-test-owner@kf-gcp-deploy0.iam.gserviceaccount.com"
 # https://pantheon.corp.google.com/iam-admin/serviceaccounts/details/112401461927766705527?organizationId=714441643818&project=kf-gcp-deploy0
 #
-# CLIENT_ID and CLIENT_SECRET both point to
+# CLIENT_ID and CLIENT_SECRET should be set to
 # https://pantheon.corp.google.com/apis/credentials/oauthclient/459682233032-a76ps35eh3j8odvudf4292bgq0jam74i.apps.googleusercontent.com?project=kf-gcp-deploy0&organizationId=714441643818
+#
+# Make sure kubeflow/test/py is on the path.
+set -ex
+export GOOGLE_APPLICATION_CREDENTIALS=/home/jlewi/secrets/kf-gcp-deploy0-11089b0d75ae.json
+export SERVICE_CLIENT_ID=112401461927766705527
 
-GOOGLE_APPLICATION_CREDENTIALS=/usr/local/google/home/zhenghui/env/kf-gcp-deploy0-load-test.json \
-SERVICE_CLIENT_ID=112401461927766705527 \
-CLIENT_ID=459682233032-a76ps35eh3j8odvudf4292bgq0jam74i.apps.googleusercontent.com \
-CLIENT_SECRET=<client secret> \
+if [[ -z "${CLIENT_ID}" ]]; then
+	echo You must sect CLIENT_ID to id for OAUTH client
+	exit 1
+fi
+
+if [[ -z "${CLIENT_SECRET}" ]]; then
+	echo You must sect CLIENT_SECRET to id for OAUTH client
+	exit 1
+fi
+
+# IAP requires setting CLIENT_ID and CLIENT_SECRET environment variable
+#CLIENT_ID=459682233032-a76ps35eh3j8odvudf4292bgq0jam74i.apps.googleusercontent.com \
   python test_deploy_app.py \
   --mode="loadtest" \
-  --kfversion="v0.4.1" \
+  --kfversion="v0.5.1" \
   --project_prefix="kf-load-test-project" \
   --email="load-test-owner@kf-gcp-deploy0.iam.gserviceaccount.com" \
   --number_projects="1" \
   --number_deployments_per_project="1" \
-  --sa_client_id="112401461927766705527" \
+  --sa_client_id="${SERVICE_CLIENT_ID}" \
   --iap_wait_min="45"
