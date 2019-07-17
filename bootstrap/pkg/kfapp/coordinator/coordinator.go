2c7321a4087dcafecb452a8056088639d76c5cf7
Gabriel Wen <gabrielwen@google.com>
2019-07-17 15:10:02 -0700

    invoke in coordinator

--- bootstrap/pkg/kfapp/coordinator/coordinator.go
+++ bootstrap/pkg/kfapp/coordinator/coordinator.go
@@ -755,14 +755,35 @@ func (kfapp *coordinator) Apply(resources kftypes.ResourceEnum) error {
 		return nil
 	}
 
+	gcpAddedConfig := func() error {
+		if kfapp.KfDef.Spec.Email == "" || kfapp.KfDef.Spec.Platform != "gcp" {
+			return nil
+		}
+		p := kfapp.Platforms[kfapp.KfDef.Spec.Platform]
+		if p == nil {
+			return &kfapis.KfError{
+				Code:    int(kfapis.INTERNAL_ERROR),
+				Message: "gcp not in Platforms",
+			}
+		}
+		gcp := p.(*gcp.Gcp)
+		return gcp.ConfigPodDefault()
+	}
+
 	switch resources {
 	case kftypes.ALL:
 		if err := platform(); err != nil {
 			return err
 		}
-		return k8s()
+		if err := k8s(); err != nil {
+			return err
+		}
+		return gcpAddedConfig()
 	case kftypes.PLATFORM:
-		return platform()
+		if err := platform(); err != nil {
+			return err
+		}
+		return gcpAddedConfig()
 	case kftypes.K8S:
 		return k8s()
 	}
