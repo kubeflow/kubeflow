984a8962518fe9c472dc248d8906f1d996810a39
Apoorv Verma [AP] <avdaredevil@gmail.com>
2019-08-07 15:36:40 -0700

    MUI Basic Auth: - Added support for communicating with Profile Controller for namespaces - Renamed api_contributors to api_workgroup - Updated nomenclature, removed unnecessary deps - Fixed tests to work with new response structure

--- components/centraldashboard/app/server.ts
+++ components/centraldashboard/app/server.ts
@@ -5,7 +5,7 @@ import {resolve} from 'path';
 import {Api} from './api';
 import {attachUser} from './attach_user_middleware';
 import {DefaultApi} from './clients/profile_controller';
-import {ContributorApi} from './api_contributors';
+import {WorkgroupApi} from './api_workgroup';
 import {KubernetesService} from './k8s_service';
 import {getMetricsService} from './metrics_service_factory';
 
@@ -36,13 +36,13 @@ async function main() {
   const metricsService = await getMetricsService(k8sService);
   console.info(`Using Profiles service at ${profilesServiceUrl}`);
   const profilesService = new DefaultApi(profilesServiceUrl);
-  const contribApi = new ContributorApi(profilesService);
+  const workgroupApi = new WorkgroupApi(profilesService);
 
   app.use(express.json());
   app.use(express.static(frontEnd));
   app.use(attachUser(USERID_HEADER, USERID_PREFIX));
   app.use(
-      '/api', new Api(k8sService, profilesService, contribApi, metricsService).routes());
+      '/api', new Api(k8sService, workgroupApi, metricsService).routes());
   app.get('/*', (_: express.Request, res: express.Response) => {
     res.sendFile(resolve(frontEnd, 'index.html'));
   });
