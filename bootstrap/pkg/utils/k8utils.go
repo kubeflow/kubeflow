0e06592d6493934a60276357c8e50fbebc0922e4
yeya24 <yb532204897@gmail.com>
2019-08-13 19:39:13 +0800

    add support for azure
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- bootstrap/pkg/utils/k8utils.go
+++ bootstrap/pkg/utils/k8utils.go
@@ -49,6 +49,7 @@ import (
 	// Auth plugins
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 )
 
 const (
