dfd52730a65ebae53a327fd9564880744eaf3e06
kunming <kunming@google.com>
2019-09-05 16:16:04 -0700

    Make config version consistent with kubeflow.org doc

--- components/gcp-click-to-deploy/src/DeployForm.tsx
+++ components/gcp-click-to-deploy/src/DeployForm.tsx
@@ -22,7 +22,7 @@ import {
 /** Relative paths from the root of the repository. */
 enum ConfigPath {
     V05 = 'v0.5-branch/components/gcp-click-to-deploy/app-config.yaml',
-    V06 = 'v0.6-branch/bootstrap/config/kfctl_gcp_iap.0.6.2.yaml'
+    V06 = 'c54401e/bootstrap/config/kfctl_gcp_iap.0.6.2.yaml'
 }
 
 /** Versions available for deployment. */
