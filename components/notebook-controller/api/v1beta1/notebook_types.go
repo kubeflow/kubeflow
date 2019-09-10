b375446f8c3db94832146d567eff4204005e5262
Lun-kai Hsu <lunkai@google.com>
2019-09-10 12:07:06 -0700

    add storage version

--- components/notebook-controller/api/v1beta1/notebook_types.go
+++ components/notebook-controller/api/v1beta1/notebook_types.go
@@ -16,6 +16,7 @@ limitations under the License.
 package v1beta1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -26,16 +27,40 @@ import (
 type NotebookSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Template NotebookTemplateSpec `json:"template,omitempty"`
+}
+
+type NotebookTemplateSpec struct {
+	Spec corev1.PodSpec `json:"spec,omitempty"`
 }
 
 // NotebookStatus defines the observed state of Notebook
 type NotebookStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Conditions is an array of current conditions
+	Conditions []NotebookCondition `json:"conditions"`
+	// ReadyReplicas is the number of Pods created by the StatefulSet controller that have a Ready Condition.
+	ReadyReplicas int32 `json:"readyReplicas"`
+	// ContainerState is the state of underlying container.
+	ContainerState corev1.ContainerState `json:"containerState"`
+}
+
+type NotebookCondition struct {
+	// Type is the type of the condition. Possible values are Running|Waiting|Terminated
+	Type string `json:"type"`
+	// Last time we probed the condition.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	// (brief) reason the container is in the current state
+	// +optional
+	Reason string `json:"reason,omitempty"`
+	// Message regarding why the container is in the current state.
+	// +optional
+	Message string `json:"message,omitempty"`
 }
 
 // +kubebuilder:object:root=true
-
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
 // Notebook is the Schema for the notebooks API
 type Notebook struct {
 	metav1.TypeMeta   `json:",inline"`
