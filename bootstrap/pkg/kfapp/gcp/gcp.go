ee8fe0e4280e4d729cc488442bdba797bf9ad995
Jeremy Lewi <jeremy+github@lewi.us>
2019-09-12 10:24:27 -0700

    GCPPlugin spec should allow specifying location of deployment manager configs. (#4118)
    * Currently kfctl hard codes the repository and location within the repository
      of the deployment manager configs.
    
    * This PR changes that so the location of the deployment manager configs
      can be provided using a RepoRef.
    
    * For backwards compatibility the location defaults to the current location.
    
    * The immediate need is we want to move all the manifests to kubeflow/manifests
      Related to kubeflow/manifests#241

--- bootstrap/pkg/kfapp/gcp/gcp.go
+++ bootstrap/pkg/kfapp/gcp/gcp.go
@@ -83,6 +83,11 @@ const (
 	BASIC_AUTH_SECRET = "kubeflow-login"
 	KUBECONFIG_FORMAT = "gke_{project}_{zone}_{cluster}"
 
+	// The default path in kubeflow/kubeflow to the deployment manager configs.
+	// TODO(jlewi): This is only provided for legacy reasons. In 0.7 the path should be set explicitly
+	// in the KfDef spec.
+	DEFAULT_DM_PATH = "deployment/gke/deployment_manager_configs"
+
 	// Plugin parameter constants
 	GcpPluginName               = kftypesv3.GCP
 	GcpAccessTokenName          = "accessToken"
@@ -1283,15 +1288,15 @@ func (gcp *Gcp) generateDMConfigs() error {
 				gcpConfigDirErr, appDir),
 		}
 	}
-	repo, ok := gcp.kfDef.Status.ReposCache[kftypesv3.KubeflowRepoName]
+	repo, ok := gcp.kfDef.Status.ReposCache[pluginSpec.DeploymentManagerConfig.RepoRef.Name]
 
 	if !ok {
-		err := fmt.Errorf("Repo %v not found in KfDef.Status.ReposCache", kftypesv3.KubeflowRepoName)
+		err := fmt.Errorf("Repo %v not found in KfDef.Status.ReposCache", pluginSpec.DeploymentManagerConfig.RepoRef.Name)
 		log.Errorf("%v", err)
 		return errors.WithStack(err)
 	}
 
-	sourceDir := path.Join(repo.LocalPath, "deployment/gke/deployment_manager_configs")
+	sourceDir := path.Join(repo.LocalPath, pluginSpec.DeploymentManagerConfig.RepoRef.Path)
 	files := []string{"cluster.jinja", "cluster.jinja.schema", "storage.jinja",
 		"storage.jinja.schema"}
 	for _, file := range files {
@@ -1912,6 +1917,18 @@ func (gcp *Gcp) setGcpPluginDefaults() error {
 					},
 				},
 			})
+		}
+	}
+
+	// Initialize the deployment manager configs.
+	if pluginSpec.DeploymentManagerConfig == nil {
+		pluginSpec.DeploymentManagerConfig = &DeploymentManagerConfig{}
+	}
+
+	if pluginSpec.DeploymentManagerConfig.RepoRef == nil {
+		pluginSpec.DeploymentManagerConfig.RepoRef = &kfdefs.RepoRef{
+			Name: kftypesv3.KubeflowRepoName,
+			Path: DEFAULT_DM_PATH,
 		}
 	}
 
