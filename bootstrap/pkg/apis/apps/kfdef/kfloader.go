d8c338b35bbb36b323361352cce4411762a8390a
Gabriel Wen <gabrielwen@google.com>
2019-09-18 14:25:25 -0700

    kfloader init

---
+++ bootstrap/pkg/apis/apps/kfdef/kfloader.go
@@ -0,0 +1,77 @@
+package kfdef
+
+import (
+	"fmt"
+	"github.com/ghodss/yaml"
+	gogetter "github.com/hashicorp/go-getter"
+	kfapis "github.com/kubeflow/kubeflow/bootstrap/v3/pkg/apis"
+	log "github.com/sirupsen/logrus"
+	"ioutil"
+	"k8s.io/apimachinery/pkg/runtime"
+	netUrl "net/url"
+	"path"
+)
+
+func isValidUrl(toTest string) bool {
+	_, err := netUrl.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func LoadKfDefFromURI(configFile string) (*KfDef, error) {
+	if configFile == "" {
+		return nil, fmt.Errorf("config file must be the URI of a KfDef spec")
+	}
+
+	// TODO(jlewi): We should check if configFile doesn't specify a protocol or the protocol
+	// is file:// then we can just read it rather than fetching with go-getter.
+	appDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return nil, fmt.Errorf("Create a temporary directory to copy the file to.")
+	}
+	// Open config file
+	//
+	// TODO(jlewi): Should we use hashicorp go-getter.GetAny here? We use that to download
+	// the tarballs for the repos. Maybe we should use that here as well to be consistent.
+	appFile := path.Join(appDir, KfConfigFile)
+
+	log.Infof("Downloading %v to %v", configFile, appFile)
+	configFileUri, err := netUrl.Parse(configFile)
+	if err != nil {
+		log.Errorf("could not parse configFile url")
+	}
+	if isValidUrl(configFile) {
+		errGet := gogetter.GetFile(appFile, configFile)
+		if errGet != nil {
+			return nil, &kfapis.KfError{
+				Code:    int(kfapis.INVALID_ARGUMENT),
+				Message: fmt.Sprintf("could not fetch specified config %s: %v", configFile, err),
+			}
+		}
+	} else {
+		g := new(gogetter.FileGetter)
+		g.Copy = true
+		errGet := g.GetFile(appFile, configFileUri)
+		if errGet != nil {
+			return nil, &kfapis.KfError{
+				Code:    int(kfapis.INVALID_ARGUMENT),
+				Message: fmt.Sprintf("could not fetch specified config %s: %v", configFile, err),
+			}
+		}
+	}
+
+	// Read contents
+	configFileBytes, err := ioutil.ReadFile(appFile)
+	if err != nil {
+		return nil, &kfapis.KfError{
+			Code:    int(kfapis.INTERNAL_ERROR),
+			Message: fmt.Sprintf("could not read from config file %s: %v", configFile, err),
+		}
+	}
+
+	// Check API version.
+	return nil, nil
+}
