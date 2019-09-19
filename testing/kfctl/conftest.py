c801b7873383db9ea9c18b35a010dd0910648389
Yannis Zarkadas <yanniszark@arrikto.com>
2019-09-19 16:11:52 +0300

    kfctl: existing_arrikto: initial e2e test
    Signed-off-by: Yannis Zarkadas <yanniszark@arrikto.com>

--- testing/kfctl/conftest.py
+++ testing/kfctl/conftest.py
@@ -34,7 +34,15 @@ def pytest_addoption(parser):
   parser.addoption(
       "--use_istio", action="store", default="False",
       help="Use istio.")
-
+  
+  parser.addoption(
+      "--cluster_creation_script", action="store", default="",
+      help="The script to use to create a K8s cluster before running kfctl.")
+  
+  parser.addoption(
+      "--cluster_deletion_script", action="store", default="",
+      help="The script to use to delete a K8s cluster before running kfctl.")
+  
 @pytest.fixture
 def app_path(request):
   return request.config.getoption("--app_path")
@@ -58,6 +66,14 @@ def project(request):
 @pytest.fixture
 def config_path(request):
   return request.config.getoption("--config_path")
+
+@pytest.fixture
+def cluster_creation_script(request):
+  return request.config.getoption("--cluster_creation_script")
+
+@pytest.fixture
+def cluster_deletion_script(request):
+  return request.config.getoption("--cluster_deletion_script")
 
 @pytest.fixture
 def use_basic_auth(request):
