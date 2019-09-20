f45d672594870152e10dd34c05566d1906be7cd5
Gabriel Wen <gabrielwen@google.com>
2019-09-19 22:15:50 -0700

    fix

--- bootstrap/pkg/apis/apps/kfdef/kfloader_test.go
+++ bootstrap/pkg/apis/apps/kfdef/kfloader_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestKfLoader_LoadKfDefBackwardCompatibility(t *testing.T) {
+func TestKfLoader_LoadKfDefBackwardCompatible(t *testing.T) {
 	wd, _ := os.Getwd()
 
 	alpha, err := LoadKfDefFromURI(path.Join(wd, "testdata", "kfdef_v1alpha1.yaml"))
@@ -21,10 +21,8 @@ func TestKfLoader_LoadKfDefBackwardCompatibility(t *testing.T) {
 		t.Fatalf("error when loading v1beta1 KfDef: %v", err)
 	}
 	if !reflect.DeepEqual(alpha, beta) {
-		t.Errorf("KfDef loaded is not compatible;\n%v\nv.s.\n%v",
+		t.Errorf("KfDef loaded is not compatible;\nalpha\n%v\nbeta\n%v",
 			kfutils.PrettyPrint(alpha),
 			kfutils.PrettyPrint(beta))
-		_ = ioutil.WriteFile(path.Join(wd, "alpha.txt"), []byte(kfutils.PrettyPrint(alpha)), 0644)
-		_ = ioutil.WriteFile(path.Join(wd, "beta.txt"), []byte(kfutils.PrettyPrint(beta)), 0644)
 	}
 }
