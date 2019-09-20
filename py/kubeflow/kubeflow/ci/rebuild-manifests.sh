4940bbf93b4ec94d8558d6cf8e7e26542f02d483
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-09-20 08:35:46 -0700

    get user's email

--- py/kubeflow/kubeflow/ci/rebuild-manifests.sh
+++ py/kubeflow/kubeflow/ci/rebuild-manifests.sh
@@ -7,15 +7,17 @@ if [[ -f /workspace/${pull_request_repo}-${pull_request_id}/pr.json ]]; then
   kubeflow_forked_commit=$(cat pr.json | jq .Head.SHA? | xargs)
   new_branch_name=$(echo ${kubeflow_forked_branch}-image-update | xargs)
   user=$(cat github/pr.json | jq .user.login | xargs)
+  export GITHUB_TOKEN=$(kubectl get secrets github-token-secret -ojson | jq '. | .data.token' | xargs | base64 -d)
+  email=$(curl "https://api.github.com/users/kkasravi?access_token=${GITHUB_TOKEN}" -s | jq .email | xargs
   popd
   git config --global user.name $user
-  git config --global user.email 'kubeflow@kubeflow.org'
+  git config --global user.email $email
   kubectl get secret github-secret -ojson | jq ' . | .data["ssh-publickey"]' | xargs | base64 -d > ~/.ssh/id_rsa.pub
   cp ~/.ssh/id_github-secret ~/.ssh/id_rsa
   chmod 0600 ~/.ssh/id_rsa
   chmod 0600 ~/.ssh/id_rsa.pub
   ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-  ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+  ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
   GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git remote add upstream git@github.com:kubeflow/manifests.git
   GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git fetch upstream master
   GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git checkout -b $new_branch_name upstream/master
@@ -30,7 +32,6 @@ if [[ -f /workspace/${pull_request_repo}-${pull_request_id}/pr.json ]]; then
     echo "" >> $tmpfile
     echo "" >> $tmpfile
     echo "Image built from repo:$kubeflow_forked_repo branch:$kubeflow_forked_branch commit:$kubeflow_forked_commit" >> $tmpfile
-    export GITHUB_TOKEN=$(kubectl get secrets github-token-secret -ojson | jq '. | .data.token' | xargs | base64 -d)
     GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" hub pull-request -f -b 'kubeflow:master' -F $tmpfile
   else
     echo 'make generate && make test' failed
