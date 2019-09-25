476391f3b99ecdba35fec4388c70fe224d180b58
Yannis Zarkadas <yanniszark@arrikto.com>
2019-09-25 18:54:54 +0300

    use python for the deletion script
    Signed-off-by: Yannis Zarkadas <yanniszark@arrikto.com>

--- testing/kfctl/scripts/delete_existing_cluster.sh
+++ testing/kfctl/scripts/delete_existing_cluster.sh
@@ -1,8 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-set -e
+import os
+import logging
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+from kubeflow.testing import util
 
-export CLUSTER_NAME="existing-arrikto-${REPO_NAME}-${PULL_NUMBER}"
 
-gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
-gcloud container clusters delete "${CLUSTER_NAME}" --zone us-central1-a
+def must_getenv(name):
+    value = os.getenv(name)
+    if not name:
+        logging.fatal("Environment variable %s is not set", name)
+        raise ValueError()
+    return value
+
+
+if __name__ == "__main__":
+
+    util.run([
+        "gcloud", "auth", "activate-service-account", "--key-file",
+        must_getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    ])
+
+    cluster_name = "existing-arrikto-" + must_getenv("REPO_NAME") + "-" + must_getenv("PULL_NUMBER")
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('container', 'v1', credentials=credentials)
+    util.delete_cluster(service, cluster_name, "kubeflow-ci", "us-central1-a")
