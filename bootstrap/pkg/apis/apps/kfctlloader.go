4944e2ecf13a25f5b4ae72315479de3fbca2e97e
Gabriel Wen <gabrielwen@google.com>
2019-09-27 11:33:44 -0700

    LoadConfigFromURI init

---
+++ bootstrap/pkg/apis/apps/kfctlloader.go
@@ -0,0 +1,13 @@
+package apps
+
+import (
+	kfapis "github.com/kubeflow/kubeflow/bootstrap/v3/pkg/apis"
+	kfconfig "github.com/kubeflow/kubeflow/bootstrap/v3/pkg/apis/apps/kfctlconfig"
+)
+
+func LoadConfigFromURI(configFile string) (*kfconfig.KfctlConfig, error) {
+	return nil, &kfapis.KfError{
+		Code:    int(kfapis.INTERNAL_ERROR),
+		Message: "Not implemented.",
+	}
+}
