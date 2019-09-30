7e369a30e578b47593b03ee7491c7d55c0a32802
Chakradhar N R <nrchakradhar379@gmail.com>
2019-09-30 16:05:42 +0530

    try3

--- testing/kfctl/kfctl_go_test.py
+++ testing/kfctl/kfctl_go_test.py
@@ -2,7 +2,7 @@ import logging
 import os
 import pytest
 
-from kubeflow.ci import kfctl_go_test_utils as kfctl_util
+from kubeflow.kubeflow.ci import kfctl_go_test_utils as kfctl_util
 from kubeflow.testing import util
 
 def test_build_kfctl_go(app_path, project, use_basic_auth, use_istio, config_path):
