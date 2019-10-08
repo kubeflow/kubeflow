c1e4a541630ad3add79244ea19c266833a53adfe
kunming <kunming@google.com>
2019-10-07 19:33:53 -0700

    resolve comments

--- components/kf-monitoring/MetricsExporter.go
+++ components/kf-monitoring/MetricsExporter.go
@@ -12,8 +12,9 @@ import (
 )
 
 // Common label keys for all metrics signals
-// COMPONENT: name of component outputing the metrics, eg. "tf-operator"
-const COMPONENT = "component"
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// COMPONENT: name of Component outputing the metrics, eg. "tf-operator"
+const COMPONENT = "Component"
 // KIND: each componenet can label their metrics with custom tag "kind". Suggest keeping "kind" value to be CONSTANT PER METRICS.
 const KIND = "kind"
 const NAMESPACE = "namespace"
@@ -21,13 +22,20 @@ const NAMESPACE = "namespace"
 const ACTION = "action"
 // SEVERITY: level of importance, used to filter alerts
 const SEVERITY  = "severity"
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+// Alerting metrics severity levels
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const SEVERITY_MINOR = "Minor"
 const SEVERITY_MAJOR = "Major"
 const SEVERITY_CRITICAL = "Critical"
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+// Util const values
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const METRICSPORT = 8079
 const METRICSPATH = "/metrics"
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 var (
 	// Counter metrics
@@ -70,10 +78,10 @@ var (
 		Help:    "A histogram of request_exponential_latency",
 	}, []string{COMPONENT, KIND, NAMESPACE, ACTION, SEVERITY})
 
-	serviceHeartbeat = prometheus.NewCounter(prometheus.CounterOpts{
+	serviceHeartbeat = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "service_heartbeat",
 		Help: "Heartbeat signal every 10 seconds indicating pods are alive.",
-	})
+	}, []string{COMPONENT, SEVERITY})
 )
 
 func init() {
@@ -88,74 +96,66 @@ func init() {
 }
 
 type MetricsExporter struct {
-	component	string
-	// Add error channel so that kf_monitor user can get notified when monitoring thread is down.
-	ErrChan     chan error
-	metricsPort int
+	Component   string
+	MetricsPort int
 }
 
 func NewMetricsExporter(component string) *MetricsExporter {
 	return &MetricsExporter{
-		component: component,
-		ErrChan: make(chan error),
-		metricsPort: METRICSPORT,
+		Component:   component,
+		MetricsPort: METRICSPORT,
 	}
 }
 
-func (me *MetricsExporter) SetPort(port int) *MetricsExporter {
-	me.metricsPort = port
-	return me
-}
-
-func (me *MetricsExporter) Start() {
-	go me.serveMetrics()
-}
-
-func (me *MetricsExporter) AddRequestCounter(kind string, namespace string, action string) {
-	labels := prometheus.Labels{COMPONENT: me.component, KIND: kind, NAMESPACE: namespace, ACTION: action}
+func (me *MetricsExporter) IncRequestCounter(kind string, namespace string, action string, severity string) {
+	labels := prometheus.Labels{COMPONENT: me.Component, KIND: kind, NAMESPACE: namespace, ACTION: action,
+		SEVERITY: severity}
 	requestCounter.With(labels).Inc()
 }
 
-func (me *MetricsExporter) AddRequestFailureCounter(kind string, namespace string, action string) {
-	labels := prometheus.Labels{COMPONENT: me.component, KIND: kind, NAMESPACE: namespace, ACTION: action}
+func (me *MetricsExporter) IncRequestFailureCounter(kind string, namespace string, action string, severity string) {
+	labels := prometheus.Labels{COMPONENT: me.Component, KIND: kind, NAMESPACE: namespace, ACTION: action,
+		SEVERITY: severity}
 	requestFailureCounter.With(labels).Inc()
 }
 
-func (me *MetricsExporter) AddRequestGauge(kind string, namespace string, action string, val float64) {
-	labels := prometheus.Labels{COMPONENT: me.component, KIND: kind, NAMESPACE: namespace, ACTION: action}
+func (me *MetricsExporter) AddRequestGauge(kind string, namespace string, action string, severity string, val float64) {
+	labels := prometheus.Labels{COMPONENT: me.Component, KIND: kind, NAMESPACE: namespace, ACTION: action,
+		SEVERITY: severity}
 	requestGauge.With(labels).Add(val)
 }
 
-func (me *MetricsExporter) ObserveRequestExponentialLatency(kind string, namespace string, action string, val float64) {
-	labels := prometheus.Labels{COMPONENT: me.component, KIND: kind, NAMESPACE: namespace, ACTION: action}
+func (me *MetricsExporter) ObserveRequestExponentialLatency(kind string, namespace string, action string,
+	severity string, val float64) {
+	labels := prometheus.Labels{COMPONENT: me.Component, KIND: kind, NAMESPACE: namespace, ACTION: action,
+		SEVERITY: severity}
 	requestExponentialLatency.With(labels).Observe(val)
 }
 
-func (me *MetricsExporter) serveMetrics() {
+func (me *MetricsExporter) ServeMetrics() error {
 	mux := http.NewServeMux()
 	mux.Handle(METRICSPATH, promhttp.Handler())
 	server := http.Server{
 		Handler: mux,
 	}
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", me.metricsPort))
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", me.MetricsPort))
 	if err != nil {
-		log.Fatalf("listener failed to start! %v", err)
-		me.ErrChan <- err
-		return
+		log.Errorf("listener failed to start! %v", err)
+		return err
 	}
 	// Count heartbeat
 	go func() {
+		labels := prometheus.Labels{COMPONENT: me.Component, SEVERITY: SEVERITY_CRITICAL}
 		for {
 			time.Sleep(10 * time.Second)
-			serviceHeartbeat.Inc()
+			serviceHeartbeat.With(labels).Inc()
 		}
 	}()
 	// Serve metrics
-	go func() {
-		log.Info("starting metrics server", "path", METRICSPATH)
-		if err := server.Serve(listener); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Metrics server failed to start! %v", err)
-			me.ErrChan <- err
-		}
-	}()
+	log.Infof("starting metrics server at %v:%v", me.MetricsPort, METRICSPATH)
+	if err := server.Serve(listener); err != nil && err != http.ErrServerClosed {
+		log.Errorf("Metrics server failed to start! %v", err)
+		return err
+	}
+	return nil
 }
