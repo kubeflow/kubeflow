486e312f968acf6b50c6e21f31c08dc4aa6a1e11
Zhenghui Wang <zhenghui@google.com>
2019-10-08 15:02:24 -0700

    extend the endpoint timeout limit to 50m

--- testing/kfctl/endpoint_ready_test.py
+++ testing/kfctl/endpoint_ready_test.py
@@ -26,7 +26,7 @@ def test_endpoint_is_ready(project, app_name):
   os.environ["CLIENT_ID"] = "29647740582-7meo6c7a9a76jvg54j0g2lv8lrsb4l8g.apps.googleusercontent.com"
   if not gcp_util.endpoint_is_ready(
       "https://{}.endpoints.{}.cloud.goog".format(app_name, project),
-      wait_min=25):
+      wait_min=50):
     raise Exception("Endpoint not ready")
 
 if __name__ == "__main__":
