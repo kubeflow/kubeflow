19c0dd6fd982351f6d9d08df2bd1942826287b28
Jeremy Lewi <jeremy+github@lewi.us>
2019-12-20 11:11:32 -0800

    Make the Tekton CD pipeline for profile controller run on KF release cluster (#4568)
    * Get rid of the PVC used to pass the image digest file between the build
      and update manifests step
    
      * Creating a PVC just creates operational complexity
    
    * We combine the build and update manifests step into one task. We can
      then use /workspace (a pod volume) to pass data like the image digest
      file between the steps
    
    * Update pipelineRun to work with version 0.9 of Tekton
      * Field serviceAccount has been renamed serviceAccountName
    
      * TaskRun no longer supports outputImageDir so we remove it; we will
        have to use Tekton to pass the image digest file
    
    * Remove the namespace.yaml and secrets.yaml from the kustomize package
    
      * The secrets should be created out of band and not checked in
      * So the behavior should be to deploy the kustomize package in a namespace
        that already exists with the appropriate secrets
    
      * Checking in secrets is confusing
    
        * If we check in dummy secrets then users will get confused about
          whether the secrets are valid or not
    
        * Furthermore, the file secrets.yaml is an invitation to end up checking
          the secrets into source control.
    
    * Configure some values to use gcr.io/kubeflow-images-public
    
    * Disable ISTIO sidecar in the pipelines
    
    * For kaniko we don't need the secret to be named a certain way we just
      need to set GOOGLE_APPLICATION_CREDENTIALS to point to the correct value
    
    * We change kaniko to use the user-gcp-sa secret that Kubeflow creates
    
    * We shouldn't need an image pull secret since kubeflow-images-public is public
      * GOOGLE_APPLICATION_CREDENTIALS should be used for pushing images
    
    * Change the name of the secret containing ssh credentials for kubeflow-bot
      to kubeflow-bot-github-ssh
    
    * rebuild-manifests.sh should use /workspace to get the image digest
      rather than the PVC.
    
    * Simplify rebuild-manifests.ssh
    
      * Tekton will mount the .ssh information in /tekton/home/.ssh
        so we just need to create a symbolic link to /root/.ssh
    
      * The image digest file should be fetched from /workspace and not some PVC.
    
      * Set GITHUB_TOKEN  environment variable using secrets so that we don't
        need to use kubectl get to fetch it
    
      * We need to make the clone of kubeflow/manifests a non-shallow clone
        before we can push changes to the remote repo
    
    * I was able to successfully run the profile controller workflow and create a
      PR
    
      https://github.com/kubeflow/manifests/pull/669
    
    Next steps:
    
    * This PR only updated the profile controller
    
    * We need to refactor how the PipelineRun's are laid out
    
      * I think we may want the PipelineRun's to be separate from the reused
        resurces like Task
    
    * rebuil-manifests.sh should only regenerate tests for changed files
    
    * The created PRs don't satisfy the Kubeflow CLA check.
    
    Related to: kubeflow/testing#450

--- py/kubeflow/kubeflow/ci/rebuild-manifests.sh
+++ py/kubeflow/kubeflow/ci/rebuild-manifests.sh
@@ -26,36 +26,74 @@
 # how to set env vars from configmap if debugging
 # for i in $(kubectl get cm ci-pipeline-run-parameters -ojson | jq -r '.data | keys[] as $k | "\($k)=\(.[$k])"'); do echo export $i; export $i; done
 #
+set -ex
+
+# This is for debug
 echo '--env--'
-env
+env | sort
 echo '--env--'
-new_branch_name='update_'$image_name'_'$kubeflow_repo_revision
-export GITHUB_TOKEN=$(kubectl get secrets github-token -ojson | jq '. | .data.token' | xargs | base64 -d)
-kubectl get secret github-ssh -ojson | jq ' . | .data["ssh-publickey"]' | xargs | base64 -d > ~/.ssh/id_rsa.pub
-cp ~/.ssh/id_github-ssh ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa.pub
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-if [[ ! -d /root/.ssh ]]; then
-  mkdir /root/.ssh
-fi
+
+# GitHub user to store the fork
+fork_user=kubeflow-bot
+
+# Get the commit for the kubeflow repository
+cd /workspace/kubeflow
+kubeflow_commit=$(git rev-parse HEAD)
+kubeflow_commit=${digest:0:8}
+
+# We use a unique branch name based on the digest of the image
+digest=$(cat /workspace/${image_name}-digest)
+# Shorten the digest. The digest will start with sha256:
+short_digest=${digest:7:8}
+new_branch_name='update_'$image_name'_'${short_digest}
+
+# TODO(jlewi): We should mount the GITHUB_TOKEN as a secret
+#export GITHUB_TOKEN=$(kubectl get secrets github-token -ojson | jq '. | .data.token' | xargs | base64 -d)
+
+# Tekton will automatically mount the ssh private key and known hosts stored in the git secret
+# in /tekton/home/.ssh
+# however since this scriptt runs in our test worker image  it ends up using /root/.sssh
+ln -sf /tekton/home/.ssh /root/.ssh
 ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git fetch origin master
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git checkout -b $new_branch_name origin/master
-kustomize edit set image gcr.io/kubeflow-images-public/${image_name}=gcr.io/kubeflow-images-public/${image_name}@$(cat /kubeflow/${image_name}-digest)
+cd /workspace/manifests
+
+# Do a full fetch to unshallow the clone
+# it looks like Tekton might do a shallow checkout
+git fetch --unshallow
+
+# Create a new branch for the pull request
+git checkout -b $new_branch_name origin/${manifests_repo_revision}
+
+# Add the kubeflow-bot repo
+git remote add ${fork_user} git@github.com:${fork_user}/manifests.git
+
+cd /workspace/manifests/${path_to_manifests_dir}
+kustomize edit set image gcr.io/kubeflow-images-public/${image_name}=gcr.io/kubeflow-images-public/${image_name}@$(cat /workspace/${image_name}-digest)
 cd /workspace/manifests/tests
-make generate && make test
+
+# TODO(jlewi): Changed to make generate-changed-only once https://github.com/kubeflow/manifests/pull/665
+# is submitted
+# make generate-changed-only 
+make generate
+make test
 if (( $? == 0 )); then
   git config --global user.email "kubeflow-bot@kubflow.org"
   git config --global user.name "kubeflow-bot"
-  GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git commit -a -m "image updated as part of kubeflow repo:$kubeflow_repo_url commit:$kubeflow_repo_revision"
-  GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git push origin $new_branch_name -f
+  
   tmpfile=$(mktemp)
-  echo "[auto PR] Update the $image_name image to $(cat /kubeflow/${image_name}-digest)" > $tmpfile
+  
+  echo "[auto PR] Update the ${image_name} image to commit ${kubeflow_commit}" > $tmpfile
+  echo "" >> $tmpfile
+  echo "* Use image digest $(cat /workspace/${image_name}-digest)" >> $tmpfile
+
   echo "" >> $tmpfile
   echo "" >> $tmpfile
-  echo "Image built from repo:$manifests_repo_url branch:$new_branch_name commit:$(git rev-parse HEAD)" >> $tmpfile
-  GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" hub pull-request -f -b 'kubeflow:master' -F $tmpfile
+  echo "* Image built from repo:$manifests_repo_url branch:$new_branch_name commit:$(kubeflow_commit)" >> $tmpfile
+
+  git commit -a -F ${tmpfile}
+  
+  git push ${fork_user} $new_branch_name -f
+  hub pull-request -f -b 'kubeflow:master' -F $tmpfile
 else
   echo 'make generate && make test' failed
 fi
