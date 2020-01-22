251542a3d207ab5d50243ce0d7c47ba2cd0aba57
Kunming Qu <37601826+kunmingg@users.noreply.github.com>
2020-01-22 15:50:50 -0800

    remove unused tag (#4682)

--- components/profile-controller/controllers/moniotring.go
+++ components/profile-controller/controllers/moniotring.go
@@ -28,13 +28,13 @@ var (
 			Name: "request_kf",
 			Help: "Number of request_counter",
 		},
-		[]string{COMPONENT, KIND, REQUSER, ACTION, PATH},
+		[]string{COMPONENT, KIND},
 	)
 	// Counter metrics for failed requests
 	requestErrorCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "request_kf_failure",
 		Help: "Number of request_failure_counter",
-	}, []string{COMPONENT, KIND, REQUSER, ACTION, PATH, SEVERITY})
+	}, []string{COMPONENT, KIND, SEVERITY})
 
 	serviceHeartbeat = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "service_heartbeat",
