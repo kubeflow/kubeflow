e21177146e1add3fb299522b4990ede7d0b0293e
Apoorv Verma [AP] <avdaredevil@gmail.com>
2020-02-18 21:16:26 -0800

    Test cleanup logic + cleanup help logs added!

--- components/centraldashboard/test/e2e.test.ts
+++ components/centraldashboard/test/e2e.test.ts
@@ -2,6 +2,7 @@ import 'jasmine';
 
 import {Credentials, JWT} from 'google-auth-library';
 import {launch, Page} from 'puppeteer';
+import request from 'request';
 
 
 /**
@@ -22,6 +23,7 @@ const {
     if (!process.env[envVar]) {console.log(`${envVar} environment variable must be set`);process.exit(1);}
 });
 if (!/^https?:\/\/\S+/.test(KF_HOST)) {console.log('Invalid HOST url provided, must be like http*://*');process.exit(1);}
+const HOST = KF_HOST.replace(/\/$/, '');
 
 function getAuth(): Promise<Credentials> {
     const client = new JWT({
@@ -48,6 +50,20 @@ async function interceptHeadersForDebugging(page: Page) {
     });
 }
 
+/**
+ * This function should help any dev down the line quickly inspect all
+ *  request headers as they arrive in the browser
+ * @param page Page to intercept headers for
+ */
+async function clearServiceUser(credentials: Credentials) {
+    const body = await new Promise((res, rej) => request.delete(`${HOST}/api/workgroup/nuke-self`, {
+        headers: {Authorization: `Bearer ${credentials.id_token}`},
+    }, (err, resp, body) =>
+        err || resp.statusCode >= 400? rej(err || body) : res(body)
+    ));
+    console.log('    Cleared service-user profile:', body);
+}
+
 describe('Kubeflow Dashboard Tests', () => {
     beforeEach(() => {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
@@ -57,14 +73,18 @@ describe('Kubeflow Dashboard Tests', () => {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = ORIGINAL_TIMEOUT;
     });
 
+    xit('Will clear possible test remnants', async () => {
+        const credentials = await getAuth();
+        await clearServiceUser(credentials);
+    });
     it('Should allow the user to register on first access', async () => {
         const browser = await launch();
         const credentials = await getAuth();
         const page = await browser.newPage();
         page.setExtraHTTPHeaders({Authorization: `Bearer ${credentials.id_token}`});
-        console.log(`   Connecting to ${KF_HOST}`);
+        console.log(`   Connecting to ${HOST}`);
         // interceptHeadersForDebugging(page);
-        await page.goto(KF_HOST);
+        await page.goto(HOST);
         
         console.log(`   Scanning contents`);
         const prelimContent = await (await page.waitForFunction(() => document.body.innerHTML)).jsonValue() as string;
@@ -84,8 +104,13 @@ describe('Kubeflow Dashboard Tests', () => {
             document.querySelector('body > main-page')
                 .shadowRoot.querySelector('registration-page')
         );
-        if (!isRegistrationPresent) throw Error(`Registration view did not load, which means this is not the first run for this user, please remove this profile from the server and try again!`);
-
+        if (!isRegistrationPresent) {
+            throw Error([
+                `Registration view did not load, which means this is not the first`,
+                `run for this user, please remove this profile from the server and try again!\n`,
+                `An easy way to do this, is to update the \`xit\` case to \`fit\` which will clear leftover test relics`
+            ].join(''));
+        }
         console.log(`   Interacting with Registration flow`);
         const startRegistration = await page.waitForFunction(() =>
             document.querySelector('body > main-page')
@@ -113,5 +138,7 @@ describe('Kubeflow Dashboard Tests', () => {
         const namespaceTextContent = await namespace.jsonValue() as string;
         expect(namespaceTextContent.trim())
             .toBe(SERVICE_ACCOUNT_EMAIL.split('@')[0]);
+        console.log('   Clearing Test Data');
+        await clearServiceUser(credentials);
     });
 });
