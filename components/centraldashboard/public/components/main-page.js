014684842aa78b4696f112022a41bcb205c02dbf
Luis <luis.magana@pwc.com>
2020-06-19 14:50:34 -0500

    env driven registration flow via REGISTRATION_FLOW
    Update components/centraldashboard/public/components/main-page.js as per PR suggestion
    
    Co-authored-by: Apoorv Verma [AP] <avdaredevil@gmail.com>
    renamed registrationFlow to registrationFlowAllowed

--- components/centraldashboard/public/components/main-page.js
+++ components/centraldashboard/public/components/main-page.js
@@ -173,11 +173,16 @@ export class MainPage extends utilitiesMixin(PolymerElement) {
      * @param {Event} ev AJAX-response
      */
     _onHasWorkgroupResponse(ev) {
-        const {user, hasWorkgroup, hasAuth} = ev.detail.response;
+        const {user, hasWorkgroup, hasAuth, 
+            registrationFlowAllowed} = ev.detail.response;
         this._setIsolationMode(hasAuth ? 'multi-user' : 'single-user');
-        if (hasAuth && !hasWorkgroup) {
+        if (hasAuth) {
             this.user = user;
-            this._setRegistrationFlow(true);
+            if (registrationFlowAllowed && !hasWorkgroup) {
+                this._setRegistrationFlow(true);
+            } else {
+                this._setRegistrationFlow(false);
+            }
         }
         this._setWorkgroupStatusHasLoaded(true);
     }
