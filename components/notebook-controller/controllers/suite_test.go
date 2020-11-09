402bfa8aa994cf1ddb9ccbc02ca0db488cf7c893
naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-11-09 14:18:04 -0500

    Some tweaks based on review.

--- components/notebook-controller/controllers/suite_test.go
+++ components/notebook-controller/controllers/suite_test.go
@@ -32,7 +32,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	beta1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1beta1"
+	nbv1beta1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1beta1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -56,7 +56,7 @@ var _ = BeforeSuite(func(done Done) {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(cfg).ToNot(BeNil())
 
-	err = beta1.AddToScheme(scheme.Scheme)
+	err = nbv1beta1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	// +kubebuilder:scaffold:scheme
