d3a0bcabaf8cf14361b90b00c909105f7b79e9f9
naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-11-21 15:46:06 -0500

    Fixes the incorrect RBAC permissions that are require to run notebook controller.

--- components/notebook-controller/controllers/notebook_controller.go
+++ components/notebook-controller/controllers/notebook_controller.go
@@ -74,10 +74,15 @@ type NotebookReconciler struct {
 
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=statefulsets/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=kubeflow.org,resources=notebooks,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=kubeflow.org,resources=notebooks/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=kubeflow.org,resources=notebooks/finalizers,verbs=*
+// +kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create
+// +kubebuilder:rbac:groups="networking.istio.io",resources=virtualservices,verbs=*
 
 func (r *NotebookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
