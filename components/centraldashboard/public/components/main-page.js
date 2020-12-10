9607c57a60451bb3e0defc64c03fea8d867a9f96
wg102 <wgaul102@uottawa.ca>
2020-12-10 15:55:45 -0500

    fix(centraldashboard): remove config map usage + artifact menu and quicklink

--- components/centraldashboard/public/components/main-page.js
+++ components/centraldashboard/public/components/main-page.js
@@ -193,10 +193,10 @@ export class MainPage extends utilitiesMixin(localizationMixin(PolymerElement)) 
             quickLinks,
             documentationItems,
         } = ev.detail.response;
-        this.menuLinks = menuLinks || [];
-        this.externalLinks = externalLinks || [];
-        this.quickLinks = quickLinks || [];
-        this.documentationItems = documentationItems || [];
+        // this.menuLinks = menuLinks || [];
+        // this.externalLinks = externalLinks || [];
+        // this.quickLinks = quickLinks || [];
+        // this.documentationItems = documentationItems || [];
     }
 
     /**
@@ -204,7 +204,7 @@ export class MainPage extends utilitiesMixin(localizationMixin(PolymerElement)) 
      * @param {Event} ev AJAX-response
      */
     _onHasWorkgroupError(ev) {
-        this.showError("mainPage.errGeneric");
+        this.showError('mainPage.errGeneric');
         return;
     }
 
