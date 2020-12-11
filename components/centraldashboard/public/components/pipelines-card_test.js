77c965c7b1999756ae224909666529bd291a5da5
wg102 <wgaul102@uottawa.ca>
2020-12-10 19:36:23 -0500

    fix(centraldashboard): fix pipeline tests

--- components/centraldashboard/public/components/pipelines-card_test.js
+++ components/centraldashboard/public/components/pipelines-card_test.js
@@ -7,10 +7,14 @@ import {mockRequest} from '../ajax_test_helper';
 
 const FIXTURE_ID = 'pipelines-card-fixture';
 const PIPELINES_CARD_ID = 'test-pipelines-card';
+const PIPELING_CARD_NAMESPACE = 'test-namespace';
 const TEMPLATE = `
 <test-fixture id="${FIXTURE_ID}">
   <template>
-    <pipelines-card id="${PIPELINES_CARD_ID}"></pipelines-card>
+    <pipelines-card 
+        id="${PIPELINES_CARD_ID}" 
+        namespace="${PIPELING_CARD_NAMESPACE}">
+    </pipelines-card>
   </template>
 </test-fixture>
 `;
@@ -50,8 +54,9 @@ describe('Pipelines Card', () => {
             status: 200,
             responseText: JSON.stringify({pipelines}),
         }, false, '/pipeline/apis/v1beta1/pipelines?' +
-                'page_size=5&sort_by=created_at%20desc');
-
+                'resource_reference_key.type=NAMESPACE' +
+                '&resource_reference_key.id=test-namespace' +
+                '&page_size=5&sort_by=created_at%20desc');
         pipelinesCard.artifactType = 'pipelines';
         await requestPromise;
         flush();
@@ -97,8 +102,9 @@ describe('Pipelines Card', () => {
             status: 200,
             responseText: JSON.stringify({runs}),
         }, false, '/pipeline/apis/v1beta1/runs?' +
-                'page_size=5&sort_by=created_at%20desc');
-
+                'resource_reference_key.type=NAMESPACE' +
+                '&resource_reference_key.id=test-namespace' +
+                '&page_size=5&sort_by=created_at%20desc');
         pipelinesCard.artifactType = 'runs';
         await requestPromise;
         flush();
@@ -144,7 +150,9 @@ describe('Pipelines Card', () => {
             status: 500,
             responseText: 'Some internal error',
         }, true, '/pipeline/apis/v1beta1/pipelines?' +
-                'page_size=5&sort_by=created_at%20desc');
+                'resource_reference_key.type=NAMESPACE' +
+                '&resource_reference_key.id=test-namespace' +
+                '&page_size=5&sort_by=created_at%20desc');
         pipelinesCard.artifactType = 'pipelines';
         await requestPromise;
         flush();
@@ -159,7 +167,9 @@ describe('Pipelines Card', () => {
             status: 500,
             responseText: 'Some internal error',
         }, true, '/pipeline/apis/v1beta1/runs?' +
-                'page_size=5&sort_by=created_at%20desc');
+                'resource_reference_key.type=NAMESPACE' +
+                '&resource_reference_key.id=test-namespace' +
+                '&page_size=5&sort_by=created_at%20desc');
         pipelinesCard.artifactType = 'runs';
         await requestPromise;
         flush();
@@ -174,7 +184,9 @@ describe('Pipelines Card', () => {
             status: 200,
             responseText: JSON.stringify({pipelines: []}),
         }, false, '/pipeline/apis/v1beta1/pipelines?' +
-                'page_size=5&sort_by=created_at%20desc');
+            'resource_reference_key.type=NAMESPACE' +
+            '&resource_reference_key.id=test-namespace' +
+            '&page_size=5&sort_by=created_at%20desc');
 
         pipelinesCard.artifactType = 'pipelines';
         await requestPromise;
