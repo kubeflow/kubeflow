749dab411fd8cc6b176574f058c36de9ae9d3599
DavidSpek <vanderspek.david@gmail.com>
2021-01-20 17:19:30 +0100

    add go.* search

--- create_dependabot.sh
+++ create_dependabot.sh
@@ -14,3 +14,7 @@ done
 for directory in $(dirname $(find . -type f -name "*requirements.txt" -not -path "./*node_modules*") | sort -u | cut -c2-); do
      yq eval -i ".updates += {\"package-ecosystem\":\"pip\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
 done
+
+for directory in $(dirname $(find . -type f -name "go.*" -not -path "./*node_modules*") | sort -u | cut -c2-); do
+     yq eval -i ".updates += {\"package-ecosystem\":\"gomod\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
+done
