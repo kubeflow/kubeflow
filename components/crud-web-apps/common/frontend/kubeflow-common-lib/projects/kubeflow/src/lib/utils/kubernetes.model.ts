81d12a8b9f3ab6c238ad7acb6926f1738083245f
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-01-28 21:19:21 +0200

    web-apps(front): Make k8s fields optional
    We might need to instantiate a k8s objecti with only some of the
    subfields, like only the spec and not metadata.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/utils/kubernetes.model.ts
+++ components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/utils/kubernetes.model.ts
@@ -1,8 +1,8 @@
 import { V1ObjectMeta } from '@kubernetes/client-node';
 export interface K8sObject {
-  apiVersion: string;
-  kind: string;
-  metadata: V1ObjectMeta;
-  spec: any;
-  status: any;
+  apiVersion?: string;
+  kind?: string;
+  metadata?: V1ObjectMeta;
+  spec?: any;
+  status?: any;
 }
