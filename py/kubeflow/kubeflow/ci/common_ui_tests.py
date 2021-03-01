52e6e199e04462d52c691f52e9a81c86f5557712
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-03-02 00:08:20 +0200

    CI: Correct the name of the workflow (#5653)
    * ci: Rename testing workflow
    
    The name of the workflow must be DNS 1123 compatible.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * ci: Create test file to trigger the test (will remove)
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * ci: Rename workflow to fit the 63 chars limit
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * ci: Add exit_handler task
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * ci: Remove dependencies from exit handler
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * ci: Use browserless/chrome for running the tests
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * ci: Rename from shared-ui to common-ui
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * remove test dummy file
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- py/kubeflow/kubeflow/ci/common_ui_tests.py
+++ py/kubeflow/kubeflow/ci/common_ui_tests.py
@@ -26,8 +26,9 @@ class Builder(workflow_utils.ArgoTestBuilder):
     def _create_ui_tests_task(self, task_template):
         ui_tests = argo_build_util.deep_copy(task_template)
 
-        ui_tests["name"] = "shared-ui-tests"
-        ui_tests["container"]["image"] = "node:12.20.1-stretch-slim"
+        img = "browserless/chrome:1.44-chrome-stable"
+        ui_tests["name"] = "common-ui-tests"
+        ui_tests["container"]["image"] = img
         ui_tests["container"]["command"] = ["npm"]
         ui_tests["container"]["args"] = ["run", "test-ci"]
 
@@ -40,10 +41,24 @@ class Builder(workflow_utils.ArgoTestBuilder):
     def _create_ui_build_task(self, task_template):
         ui_build = argo_build_util.deep_copy(task_template)
 
-        ui_build["name"] = "build-shared-ui-library"
+        ui_build["name"] = "build-common-ui-library"
         ui_build["container"]["image"] = "node:12.20.1-stretch-slim"
         ui_build["container"]["command"] = ["npm"]
         ui_build["container"]["args"] = ["run", "build"]
+
+        ui_dir = ("%s/components/crud-web-apps/common/"
+                  "frontend/kubeflow-common-lib/") % self.src_dir
+        ui_build["container"]["workingDir"] = ui_dir
+
+        return ui_build
+
+    def _create_exit_handler(self, task_template):
+        ui_build = argo_build_util.deep_copy(task_template)
+
+        ui_build["name"] = "rm-node-modules"
+        ui_build["container"]["image"] = "node:12.20.1-stretch-slim"
+        ui_build["container"]["command"] = ["rm"]
+        ui_build["container"]["args"] = ["-r", "node_modules"]
 
         ui_dir = ("%s/components/crud-web-apps/common/"
                   "frontend/kubeflow-common-lib/") % self.src_dir
@@ -68,11 +83,16 @@ class Builder(workflow_utils.ArgoTestBuilder):
                                         ui_tests_task,
                                         [modules_install_task["name"]])
 
-        # run common ui frontend tests
+        # build the node module from the lib source code
         build_step = self._create_ui_build_task(task_template)
         argo_build_util.add_task_to_dag(workflow, workflow_utils.E2E_DAG_NAME,
                                         build_step,
                                         [modules_install_task["name"]])
+
+        # EXIT-HANDLER: remove node_modules folder as exit handler
+        rm_node_modules = self._create_exit_handler(task_template)
+        argo_build_util.add_task_to_dag(workflow, workflow_utils.EXIT_DAG_NAME,
+                                        rm_node_modules, [])
 
         # Set the labels on all templates
         workflow = argo_build_util.set_task_template_labels(workflow)
