d33ad1a369748585a8ec682ff2172bb074414491
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-03-05 12:41:21 +0200

    cd(jwa): Add CD workflow for building jwa
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

---
+++ py/kubeflow/kubeflow/cd/jwa.py
@@ -0,0 +1,48 @@
+""""Argo Workflow for building Jupyter web app's OCI image using Kaniko"""
+import os
+
+from kubeflow.kubeflow import ci
+from kubeflow.testing import argo_build_util
+
+TAG = os.getenv("PULL_BASE_SHA", "kaniko-local-test")
+AWS_REGISTRY = "public.ecr.aws/j1r0q0g6/jupyter-web-app"
+
+
+class Builder(ci.workflow_utils.ArgoTestBuilder):
+    def __init__(self, name=None, namespace=None, bucket=None,
+                 test_target_name=None, **kwargs):
+        super().__init__(name=name, namespace=namespace, bucket=bucket,
+                         test_target_name=test_target_name, **kwargs)
+
+    def build(self):
+        """Build the Argo workflow graph"""
+        workflow = self.build_init_workflow(exit_dag=False)
+        task_template = self.build_task_template()
+
+        # Build JWA using Kaniko
+        dockerfile = ("%s/components/crud-web-apps"
+                      "/jupyter/Dockerfile") % self.src_dir
+        context = "dir://%s/components/crud-web-apps" % self.src_dir
+        destination = "%s:%s" % (AWS_REGISTRY, TAG)
+
+        kaniko_task = self.create_kaniko_task(task_template, dockerfile,
+                                              context, destination)
+        argo_build_util.add_task_to_dag(workflow,
+                                        ci.workflow_utils.E2E_DAG_NAME,
+                                        kaniko_task, [self.mkdir_task_name])
+
+        # Set the labels on all templates
+        workflow = argo_build_util.set_task_template_labels(workflow)
+
+        return workflow
+
+
+def create_workflow(name=None, namespace=None, bucket=None, **kwargs):
+    """
+    Args:
+        name: Name to give to the workflow. This can also be used to name
+              things associated with the workflow.
+    """
+    builder = Builder(name=name, namespace=namespace, bucket=bucket, **kwargs)
+
+    return builder.build()
