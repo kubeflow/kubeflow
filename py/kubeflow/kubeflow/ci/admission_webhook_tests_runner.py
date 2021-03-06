9a14011f085433311e0fd293e7e6c776c7aa59fb
DavidSpek <vanderspek.david@gmail.com>
2021-03-06 12:52:19 +0100

    move to postsubmit and add admission presubmit kaniko build test

---
+++ py/kubeflow/kubeflow/ci/admission_webhook_tests_runner.py
@@ -0,0 +1,13 @@
+# This file is only intended for development purposes
+import os
+
+import yaml
+
+from kubeflow.kubeflow.ci import admission_webhook_tests
+
+WORKFLOW_NAME = os.getenv("WORKFLOW_NAME", "admission-webhook-tests")
+WORKFLOW_NAMESPACE = os.getenv("WORKFLOW_NAMESPACE", "kubeflow-user")
+
+if __name__ == "__main__":
+    print(yaml.dump(admission_webhook_tests.create_workflow(WORKFLOW_NAME,
+                                                    WORKFLOW_NAMESPACE)))
