c502ac787dd19675d58eae253d2cd683fd2f405d
DavidSpek <vanderspek.david@gmail.com>
2021-03-06 12:16:51 +0100

    Fix typo in profile registry name

--- py/kubeflow/kubeflow/cd/config.py
+++ py/kubeflow/kubeflow/cd/config.py
@@ -8,6 +8,6 @@ CENTRAL_DASHBOARD_IMAGE = "%s/central-dashboard" % AWS_REGISTRY
 ADMISSION_WEBHOOK_IMAGE = "%s/admission-webhook" % AWS_REGISTRY
 ACCESS_MANAGEMENT_IMAGE = "%s/access-management" % AWS_REGISTRY
 
-PROFILE_CONTROLLER_IMAGE = "%s/profile_controller" % AWS_REGISTRY
+PROFILE_CONTROLLER_IMAGE = "%s/profile-controller" % AWS_REGISTRY
 NOTEBOOK_CONTROLLER_IMAGE = "%s/notebook-controller" % AWS_REGISTRY
 TENSORBOARD_CONTROLLER_IMAGE = "%s/tensorboard-controller" % AWS_REGISTRY
