80db1da0d5a659ace27e711cd84a35c5709a377f
Subreptivus <Subreptivus@gmail.com>
2021-03-15 14:13:37 +0200

    Somehow Binding interface gets changes while merging branches back and forth

--- components/access-management/kfam/model_binding.go
+++ components/access-management/kfam/model_binding.go
@@ -10,6 +10,16 @@
 
 package kfam
 
-type BindingEntries struct {
-	Bindings []Binding `json:"bindings,omitempty"`
+import rbacv1 "k8s.io/api/rbac/v1"
+
+// Binding will give user edit access to referredNamespace
+type Binding struct {
+	User *rbacv1.Subject `json:"user,omitempty"`
+
+	ReferredNamespace string `json:"referredNamespace,omitempty"`
+
+	RoleRef *rbacv1.RoleRef `json:"RoleRef,omitempty"`
+
+	// Status of the profile, one of Succeeded, Failed, Unknown.
+	Status string `json:"status,omitempty"`
 }
