87640503bc954c71425ce095f9fb88f65427d074
DavidSpek <vanderspek.david@gmail.com>
2021-03-17 12:31:13 +0100

    jwa(back): update spanwer yaml, dump logo from yaml if file doesn't exist

--- components/crud-web-apps/jupyter/backend/apps/common/utils.py
+++ components/crud-web-apps/jupyter/backend/apps/common/utils.py
@@ -39,6 +39,18 @@ def load_spawner_ui_config():
 
         if config_dict is not None:
             log.info("Using config file: %s", config)
+            VS_CODE_LOGO_VALUE = config_dict["spawnerFormDefaults"]["imageVSCode"].get("logo")
+            RSTUDIO_LOGO_VALUE = config_dict["spawnerFormDefaults"]["imageRStudio"].get("logo")
+            if VS_CODE_LOGO_VALUE:
+                VS_LOGO_PATH = os.path.join(FILE_ABS_PATH, '../default/static/assets/visual-studio-code.svg')
+                if not os.path.exists(VS_LOGO_PATH):
+                    with open(VS_LOGO_PATH, "w") as f:
+                        f.write(VS_CODE_LOGO_VALUE)
+            if RSTUDIO_LOGO_VALUE:
+                RSTUDIO_LOGO_PATH = os.path.join(FILE_ABS_PATH, '../default/static/assets/rstudio.svg')
+                if not os.path.exists(RSTUDIO_LOGO_PATH):
+                    with open(RSTUDIO_LOGO_PATH, "w") as f:
+                        f.write(RSTUDIO_LOGO_VALUE)
             return config_dict["spawnerFormDefaults"]
 
     log.error("Couldn't find any config file.")
@@ -118,10 +130,15 @@ def pvc_dict_from_k8s_obj(pvc):
 
 def notebook_dict_from_k8s_obj(notebook):
     cntr = notebook["spec"]["template"]["spec"]["containers"][0]
+    server_type = None
+    if notebook["metadata"].get("annotations"):
+        annotations = notebook["metadata"]["annotations"]
+        server_type = annotations.get("notebooks.kubeflow.org/server-type")
 
     return {
         "name": notebook["metadata"]["name"],
         "namespace": notebook["metadata"]["namespace"],
+        "serverType": server_type,
         "age": helpers.get_uptime(notebook["metadata"]["creationTimestamp"]),
         "image": cntr["image"],
         "shortImage": cntr["image"].split("/")[-1],
