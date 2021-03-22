7884084420dba80a457e069500836d60b8e30cb4
DavidSpek <vanderspek.david@gmail.com>
2021-03-22 08:32:17 +0100

    Add notebook-server-jupyter-scipy dockerfile and CI/CD (#5699)
    * Add notebook-server-jupyter-scipy dockerfile and CI/CD
    
    * cleanup jupyter scipy image
    
    Co-authored-by: Mathew Wicks <thesuperzapper@users.noreply.github.com>

---
+++ py/kubeflow/kubeflow/cd/notebook_servers/notebook_server_jupyter_scipy_runner.py
@@ -0,0 +1,5 @@
+# This file is only intended for development purposes
+from kubeflow.kubeflow.cd import base_runner
+
+base_runner.main(component_name="notebook_servers.notebook_server_jupyter_scipy",
+                 workflow_name="nb-j-sp-build")
