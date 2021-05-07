8774c2dbbe3bcecb7283cf62cbc6e9686a3d2bb3
juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
2021-05-07 14:40:35 +0200

    Update form-default.component.ts

--- components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-default/form-default.component.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-default/form-default.component.ts
@@ -123,6 +123,11 @@ export class FormDefaultComponent implements OnInit, OnDestroy {
     if (typeof notebook.cpu === 'number') {
       notebook.cpu = notebook.cpu.toString();
     }
+    
+    // Ensure GPU input is a string
+    if (typeof notebook.gpus.num === 'number') {
+      notebook.gpus.num = notebook.gpus.num.toString();
+    }
 
     // Remove cpuLimit from request if null
     if (notebook.cpuLimit == null) {
