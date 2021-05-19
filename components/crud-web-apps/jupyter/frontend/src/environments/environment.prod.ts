3f7d021e747982228bfeb6fd1a8f16dd3721a4a0
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-05-19 14:01:10 +0300

    jwa(front): Change logos fetch url
    Change the URLs of the logos from `static/assets/*` to
    `static/assets/logos`.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/crud-web-apps/jupyter/frontend/src/environments/environment.prod.ts
+++ components/crud-web-apps/jupyter/frontend/src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
   apiUrl: '/jupyter',
   resource: 'notebooks',
   ui: 'default',
-  jupyterlabLogo: 'static/assets/jupyterlab-logo.svg',
-  jupyterIcon: 'static/assets/jupyter-icon.svg',
-  groupOneLogo: 'static/assets/group-one-logo.svg',
-  groupOneIcon: 'static/assets/group-one-icon.svg',
-  groupTwoLogo: 'static/assets/group-two-logo.svg',
-  groupTwoIcon: 'static/assets/group-two-icon.svg',
+  jupyterlabLogo: 'static/assets/logos/jupyterlab-logo.svg',
+  jupyterIcon: 'static/assets/logos/jupyter-icon.svg',
+  groupOneLogo: 'static/assets/logos/group-one-logo.svg',
+  groupOneIcon: 'static/assets/logos/group-one-icon.svg',
+  groupTwoLogo: 'static/assets/logos/group-two-logo.svg',
+  groupTwoIcon: 'static/assets/logos/group-two-icon.svg',
 
   // Rok
   rokUrl: '',
