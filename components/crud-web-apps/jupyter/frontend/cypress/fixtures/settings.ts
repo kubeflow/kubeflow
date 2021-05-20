95bfd479a15872aea836d18c6d51782563364836
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-05-20 15:33:44 +0300

    jwa(e2e): Add first cypress tests
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

---
+++ components/crud-web-apps/jupyter/frontend/cypress/fixtures/settings.ts
@@ -0,0 +1,11 @@
+export const settings = {
+  // namespace to create test InferenceServices in
+  namespace: 'default',
+
+  // POST/PATCH/PUT/DELETE requests will need these headers since we are
+  // using the double submit cookie CSRF mechanism
+  postHeaders: {
+    'X-XSRF-Token': 'asdf',
+    Cookie: 'XSRF-TOKEN=asdf',
+  },
+};
