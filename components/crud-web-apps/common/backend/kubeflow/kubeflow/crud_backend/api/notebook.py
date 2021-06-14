a07f8f2be8e12450613bb7c98fd43b04072d3c66
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-06-14 16:13:12 +0300

    wa(back): Update kubernetes to 0.17
    In order to support dry-run we must use the 0.17 version of the Python
    k8s client.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    Reviewed-by: Ilias Katsakioris <elikatsis@arrikto.com>

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/notebook.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/notebook.py
@@ -1,5 +1,3 @@
-from kubernetes import client
-
 from .. import authz
 from . import custom_api, v1_core
 
@@ -37,12 +35,12 @@ def delete_notebook(notebook, namespace):
         "delete", "kubeflow.org", "v1beta1", "notebooks", namespace
     )
     return custom_api.delete_namespaced_custom_object(
-        "kubeflow.org",
-        "v1beta1",
-        namespace,
-        "notebooks",
-        notebook,
-        client.V1DeleteOptions(propagation_policy="Foreground"),
+        group="kubeflow.org",
+        version="v1beta1",
+        namespace=namespace,
+        plural="notebooks",
+        name=notebook,
+        propagation_policy="Foreground",
     )
 
 
