e037f2616923aa57001d1ced23e8ebb9401c10a1
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-06-14 12:55:14 +0300

    wa(back): Add dry run to Notebooks and PVCs
    The backend will need to be able to create objects with dry-run, in
    order to ensure they are valid. The backend will need to check that both
    the Notebook and the PVCs can be created beforehand.
    
    This way we avoid the scenario where we create PVCs but the Notebook
    fails to be created, and the PVCs are never garbage collected.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    Reviewed-by: Ilias Katsakioris <elikatsis@arrikto.com>

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/notebook.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/notebook.py
@@ -13,13 +13,14 @@ def get_notebook(notebook, namespace):
     )
 
 
-def create_notebook(notebook, namespace):
+def create_notebook(notebook, namespace, dry_run=False):
     authz.ensure_authorized(
         "create", "kubeflow.org", "v1beta1", "notebooks", namespace
     )
+
     return custom_api.create_namespaced_custom_object(
-        "kubeflow.org", "v1beta1", namespace, "notebooks", notebook
-    )
+        "kubeflow.org", "v1beta1", namespace, "notebooks", notebook,
+        dry_run="All" if dry_run else None)
 
 
 def list_notebooks(namespace):
