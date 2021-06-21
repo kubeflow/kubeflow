3e0c7f139fcc0f5111ab7be4a476250eb5f1037c
DavidSpek <vanderspek.david@gmail.com>
2021-06-21 19:17:35 +0200

    common: update material, cdk and cdk-experimental to 9
    ng update @angular/cdk@9 @angular/material@9 @angular/cdk-experimental@9

--- components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/snack-bar/component/snack-bar.component.ts
+++ components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/snack-bar/component/snack-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, EventEmitter } from '@angular/core';
-import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material';
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
 import { SnackType } from '../types';
 
 @Component({
