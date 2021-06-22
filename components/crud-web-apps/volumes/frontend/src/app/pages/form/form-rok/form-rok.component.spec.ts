549418d10e0b0f097ba64ad3f83f211909965683
DavidSpek <vanderspek.david@gmail.com>
2021-06-22 13:21:14 +0200

    volumes: update angular core and cli to 11
    ng update @angular/core@11 @angular/cli@11

--- components/crud-web-apps/volumes/frontend/src/app/pages/form/form-rok/form-rok.component.spec.ts
+++ components/crud-web-apps/volumes/frontend/src/app/pages/form/form-rok/form-rok.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FormRokComponent } from './form-rok.component';
 
@@ -6,7 +6,7 @@ describe('FormRokComponent', () => {
   let component: FormRokComponent;
   let fixture: ComponentFixture<FormRokComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [FormRokComponent],
     }).compileComponents();
