9d35df3b38e8d10f4ea436944b7e7c7dbf3c8fc7
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-07-22 17:07:24 +0300

    jwa(front): i18n App
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/crud-web-apps/jupyter/frontend/src/app/app.module.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/app.module.ts
@@ -9,21 +9,12 @@ import { IndexModule } from './pages/index/index.module';
 import { FormModule } from './pages/form/form.module';
 import { KubeflowModule } from 'kubeflow';
 
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient],
-      },
-    }),
     BrowserModule,
     AppRoutingModule,
     CommonModule,
@@ -35,7 +26,3 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'static/assets/i18n/', '.json');
-}
