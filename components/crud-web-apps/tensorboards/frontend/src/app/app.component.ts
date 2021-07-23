940aff955a9c784135260a4b9436a1eb6285c725
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-07-23 14:57:50 +0300

    twa(front): i18n App
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/crud-web-apps/tensorboards/frontend/src/app/app.component.ts
+++ components/crud-web-apps/tensorboards/frontend/src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private translate: TranslateService) {
-    const currentLanguage = this.translate.getBrowserLang();
-    const lang = currentLanguage.match(/en|fr/) ? currentLanguage : 'en';
-    translate.setDefaultLang(lang);
-  }
-}
+export class AppComponent {}
