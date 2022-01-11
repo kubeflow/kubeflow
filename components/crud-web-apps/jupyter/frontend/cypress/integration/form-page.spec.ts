ed0ed5f5e18fe8f720853bce8b9d5acc50de7362
Tasos Alexiou <tasos@arrikto.com>
2022-01-11 12:35:59 +0200

    jwa(front): Add UI tests for volume's mount path input
    Signed-off-by: Tasos Alexiou <tasos@arrikto.com>

---
+++ components/crud-web-apps/jupyter/frontend/cypress/integration/form-page.spec.ts
@@ -0,0 +1,50 @@
+describe('New notebook form', () => {
+  beforeEach(() => {});
+
+  it('should have a "New notebook" title', () => {
+    cy.visit('/new');
+    cy.get('[data-cy-toolbar-title]').contains('New notebook').should('exist');
+  });
+
+  it('should auto update mount value when name change', () => {
+    cy.get('[data-cy="add new volume"]').click();
+
+    cy.get('.last[data-cy="data volumes"]').click();
+
+    cy.get('.last[data-cy="data volumes"]')
+      .find('[data-cy="volume name input"]')
+      .type('new-volume-name')
+      .then($nameInput => {
+        const nameValue = $nameInput.val();
+        cy.get('.last[data-cy="data volumes"]')
+          .find('[data-cy="mount path"]')
+          .should($mountInput => {
+            const mountValue = $mountInput.val();
+            expect(mountValue).equal(`/home/jovyan/${nameValue}`);
+          });
+      });
+  });
+
+  it('should not auto update mount value when it is dirty', () => {
+    cy.get('[data-cy="add new volume"]').click();
+
+    cy.get('.last[data-cy="data volumes"]').click();
+
+    cy.get('.last[data-cy="data volumes"]')
+      .find('[data-cy="mount path"]')
+      .type('dirty');
+
+    cy.get('.last[data-cy="data volumes"]')
+      .find('[data-cy="volume name input"]')
+      .type('new-volume-name')
+      .then($nameInput => {
+        const nameValue = $nameInput.val();
+        cy.get('.last[data-cy="data volumes"]')
+          .find('[data-cy="mount path"]')
+          .should($mountInput => {
+            const mountValue = $mountInput.val();
+            expect(mountValue).not.equal(`/home/jovyan/${nameValue}`);
+          });
+      });
+  });
+});
