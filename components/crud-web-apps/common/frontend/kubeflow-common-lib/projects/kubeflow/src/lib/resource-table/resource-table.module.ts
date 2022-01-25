de99bb4ee9dd7a008cc647f123acb61982bf99f7
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2022-01-25 15:59:40 +0200

    fix unit tests failing
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/resource-table/resource-table.module.ts
+++ components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/resource-table/resource-table.module.ts
@@ -22,10 +22,12 @@ import { PopoverModule } from '../popover/popover.module';
 import { TableChipsListComponent } from './chips-list/chips-list.component';
 import { ComponentValueComponent } from './component-value/component-value.component';
 import { PortalModule } from '@angular/cdk/portal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
     MatTableModule,
     MatTooltipModule,
     MatProgressSpinnerModule,
