752cfd50db70d0ceafef11c210f2e30593a52443
Javy <xujavy@gmail.com>
2022-06-15 16:06:39 +0800

    MODIFIED update common libs for crud web apps

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/secret.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/secret.py
@@ -8,9 +8,25 @@ def get_secret(namespace, name, auth=True):
 
     return v1_core.read_namespaced_secret(name, namespace)
 
+def list_secrets(namespace, auth=True):
+    if auth:
+        authz.ensure_authorized("list", "", "v1", "secrets", namespace)
+    return v1_core.list_namespaced_secret(namespace)
 
 def create_secret(namespace, secret, auth=True):
     if auth:
         authz.ensure_authorized("create", "", "v1", "secrets", namespace)
 
     return v1_core.create_namespaced_secret(namespace, secret)
+
+def delete_secret(namespace, secret, auth=True):
+    if auth:
+        authz.ensure_authorized("delete", "", "v1", "secrets", namespace)
+
+    return v1_core.delete_namespaced_secret(secret, namespace)
+
+def patch_secret(name, namespace, secret, auth=True):
+    if auth:
+        authz.ensure_authorized("patch", "", "v1", "secrets", namespace)
+
+    return v1_core.patch_namespaced_secret(name, namespace, secret)
