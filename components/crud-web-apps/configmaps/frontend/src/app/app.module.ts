a6e30c10504bed1ca5af133e4a7c51393c899b6b
Javy <xujavy@gmail.com>
2022-06-16 15:43:25 +0800

    FUTURE fist commit configmaps project

---
+++ components/crud-web-apps/configmaps/frontend/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ErrorStateMatcher } from '@angular/material/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {
+  ResourceTableModule,
+  NamespaceSelectModule,
+  ConfirmDialogModule,
+  FormModule,
+  ImmediateErrorStateMatcher,
+  KubeflowModule,
+} from 'kubeflow';
+
+import { IndexComponent } from './pages/index/index.component';
+import { FormDefaultComponent } from './pages/form/form-default/form-default.component';
+import { IndexDefaultComponent } from './pages/index/index-default/index-default.component';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { YamlsModule } from './pages/form/yamls/yamls.module';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    FormDefaultComponent,
+    IndexDefaultComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    ResourceTableModule,
+    NamespaceSelectModule,
+    ConfirmDialogModule,
+    FormModule,
+    MatTabsModule,
+    YamlsModule,
+    KubeflowModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: ErrorStateMatcher, useClass: ImmediateErrorStateMatcher },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
