2418828133b5c7d79d73dba4cbe949cb55fe9487
Javy <xujavy@gmail.com>
2022-06-17 18:33:04 +0800

    FUTURE fist commit configuration project

---
+++ components/crud-web-apps/configurations/frontend/src/app/pages/index/index-default/index-default.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { IndexDefaultComponent } from './index-default.component';
+
+describe('IndexDefaultComponent', () => {
+  let component: IndexDefaultComponent;
+  let fixture: ComponentFixture<IndexDefaultComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [IndexDefaultComponent],
+      }).compileComponents();
+    }),
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IndexDefaultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
