9d69326a4ff73730e0726c250854895182d3a645
Javy <xujavy@gmail.com>
2022-06-17 18:33:04 +0800

    FUTURE fist commit configuration project

---
+++ components/crud-web-apps/configurations/backend/apps/default/routes/post.py
@@ -0,0 +1,44 @@
+from flask import request
+
+from kubeflow.kubeflow.crud_backend import api, decorators, logging
+
+from ...common import form
+from . import bp
+
+log = logging.getLogger(__name__)
+
+
+@bp.route("/api/namespaces/<namespace>/configuration", methods=["POST"])
+@decorators.request_is_json_type
+@decorators.required_body_params("name", "labels", "annotations", "desc",
+                                 "env", "volumeMounts", "volumes")
+def post_configuration(namespace):
+    body = request.get_json()
+    log.info("Received body: %s", body)
+
+    configuration = form.poddefault_from_dict(body, namespace)
+    log.info("Received configuration: %s", configuration)
+
+    log.info("Creating Configuration '...")
+    api.create_poddefault(configuration, namespace)
+    log.info("Successfully created Configuration %s/%s", namespace, body["name"])
+
+    return api.success_response("message", "Configuration created successfully.")
+
+
+@bp.route("/api/namespaces/<namespace>/configuration", methods=["PATCH"])
+@decorators.request_is_json_type
+@decorators.required_body_params("name", "labels", "annotations", "desc",
+                                 "env", "volumeMounts", "volumes")
+def patch_configuration(namespace):
+    body = request.get_json()
+    log.info("Received body: %s", body)
+
+    configuration = form.poddefault_from_dict(body, namespace)
+    log.info("Received configuration: %s", configuration)
+
+    log.info("Creating Configuration '...")
+    api.patch_poddefault(body["name"], namespace, configuration)
+    log.info("Successfully update Configuration %s/%s", namespace, body["name"])
+
+    return api.success_response("message", "Configuration update successfully.")
