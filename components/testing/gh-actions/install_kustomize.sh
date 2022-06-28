ea282cceb83b983214019928afa1c9cf17a467be
Nikolaos Loukas <nickloukas@arrikto.com>
2022-06-28 16:19:05 +0300

    Add scripts for all the KinD GH actions.

---
+++ components/testing/gh-actions/install_kustomize.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl --silent --location --remote-name "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.2.3/kustomize_kustomize.v3.2.3_linux_amd64"
+chmod a+x kustomize_kustomize.v3.2.3_linux_amd64
+sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
