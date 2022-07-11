a3feaaceeca166df1995ab30e377c642fc608cbd
Javy <xujavy@gmail.com>
2022-07-11 10:46:50 +0800

    MODIFIED add s3 data accelerate module

---
+++ components/crud-web-apps/datasets/backend/apps/common/form.py
@@ -0,0 +1,108 @@
+
+def dataset_from_dict(body, namespace):
+    """
+    body: json object (frontend json data)
+
+    Convert the Dataset json object that is sent from the backend to a python
+    client Dataset instance.
+    """
+    
+    dataset = {
+        "apiVersion": "data.fluid.io/v1alpha1",
+        "kind": "Dataset",
+        "metadata":{
+            "name": body["name"],
+            "namesapce": namespace,
+        },
+        "spec":{
+            "mounts":[
+                {
+                    "mountPoint": body["mountPoint"],
+                    "name": body["name"],
+                    "options": {
+                        "bucket": body["name"],
+                        "storage": body["storage"],
+                    },
+                    "encryptOptions": [
+                        {
+                            "name": "metaurl",
+                            "valueFrom": {
+                                "secretKeyRef": {
+                                    "key": "metaurl",
+                                    "name": "jfs-secret"
+                                }
+                            }
+                        },
+                        {
+                            "name": "access-key",
+                            "valueFrom": {
+                                "secretKeyRef": {
+                                    "key": "access-key",
+                                    "name": "jfs-secret"
+                                }
+                            }
+                        },
+                        {
+                            "name": "secret-key",
+                            "valueFrom": {
+                                "secretKeyRef": {
+                                    "key": "secret-key",
+                                    "name": "jfs-secret"
+                                }
+                            }
+                        }
+                ],
+                }
+            ]
+        }
+    }
+    return dataset
+
+def juicefsruntime_from_dict(body, namespace):
+    """
+    body: json object (frontend json data)
+
+    Convert the JuiceFSRuntime json object that is sent from the backend to a python
+    client JuiceFSRuntime instance.
+    """
+    
+    juicefsruntime = {
+        "apiVersion": "data.fluid.io/v1alpha1",
+        "kind": "JuiceFSRuntime",
+        "metadata":{
+            "name": body["name"],
+            "namesapce": namespace,
+        },
+        "spec":{
+            "fuse": {
+                "resources": {}
+            },
+            "initUsers": {
+                "resources": {}
+            },
+            "jobWorker": {
+                "resources": {}
+            },
+            "juicefsVersion": {},
+            "master": {
+                "resources": {}
+            },
+            "replicas": 1,
+            "tieredstore":{
+                "levels":[
+                    {
+                       "mediumtype": body["mediumType"],
+                       "path": body["path"],
+                       "quota": body["quotaSize"],
+                       "high": "0.95",
+                       "low": "0.7"
+                    }
+                ]
+            },
+            "worker": {
+                "resources": {}
+            }
+        }
+    }
+    
+    return juicefsruntime
---
+++ components/crud-web-apps/datasets/backend/apps/common/utils.py
@@ -0,0 +1,28 @@
+from kubeflow.kubeflow.crud_backend import helpers, api
+
+def parse_s3_accerlerate(juicefsruntime):
+    """
+    dataset: custom.Dataset
+
+    Process the Dataset and format it as the UI expects it.
+    """
+    
+    dataset = api.get_dynamic_custom_rsrc("data.fluid.io", "v1alpha1", "Dataset", 
+                                          juicefsruntime["metadata"]["namespace"], 
+                                          juicefsruntime["metadata"]["name"])
+
+    parsed_dataset = {
+        "name": juicefsruntime["metadata"]["name"],
+        "namespace": juicefsruntime["metadata"]["namespace"],
+        "age": {
+            "uptime": helpers.get_uptime(juicefsruntime["metadata"]["creationTimestamp"]),
+            "timestamp": helpers.get_uptime(juicefsruntime["metadata"]["creationTimestamp"])
+        },
+        "bucket": dataset["spec"]["mounts"][0]["options"]["bucket"],
+        "storage": dataset["spec"]["mounts"][0]["options"]["storage"],
+        "mediumType": juicefsruntime["spec"]["tieredstore"]["levels"][0]["mediumtype"],
+        "quotaSize": juicefsruntime["spec"]["tieredstore"]["levels"][0]["quota"],
+        "path": juicefsruntime["spec"]["tieredstore"]["levels"][0]["path"],
+    }
+
+    return parsed_dataset
