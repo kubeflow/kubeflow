54006e18180840d8bc11a4ec32a608645083a2cb
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2022-07-14 15:32:33 +0300

    Support K8s 1.22 in profile controller (#6491)
    * profile-controller: Delete old kubebuilder v1 project
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: Initialize Kubebuilder v3 project
    
    go module name:
    github.com/kubeflow/kubeflow/components/profile-controller
    
    project domain:
    kubeflow.org
    
    Changes:
    
    * Go Version got upgraded from v1.15 to v1.17
    * With Kubebuilder V3 our project dependencies got
    updated to a more up-to-date version.
    * controller-runtime library got updated from v0.2.0 to v0.12.1.
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: create placeholder API types and controller logic files
    
    We created the Profile API types
    v1: kubebuilder create api --version v1 kind Profile
    v1beta1: kubebuilder create api --version v1beta1 Profile
    
    We copied over the API definitions from the old project
    to the new one
    
    (Note: Currently KFAM uses the v1beta1 version of Profile
    to provide fine-grain user-namespace level access control)
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: copy controllers logic from old project to the new one
    
    * Copied the files under controller/ dir from the old to our new project
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: Code modifications to fix profile-controller
    
    With Kubebuilder V3 the dependencies of our project got updated
    to a more up-to-date version. Newer versions of certain packages
    resulted to broken code parts that we had to fix. More specifically:
    
    * In this project we are using a namespace-labels-data ConfigMap
    to set certain labels on every Profile namespace. Every change in
    this ConfigMap produces an event and for every event we construct
    a list of reconcile.Requests that contains the name and namespace
    of every Profile in our system.
    
    Thus we introduced a mapEventToRequest MapFunc (profile_controller.go)
    that maps an event to reconcile requests for all Profiles. This change
    was necessary because controller-runtime library got updated from v0.6.3
    to v0.12.1
    
    * Kubebuilder V3 introduced for the profile-controller:
    1) a livenessprobe (/healthz) on port :8081
    2) a readinessProbe (/readyz) on port :8081
    
    We have a conflict here as KFAM Service uses the :8081 port as well.
    Therefore, we switched the port to :9876 for the above Probes.
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: Fix manifests, Makefile, Dockerfile
    
    Makefile:
    * Add image (build + push) target
    * Add test-overlays target
    * Build image using public.ecr.aws registry instead of gcr.io.
    
    Dockerfile:
    * fix Dockerfile to build our controller correclty
    
    Manifests
    * Add base/ and overlays/ manifests
    * Fix the manifests to be in sync with upstream
    
    other changes:
    Update README.md file
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: Add test files
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>
    
    * profile-controller: Fix Makefile
    
    * Set the correct IMG:TAG
    * Remove test dependency
    
    Signed-off-by: Apotolos Gerakaris <apoger@arrikto.com>

--- components/profile-controller/main.go
+++ components/profile-controller/main.go
@@ -1,4 +1,5 @@
 /*
+Copyright 2022.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,15 +20,22 @@ import (
 	"flag"
 	"os"
 
-	profilev1 "github.com/kubeflow/kubeflow/components/profile-controller/api/v1"
-	"github.com/kubeflow/kubeflow/components/profile-controller/controllers"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
 	istioSecurityClient "istio.io/client-go/pkg/apis/security/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	// +kubebuilder:scaffold:imports
+
+	profilev1 "github.com/kubeflow/kubeflow/components/profile-controller/api/v1"
+	kubefloworgv1beta1 "github.com/kubeflow/kubeflow/components/profile-controller/api/v1beta1"
+	"github.com/kubeflow/kubeflow/components/profile-controller/controllers"
+	//+kubebuilder:scaffold:imports
 )
 
 const USERIDHEADER = "userid-header"
@@ -41,41 +49,47 @@ var (
 )
 
 func init() {
-	_ = clientgoscheme.AddToScheme(scheme)
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	_ = profilev1.AddToScheme(scheme)
-	_ = istioSecurityClient.AddToScheme(scheme)
-	// +kubebuilder:scaffold:scheme
+	utilruntime.Must(profilev1.AddToScheme(scheme))
+	utilruntime.Must(istioSecurityClient.AddToScheme(scheme))
+	utilruntime.Must(kubefloworgv1beta1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
 }
 
 func main() {
 	var metricsAddr, leaderElectionNamespace string
 	var enableLeaderElection bool
+	var probeAddr string
 	var userIdHeader string
 	var userIdPrefix string
 	var workloadIdentity string
 	var defaultNamespaceLabelsPath string
-
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":9876", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&leaderElectionNamespace, "leader-election-namespace", "",
 		"Determines the namespace in which the leader election configmap will be created.")
 	flag.StringVar(&userIdHeader, USERIDHEADER, "x-goog-authenticated-user-email", "Key of request header containing user id")
 	flag.StringVar(&userIdPrefix, USERIDPREFIX, "accounts.google.com:", "Request header user id common prefix")
 	flag.StringVar(&workloadIdentity, WORKLOADIDENTITY, "", "Default identity (GCP service account) for workload_identity plugin")
 	flag.StringVar(&defaultNamespaceLabelsPath, DEFAULTNAMESPACELABELSPATH, "/etc/profile-controller/namespace-labels.yaml", "A YAML file with a map of labels to be set on every Profile namespace")
-
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.Logger(true))
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                  scheme,
-		MetricsBindAddress:      metricsAddr,
-		LeaderElection:          enableLeaderElection,
-		LeaderElectionNamespace: leaderElectionNamespace,
-		LeaderElectionID:        "kubeflow-profile-controller",
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "kubeflow-profile-controller",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -94,7 +108,16 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Profile")
 		os.Exit(1)
 	}
-	// +kubebuilder:scaffold:builder
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
 
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
