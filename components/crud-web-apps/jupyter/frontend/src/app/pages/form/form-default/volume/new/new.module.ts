69d190b046dc99db1f31ae32dca1dee5c9fdd2e7
Orfeas Kourkakis <orfeas@arrikto.com>
2022-07-15 13:51:33 +0300

    jwa(front): Use new Editor component
    Import new Editor component from Kubeflow Common Library and replace
    all instances of previous Ace Editor.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-default/volume/new/new.module.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-default/volume/new/new.module.ts
@@ -5,12 +5,12 @@ import { VolumeNameModule } from './name/name.module';
 import { StorageClassModule } from './storage-class/storage-class.module';
 import { VolumeAccessModesModule } from './access-modes/access-modes.module';
 import { VolumeSizeModule } from './size/size.module';
-import { AceEditorModule } from 'ng2-ace-editor';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { RokUrlModule } from './rok-url/rok-url.module';
+import { EditorModule } from 'kubeflow';
 
 @NgModule({
   declarations: [NewVolumeComponent],
@@ -20,12 +20,12 @@ import { RokUrlModule } from './rok-url/rok-url.module';
     StorageClassModule,
     VolumeAccessModesModule,
     VolumeSizeModule,
-    AceEditorModule,
     MatInputModule,
     MatSelectModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     RokUrlModule,
+    EditorModule,
   ],
   exports: [NewVolumeComponent],
 })
