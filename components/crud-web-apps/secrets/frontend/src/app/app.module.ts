3b9419054b737bf1945ce95e5dffd2fe66e05910
Javy <xujavy@gmail.com>
2022-07-18 11:51:34 +0800

    MODIFIED update secret module

--- components/crud-web-apps/secrets/frontend/src/app/app.module.ts
+++ components/crud-web-apps/secrets/frontend/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { IndexDefaultComponent } from './pages/index/index-default/index-default
 import { MatTabsModule } from '@angular/material/tabs';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { YamlsModule } from './pages/form/yamls/yamls.module';
+import { ListKeyValueModule } from './pages/form/list-key-value/list-key-value.module';
 
 
 import { HttpClientModule } from '@angular/common/http';
@@ -45,6 +46,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatTabsModule,
     MatCheckboxModule,
     YamlsModule,
+    ListKeyValueModule,
     KubeflowModule,
     HttpClientModule,
   ],
