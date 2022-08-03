7431f57625ed4c471092bdb2921b2b13e991a46a
Orfeas Kourkakis <orfeas@arrikto.com>
2022-08-03 18:37:28 +0300

    jwa(back): Get a notebook's logs
    Add logic in the backend to fetch the logs of a notebook's
    underlying pod.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/pod.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/pod.py
@@ -10,7 +10,7 @@ def list_pods(namespace, auth=True, label_selector = None):
 
 def get_pod_logs(namespace, pod, container, auth=True):
     if auth:
-        authz.ensure_authorized("read", "", "v1", "pods", namespace, "logs")
+        authz.ensure_authorized("get", "", "v1", "pods", namespace, "log")
 
     return v1_core.read_namespaced_pod_log(
         namespace=namespace, name=pod, container=container
