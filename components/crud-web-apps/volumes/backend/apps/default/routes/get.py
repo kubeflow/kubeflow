b353b246f89aaab6a62f89a14c3643b4eb73a9ca
Orfeas Kourkakis <orfeas@arrikto.com>
2022-08-16 14:00:09 +0300

    vwa(back): Get a single PVC and the pods using it
    Add logic in the backend to fetch a single PVC and the pods using
    it.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/crud-web-apps/volumes/backend/apps/default/routes/get.py
+++ components/crud-web-apps/volumes/backend/apps/default/routes/get.py
@@ -13,3 +13,14 @@ def get_pvcs(namespace):
     content = [utils.parse_pvc(pvc) for pvc in pvcs.items]
 
     return api.success_response("pvcs", content)
+
+@bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>")
+def get_pvc(namespace, pvc_name):
+    pvc = api.get_pvc(pvc_name, namespace)
+    return api.success_response("pvc", api.serialize(pvc))
+
+@bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>/pods")
+def get_pvc_pods(namespace, pvc_name):
+    pods = utils.get_pods_using_pvc(pvc_name, namespace)
+
+    return api.success_response("pods", api.serialize(pods))
