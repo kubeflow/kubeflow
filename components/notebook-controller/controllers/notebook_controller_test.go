fd4f42b2b380ff82a968accd1a3ff7f6913e6f91
Apostolos Gerakaris <apoger@arrikto.com>
2022-08-29 16:22:03 +0300

    notebook-controller: Add unit tests
    *Introduce basic unit tests for "createNotebookStatus" function
    *Add GH action for unit tests
    
    Signed-off-by: Apostolos Gerakaris apoger@arrikto.com

--- components/notebook-controller/controllers/notebook_controller_test.go
+++ components/notebook-controller/controllers/notebook_controller_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"reflect"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -11,6 +12,9 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
+
+	nbv1beta1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1beta1"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 func TestNbNameFromInvolvedObject(t *testing.T) {
@@ -83,4 +87,166 @@ func TestNbNameFromInvolvedObject(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCreateNotebookStatus(t *testing.T) {
+
+	tests := []struct {
+		name             string
+		currentNb        nbv1beta1.Notebook
+		pod              corev1.Pod
+		sts              appsv1.StatefulSet
+		expectedNbStatus nbv1beta1.NotebookStatus
+	}{
+		{
+			name: "NotebookStatusInitialization",
+			currentNb: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			pod: corev1.Pod{},
+			sts: appsv1.StatefulSet{},
+			expectedNbStatus: nbv1beta1.NotebookStatus{
+				Conditions:     []nbv1beta1.NotebookCondition{},
+				ReadyReplicas:  int32(0),
+				ContainerState: corev1.ContainerState{},
+			},
+		},
+		{
+			name: "NotebookStatusReadyReplicas",
+			currentNb: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			pod: corev1.Pod{},
+			sts: appsv1.StatefulSet{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: appsv1.StatefulSetStatus{
+					ReadyReplicas: int32(1),
+				},
+			},
+			expectedNbStatus: nbv1beta1.NotebookStatus{
+				Conditions:     []nbv1beta1.NotebookCondition{},
+				ReadyReplicas:  int32(1),
+				ContainerState: corev1.ContainerState{},
+			},
+		},
+		{
+			name: "NotebookContainerState",
+			currentNb: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			pod: corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: corev1.PodStatus{
+					ContainerStatuses: []corev1.ContainerStatus{
+						{
+							Name: "test",
+							State: corev1.ContainerState{
+								Running: &corev1.ContainerStateRunning{
+									StartedAt: v1.Time{},
+								},
+							},
+						},
+					},
+				},
+			},
+			sts: appsv1.StatefulSet{},
+			expectedNbStatus: nbv1beta1.NotebookStatus{
+				Conditions:    []nbv1beta1.NotebookCondition{},
+				ReadyReplicas: int32(0),
+				ContainerState: corev1.ContainerState{
+					Running: &corev1.ContainerStateRunning{
+						StartedAt: v1.Time{},
+					},
+				},
+			},
+		},
+		{
+			name: "mirroringPodConditions",
+			pod: corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: corev1.PodStatus{
+					Conditions: []corev1.PodCondition{
+						{
+							Type:          "Running",
+							LastProbeTime: v1.Time{},
+						},
+						{
+							Type:          "Waiting",
+							LastProbeTime: v1.Time{},
+							Reason:        "PodInitializing",
+						},
+					},
+				},
+			},
+			sts: appsv1.StatefulSet{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: appsv1.StatefulSetStatus{
+					ReadyReplicas: int32(1),
+				},
+			},
+			expectedNbStatus: nbv1beta1.NotebookStatus{
+				Conditions: []nbv1beta1.NotebookCondition{
+					{
+						Type:          "Running",
+						LastProbeTime: v1.Time{},
+					},
+					{
+						Type:          "Waiting",
+						LastProbeTime: v1.Time{},
+						Reason:        "PodInitializing",
+					},
+				},
+				ReadyReplicas:  int32(1),
+				ContainerState: corev1.ContainerState{},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r := createMockReconciler()
+			req := ctrl.Request{}
+			status, err := createNotebookStatus(r, &test.currentNb, &test.sts, &test.pod, req)
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if !reflect.DeepEqual(status, test.expectedNbStatus) {
+				t.Errorf("Expect:\n%v; Output:\n%v", test.expectedNbStatus, status)
+			}
+			// t.Logf("Success:%v\n", status)
+		})
+	}
+
+}
+
+func createMockReconciler() *NotebookReconciler {
+	reconciler := &NotebookReconciler{
+		Scheme: runtime.NewScheme(),
+		Log:    ctrl.Log,
+	}
+	return reconciler
 }
