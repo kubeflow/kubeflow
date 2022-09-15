fdf9c66a8fb81a681e35200a1d0b03f6ede59b5b
Orfeas Kourkakis <orfeas@arrikto.com>
2022-09-15 15:41:48 +0300

    vwa(back): Append notebooks using each PVC
    Append notebooks using each PVC when getting and parsing all PVCs.
    
    Refs arrikto/dev#2017
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>
    Reviewed-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- components/crud-web-apps/volumes/backend/apps/rok/routes/get.py
+++ components/crud-web-apps/volumes/backend/apps/rok/routes/get.py
@@ -21,20 +21,24 @@ def get_pvcs(namespace):
 
     # Return the list of PVCs and the corresponding Viewer's state
     pvcs = api.list_pvcs(namespace)
-    content = [utils.parse_pvc(pvc, viewers) for pvc in pvcs.items]
+    notebooks = api.list_notebooks(namespace)["items"]
+    content = [utils.parse_pvc(pvc, viewers, notebooks) for pvc in pvcs.items]
 
     return api.success_response("pvcs", content)
+
 
 @bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>")
 def get_pvc(namespace, pvc_name):
     pvc = api.get_pvc(pvc_name, namespace)
     return api.success_response("pvc", api.serialize(pvc))
 
+
 @bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>/pods")
 def get_pvc_pods(namespace, pvc_name):
     pods = get_pods_using_pvc(pvc_name, namespace)
 
     return api.success_response("pods", api.serialize(pods))
+
 
 @bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>/events")
 def get_pvc_events(namespace, pvc_name):
