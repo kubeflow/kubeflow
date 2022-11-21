88e1b81c417438bd5dc569b45e5798238a628307
Tasos Alexiou <tasos@arrikto.com>
2022-11-21 11:44:13 +0200

    centraldashboard: Add support for apps in new windows (#6750)
    Add support for apps opened in new windows in the dashboard's
    library. These apps can now communicate with the dashboard and
    receive events with the new namespace when it changes.
    
    Signed-off-by: Tasos Alexiou <tasos@arrikto.com>
    
    Signed-off-by: Tasos Alexiou <tasos@arrikto.com>

--- components/centraldashboard/public/library.js
+++ components/centraldashboard/public/library.js
@@ -3,7 +3,7 @@
  * Central Dashboard.
  */
 export const PARENT_CONNECTED_EVENT = 'parent-connected';
-export const IFRAME_CONNECTED_EVENT = 'iframe-connected';
+export const APP_CONNECTED_EVENT = 'iframe-connected';
 export const NAMESPACE_SELECTED_EVENT = 'namespace-selected';
 export const ALL_NAMESPACES_EVENT = 'all-namespaces';
 export const MESSAGE = 'message';
@@ -17,6 +17,7 @@ class CentralDashboardEventHandler_ {
     constructor() {
         this.window = window;
         this.parent = window.parent;
+        this.opener = window.opener;
         this._messageEventListener = null;
         this._onParentConnected = null;
         this._onNamespaceSelected = null;
@@ -32,14 +33,13 @@ class CentralDashboardEventHandler_ {
      *  is set to true, forcing every app to be iframed, set this flag to avoid
      *  the redirection.
      */
-    init(callback, disableForceIframe=false) {
-        const isIframed = this.window.location !== this.parent.location;
-        callback(this, isIframed);
-        if (isIframed) {
-            this._messageEventListener = this._onMessageReceived.bind(this);
-            this.window.addEventListener(MESSAGE, this._messageEventListener);
-            this.parent.postMessage({type: IFRAME_CONNECTED_EVENT},
-                this.parent.origin);
+    init(callback, disableForceIframe = false) {
+        callback(this, this.isIframed || this.isOpenedByApp);
+
+        if (this.isOpenedByApp) {
+            this._attachListenerToIframe();
+        } else if (this.isIframed) {
+            this._attachListenerToDashboard();
         } else if (!disableForceIframe) {
             fetch('/api/dashboard-settings')
                 .then((res) => res.json())
@@ -61,7 +61,16 @@ class CentralDashboardEventHandler_ {
      * Removes the message event listener.
      */
     detach() {
-        if (this._messageEventListener) {
+        if (!this._messageEventListener) {
+            return;
+        }
+
+        if (this.isOpenedByApp) {
+            this.opener.removeEventListener(MESSAGE,
+                this._messageEventListener);
+        }
+
+        if (this.isIframed) {
             this.window.removeEventListener(MESSAGE,
                 this._messageEventListener);
         }
@@ -103,6 +112,14 @@ class CentralDashboardEventHandler_ {
         }
     }
 
+    get isIframed() {
+        return this.window.location !== this.parent.location;
+    }
+
+    get isOpenedByApp() {
+        return this.opener !== null;
+    }
+
     /**
      * Handle the receipt of a message and dispatch to any added callbacks.
      * @param {MessageEvent} event
@@ -126,6 +143,41 @@ class CentralDashboardEventHandler_ {
             }
             break;
         }
+    }
+
+    /**
+     * If the app is iframed then it should add an eventListener on its own
+     * window. The CentralDashboard will be sending NAMESPACE_SELECTED_EVENT
+     * events to the iframed window.
+     */
+    _attachListenerToDashboard() {
+        this._messageEventListener = this._onMessageReceived.bind(this);
+        this.window.addEventListener(MESSAGE, this._messageEventListener);
+        this.parent.postMessage({type: APP_CONNECTED_EVENT},
+            this.parent.origin);
+    }
+
+    /**
+     * If the app is opened by another app, then it should add an eventListener
+     * to its opener window. We expect the opener window to be iframed in the
+     * CentralDashboard.
+     */
+    _attachListenerToIframe() {
+        this._messageEventListener = this._onMessageReceived.bind(this);
+        this.opener.addEventListener(MESSAGE,
+            this._messageEventListener);
+
+        // Notify the Dashboard that an app connected, in order for Dashboard
+        // to propagate the selected namespace. We expect the dasbhoard to be
+        // in the opener's parent window.
+        this.opener.parent.postMessage({type: APP_CONNECTED_EVENT},
+            this.opener.parent.origin);
+
+        // Remove event listener when the window closes in order to avoid
+        // being called with wrong context.
+        this.window.addEventListener('beforeunload', (evt) => {
+            this.detach();
+        });
     }
 }
 
