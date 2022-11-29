f3265ba4aaadd04279a29bdf8a5e48981684fb9f
Orfeas Kourkakis <orfeas@arrikto.com>
2022-11-29 09:59:35 +0200

    web-apps(back): Backend fetch pods fix
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/pod.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/pod.py
@@ -6,10 +6,7 @@ def list_pods(namespace, auth=True, label_selector = None):
     if auth:
         authz.ensure_authorized("list", "", "v1", "pods", namespace)
 
-    if label_selector is None:
-        return v1_core.list_namespaced_pod(namespace)
-    else:
-        return v1_core.list_namespaced_pod(namespace = namespace, label_selector = label_selector)
+    return v1_core.list_namespaced_pod(namespace = namespace, label_selector = label_selector)
 
 def get_pod_logs(namespace, pod, container, auth=True):
     if auth:
