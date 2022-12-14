26e4569083748e7ce9a0aa7b8de8e80022174e4d
apoger <apoger@arrikto.com>
2022-12-14 13:42:29 +0200

    poddefaults: Extend tests for using images of each PR (#6836)
    * poddefaults: Extend tests for using images of each PR
    
    Changes:
    
    * Introduce intergration test workflow
    * Publish Docker image only when PR is merged
    * Remove kind & manifest gh-action workflows
    * Update tag in manifests to v1.6.0
      - This change is required as images with tag v1.5.0 do not
        exist in "docker.io/kubeflownotebookswg" registry.
    
    Signed-off-by: Apostolos Gerakaris <apoger@arrikto.com>
    
    * Introduce bash script to install cert-manager
    
    Signed-off-by: Apostolos Gerakaris <apoger@arrikto.com>
    
    * review: Fix cert-manager installation script
    
    Signed-off-by: Apostolos Gerakaris <apoger@arrikto.com>
    
    Signed-off-by: Apostolos Gerakaris <apoger@arrikto.com>

---
+++ components/testing/gh-actions/install_cert_manager.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+echo "Installing cert-manager ..."
+CERT_MANAGER_VERSION="v1.10.1"
+mkdir cert_tmp
+pushd cert_tmp > /dev/null
+    OS=$(go env GOOS); ARCH=$(go env GOARCH); \
+        curl -L -o cmctl.tar.gz https://github.com/cert-manager/cert-manager/releases/download/$CERT_MANAGER_VERSION/cmctl-$OS-$ARCH.tar.gz
+    tar xzf cmctl.tar.gz
+    sudo mv cmctl /usr/local/bin
+    cmctl x install
+popd
+echo "Waiting for cert-manager to be ready ..."
+kubectl wait --for=condition=ready pod -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
