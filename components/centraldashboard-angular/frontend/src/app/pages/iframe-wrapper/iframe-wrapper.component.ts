06e60ed1a0b9478140349dedd3c9d7ed78cdd274
Orfeas Kourkakis <orfeas@arrikto.com>
2022-12-14 17:11:53 +0200

    cdb-angular(front): Sync Browser and Iframe URLs
     - Detect changes in iframe's internal URL by checking in time
       intervals. Upon change, modify the browser's (Router) URL to be the
       same as the iframe's one.
     - Update iframe's src property based on route events.
       - Modify iframe's src property setter by appending/removing the
         location origin, in order for the iframe to reload even when
         passing the same value to src.
       - Update the src property only when the browser's URL and the
         iframe's URL are different in order to prevent the iframe from
         reloading on every user click.
     - Send manually a postMessage with a hardcoded namespace
       'kubeflow-user' in order to be able to test the WAs until we
       implement a functionality for handling namespaces.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/centraldashboard-angular/frontend/src/app/pages/iframe-wrapper/iframe-wrapper.component.ts
+++ components/centraldashboard-angular/frontend/src/app/pages/iframe-wrapper/iframe-wrapper.component.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-iframe-wrapper',
   templateUrl: './iframe-wrapper.component.html',
-  styleUrls: ['./iframe-wrapper.component.scss']
+  styleUrls: ['./iframe-wrapper.component.scss'],
 })
-export class IframeWrapperComponent implements OnInit {
+export class IframeWrapperComponent implements AfterViewInit {
+  @ViewChild('iframe') iframe: ElementRef<HTMLIFrameElement>;
 
-  constructor() { }
+  public prvSrcPath: string;
+  get srcPath(): string {
+    return this.prvSrcPath;
+  }
+  set srcPath(src: string) {
+    /*
+     * The following hacky logic ensures that the Iframe will reload,
+     * even when it receives values for src that are the same with the
+     * one it already has. This is because, in order to avoid the iframe
+     * reloading constantly, its src is not updated on each navigation
+     * a user does.
+     */
+    if (window.location.origin) {
+      if (!this.prvSrcPath?.includes(window.location.origin)) {
+        src = window.location.origin + src;
+      }
+    }
 
-  ngOnInit(): void {
+    this.prvSrcPath = src;
+    // Some KF distributions need a trailing slash "/" in order to resolve paths
+    this.prvSrcPath += this.prvSrcPath?.endsWith('/') ? '' : '/';
+  }
+  public iframeLocation: string | undefined = 'about:blank';
+  private urlSub: Subscription;
+  private interval: any;
+
+  constructor(private router: Router) {
+    this.urlSub = this.router.events.subscribe(event => {
+      if (!(event instanceof NavigationEnd)) {
+        return;
+      }
+
+      const iframeWindow = this.iframe?.nativeElement?.contentWindow;
+      let iframeUrl = iframeWindow?.location.pathname;
+      if (iframeUrl) {
+        // Include URL's query parameters
+        iframeUrl += iframeWindow?.location.search;
+      }
+
+      if (!this.equalUrlPaths(event.url, iframeUrl)) {
+        this.srcPath = event.url;
+      }
+    });
   }
 
+  equalUrlPaths(firstUrl: string, secondUrl: string | undefined) {
+    if (!firstUrl && !secondUrl) {
+      console.warn(`Got undefined URLs ${firstUrl} and ${secondUrl}`);
+      return true;
+    }
+    if (!firstUrl || !secondUrl) {
+      return false;
+    }
+    firstUrl += firstUrl?.endsWith('/') ? '' : '/';
+    secondUrl += secondUrl?.endsWith('/') ? '' : '/';
+    return firstUrl === secondUrl;
+  }
+
+  ngAfterViewInit() {
+    this.interval = setInterval(() => {
+      let currentUrl = this.iframe?.nativeElement?.contentWindow?.location.href;
+
+      if (currentUrl !== this.iframeLocation) {
+        this.iframeLocation = currentUrl;
+        const path =
+          this.iframe?.nativeElement?.contentWindow?.location.pathname;
+        const queryParams = this.getQueryParams(
+          this.iframe?.nativeElement?.contentWindow?.location.search,
+        );
+        this.router.navigate([path], { queryParams });
+      }
+    }, 100);
+  }
+
+  getQueryParams(locationSearch: string | undefined): {
+    [key: string]: string;
+  } {
+    const searchParams = new URLSearchParams(locationSearch);
+    const queryParams: { [key: string]: string } = {};
+    searchParams.forEach((value, key) => {
+      queryParams[key] = value;
+    });
+    return queryParams;
+  }
+
+  ngOnDestroy() {
+    if (this.urlSub) {
+      this.urlSub.unsubscribe();
+    }
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  onLoad(ev: Event) {
+    setTimeout(() => {
+      this.iframe?.nativeElement?.contentWindow?.postMessage(
+        { type: 'namespace-selected', value: 'kubeflow-user' },
+        '*',
+      );
+    }, 4000);
+  }
 }
