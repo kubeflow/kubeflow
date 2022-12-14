8be8fb7780e1d04a3fde2d15483cbe37316169da
Orfeas Kourkakis <orfeas@arrikto.com>
2022-12-14 17:11:20 +0200

    cdb-angular(front): Add JWA and VWA in the iframe
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/centraldashboard-angular/frontend/src/app/pages/iframe-wrapper/iframe-wrapper.component.spec.ts
+++ components/centraldashboard-angular/frontend/src/app/pages/iframe-wrapper/iframe-wrapper.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SafePipe } from 'src/app/pipes/safe.pipe';
 
 import { IframeWrapperComponent } from './iframe-wrapper.component';
 
@@ -8,7 +10,8 @@ describe('IframeWrapperComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [IframeWrapperComponent],
+      imports: [RouterTestingModule],
+      declarations: [IframeWrapperComponent, SafePipe],
     }).compileComponents();
   });
 
