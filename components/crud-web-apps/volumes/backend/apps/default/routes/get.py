d5cc0b4305efac28850bc7bfe2c9cad842acce36
Tobias Goerke <tobias.goerke@gmail.com>
2023-01-06 10:56:16 +0100

    Reducing diff on get.py

--- components/crud-web-apps/volumes/backend/apps/default/routes/get.py
+++ components/crud-web-apps/volumes/backend/apps/default/routes/get.py
@@ -27,19 +27,16 @@ def get_pvcs(namespace):
 
     return api.success_response("pvcs", content)
 
-
 @bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>")
 def get_pvc(namespace, pvc_name):
     pvc = api.get_pvc(pvc_name, namespace)
     return api.success_response("pvc", api.serialize(pvc))
-
 
 @bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>/pods")
 def get_pvc_pods(namespace, pvc_name):
     pods = utils.get_pods_using_pvc(pvc_name, namespace)
 
     return api.success_response("pods", api.serialize(pods))
-
 
 @bp.route("/api/namespaces/<namespace>/pvcs/<pvc_name>/events")
 def get_pvc_events(namespace, pvc_name):
