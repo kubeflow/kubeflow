3f50581a76d5626bd61ebe16596779026b014c37
Orfeas Kourkakis <orfeas@arrikto.com>
2023-01-11 17:43:56 +0200

    cdb-angular(front): Fix query parameters bug
     - Modify appendBackslash to append the backslash at the end of the URL
    path but always before the query parameters.
     - Add/modify comments for clearness and readability
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/centraldashboard-angular/frontend/src/app/pages/iframe-wrapper/iframe-wrapper.component.ts
+++ components/centraldashboard-angular/frontend/src/app/pages/iframe-wrapper/iframe-wrapper.component.ts
@@ -21,33 +21,48 @@ export class IframeWrapperComponent implements AfterViewInit, OnDestroy {
     return this.prvSrcPath;
   }
   set srcPath(src: string) {
-    /*
-     * The following hacky logic ensures that the Iframe will reload,
-     * even when it receives values for src that are the same with the
-     * one it already has. This is because, in order to avoid the iframe
-     * reloading constantly, its src is not updated on each navigation
-     * a user does.
-     */
     src = this.removePrefixFrom(src);
-
-    if (window.location.origin) {
-      if (!this.prvSrcPath?.includes(window.location.origin)) {
-        src = window.location.origin + src;
-      }
-    }
 
     /**
      * When Istio exports Services, it always expects
      * a '/' at the end. SO we'll need to make sure the
      * links propagated to the iframe end with a '/'
      */
-    this.prvSrcPath = this.appendBackslash(src);
+    src = this.appendBackslash(src);
+
+    /**
+     * The following hacky logic appends the window.location.origin
+     * when it is not present in the current src value. This is to
+     * force-reload the iframe even when it receives the same value
+     * with the one it already has. This is useful in the following
+     * sub-case scenarios when we don't update the src value in order
+     * to prevent the iframe from reloading:
+     *  - User is in a WA and by clicking inside this WA, they navigate
+     * to another WA. Then they want to move back to the previous WA by
+     * using its left sidebar link.
+     *  - User navigates inside a WA and wants to navigate to the WA's
+     * index page by using its left sidebar link.
+     */
+    if (!this.prvSrcPath?.includes(window.location.origin)) {
+      src = window.location.origin + src;
+    }
+
+    this.prvSrcPath = src;
   }
+
   public iframeLocation: string | undefined = 'about:blank';
   private urlSub: Subscription;
   private interval: any;
 
   constructor(private router: Router) {
+    /**
+     * On router events, we want to ensure that:
+     *  - the iframe's src won't be updated when the URLs of the
+     * browser and the iframe are the same in order to prevent
+     * the iframe from reloading on every user click.
+     *  - the iframe's src will be updated when the user clicks
+     * on the left sidebar links or navigates directly to a URL.
+     */
     this.urlSub = this.router.events.subscribe(event => {
       if (!(event instanceof NavigationEnd)) {
         return;
@@ -71,10 +86,9 @@ export class IframeWrapperComponent implements AfterViewInit, OnDestroy {
 
   /**
    * We treat URLs with or without a trailing slash as the same
-   * URL. Thus, in order to compare URLs, we need to append
-   * a '/' at the end of both URLs if there is none to avoid
-   * false statements in cases where they only differ in the
-   * trailing slash.
+   * URL. Thus, in order to compare URLs, we need to use
+   * appendBackslash for both URLS to avoid false statements
+   * in cases where they only differ in the trailing slash.
    */
   equalUrlPaths(firstUrl: string, secondUrl: string | undefined) {
     if (!firstUrl && !secondUrl) {
@@ -89,12 +103,30 @@ export class IframeWrapperComponent implements AfterViewInit, OnDestroy {
     return firstUrl === secondUrl;
   }
 
+  /**
+   * Appends a trailing slash either at the end of the URL
+   * or at the end of path, just before query parameters
+   */
   appendBackslash(url: string): string {
-    url += url?.endsWith('/') ? '' : '/';
-    return url;
+    const href = window.location.origin + url;
+    const urlObject = new URL(href);
+
+    let urlPath = urlObject.pathname;
+    urlPath += urlPath?.endsWith('/') ? '' : '/';
+    const urlParams = urlObject.search;
+
+    return urlPath + urlParams;
   }
 
   ngAfterViewInit() {
+    /**
+     * We check every 100ms for changes in the Iframe's location.href,
+     * In the case that this URL has a new value (that is different
+     * from the stored one), we use router.navigate to mirror the new
+     * URL in the browser's one. This works because Angular recognizes
+     * that the same component (IframeComponent) will be used for the next
+     * route as well (except if we navigate to Home) and doesn't reload.
+     */
     this.interval = setInterval(() => {
       const iframeWindow = this.iframe?.nativeElement?.contentWindow;
       let currentUrl = iframeWindow?.location.href;
