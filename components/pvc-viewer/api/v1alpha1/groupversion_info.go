938b0857d2964201a425f80fb463198c4e824a9a
Tobias Goerke <tobias.goerke@gmail.com>
2023-01-20 17:36:31 +0100

    Renaming file names

--- components/pvc-viewer/api/v1alpha1/groupversion_info.go
+++ components/pvc-viewer/api/v1alpha1/groupversion_info.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha1 contains API Schema definitions for the  v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=kubeflow.org
 package v1alpha1
