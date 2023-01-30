ac11461ff5d2ced009b6de18a719a7220a95dfef
Orfeas Kourkakis <orfeas@arrikto.com>
2023-01-30 12:40:26 +0200

    cdb-angular(front): Introduce backend service and ErrorInterceptor
     - Introduce backend service to handle requests to the backend
     - Use snackBar service by KF common library
     - Introduce ErrorInterceptor to hanlde errors in a uniform way.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

---
+++ components/centraldashboard-angular/frontend/src/app/services/backend.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CDBBackendService {
+  constructor(private http: HttpClient) {}
+}
