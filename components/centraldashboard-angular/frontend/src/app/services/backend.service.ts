2a70bd162351eeae3a4ead02d9c30ed6d23a0559
Orfeas Kourkakis <orfeas@arrikto.com>
2023-01-30 12:56:49 +0200

    cdb-angular(front): Show KF version in the left sidebar
    Show KF version at the bottom of the left sidebar navigation menu.
    Variables that are used to show the KF version get their default values
    during build time but may be modified during runtime by the
    depoloyment's ENV vars fetched from the request at
    '/api/workflow/env-info'.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/centraldashboard-angular/frontend/src/app/services/backend.service.ts
+++ components/centraldashboard-angular/frontend/src/app/services/backend.service.ts
@@ -1,10 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { envInfo } from '../types/env-info';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CDBBackendService {
   constructor(private http: HttpClient) {}
+
+  public getEnvInfo(): Observable<envInfo> {
+    const url = `api/workgroup/env-info`;
+
+    return this.http.get<envInfo>(url);
+  }
 }
