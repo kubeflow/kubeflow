7db9c95136ac640777eddaf28ccaf39e28d975c1
Orfeas Kourkakis <orfeas@arrikto.com>
2023-02-01 14:59:31 +0200

    cdb-angular(front): Add UI tests (#6895)
    * centraldashboard(front): Install Cypress
    
     - Install Cypress & npm scripts for UI tests
     - Update frontend README.md with UI tests instructions
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>
    
    * centraldashboard-angular(front): Add UI tests with Cypress
    
    Add UI tests with Cypress to check that the URLs of the browser and the
    iframe are always synced. More specifically, we test the following
    scenarios:
    
     - User navigates to pages inside the same WA (JWA)
     - User navigates to another WA from the one they were previously in (to
       the corresponding PVC from JWA details page)
     - User clicks and affects the query parameters of the current URL
       (navigates to tabs inside JWA details page)
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>
    
    * cdb-angular(front): Modify script names
    
    Modify script names in order to conform with script names in other WAs.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>
    
    * gh-actions(cdb-angular): Add UI tests to CDB's frontend workflow
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>
    
    ---------
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

---
+++ components/centraldashboard-angular/frontend/src/app/shared/utils.ts
@@ -0,0 +1,32 @@
+/**
+ * We treat URLs with or without a trailing slash as the same
+ * URL. Thus, in order to compare URLs, we need to use
+ * appendBackslash for both URLS to avoid false statements
+ * in cases where they only differ in the trailing slash.
+ */
+export function equalUrlPaths(firstUrl: string, secondUrl: string | undefined) {
+  if (!firstUrl || !secondUrl) {
+    return false;
+  }
+  firstUrl = appendBackslash(firstUrl);
+  secondUrl = appendBackslash(secondUrl);
+  return firstUrl === secondUrl;
+}
+
+/**
+ * Appends a trailing slash either at the end of the URL
+ * or at the end of path, just before query parameters
+ */
+export function appendBackslash(url: string): string {
+  const urlObject = new URL(url, window.location.origin);
+
+  let urlPath = urlObject.pathname;
+  urlPath += urlPath?.endsWith('/') ? '' : '/';
+  const urlParams = urlObject.search;
+
+  return urlPath + urlParams;
+}
+
+export function removePrefixFrom(url: string) {
+  return url.includes('/_') ? url.slice(2) : url;
+}
