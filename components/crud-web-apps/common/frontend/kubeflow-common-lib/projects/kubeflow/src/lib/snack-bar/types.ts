e32fc00e2e2678af3d6b7b0c0fe4ae9777f7e7bf
Orfeas Kourkakis <orfeas@arrikto.com>
2023-02-07 14:41:12 +0200

    web-apps(front): Modify SnackBar service to use config parameter
    Modify SnackBarService to:
    
     - Accept as a parameter a config object that will be able to include
       all possible configurations MatSnackBarService
     - Inject MAT_SNACK_BAR_DEFAULT_OPTIONS. This allows a structured way
       for every WA to provide to the service a default configuration
       app-wide for values like like HorizontalPosition and duration.
    
    Signed-off-by: Orfeas Kourkakis <orfeas@arrikto.com>

--- components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/snack-bar/types.ts
+++ components/crud-web-apps/common/frontend/kubeflow-common-lib/projects/kubeflow/src/lib/snack-bar/types.ts
@@ -1,6 +1,15 @@
+import { MatSnackBarConfig } from '@angular/material/snack-bar';
+
 export enum SnackType {
   Success = 'SUCCESS',
   Error = 'ERROR',
   Warning = 'WARNING',
   Info = 'INFO',
+}
+
+export interface SnackBarConfig extends MatSnackBarConfig {
+  data: {
+    msg: string;
+    snackType?: SnackType;
+  };
 }
