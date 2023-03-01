ea626e234554cab9846724039fa516b9336a1ffe
Apostolos Gerakaris <apoger@arrikto.com>
2023-03-01 21:56:42 +0200

    Unit tests
    * Fix unit test for createNotebookStatus func
    * Introduce tests for checkWarningEvents func
    
    Signed-off-by: Apostolos Gerakaris <apoger@arrikto.com>

--- components/notebook-controller/controllers/notebook_controller_test.go
+++ components/notebook-controller/controllers/notebook_controller_test.go
@@ -6,15 +6,19 @@ import (
 	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 
-	nbv1beta1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1beta1"
+	"github.com/google/go-cmp/cmp"
+	v1beta1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -94,38 +98,38 @@ func TestCreateNotebookStatus(t *testing.T) {
 
 	tests := []struct {
 		name             string
-		currentNb        nbv1beta1.Notebook
+		currentNb        v1beta1.Notebook
 		pod              corev1.Pod
 		sts              appsv1.StatefulSet
-		expectedNbStatus nbv1beta1.NotebookStatus
+		expectedNbStatus v1beta1.NotebookStatus
 	}{
 		{
-			name: "NotebookStatusInitialization",
-			currentNb: nbv1beta1.Notebook{
-				ObjectMeta: v1.ObjectMeta{
-					Name:      "test",
-					Namespace: "kubeflow-user",
-				},
-				Status: nbv1beta1.NotebookStatus{},
-			},
-			pod: corev1.Pod{},
-			sts: appsv1.StatefulSet{},
-			expectedNbStatus: nbv1beta1.NotebookStatus{
-				Conditions:     []nbv1beta1.NotebookCondition{},
-				ReadyReplicas:  int32(0),
-				ContainerState: corev1.ContainerState{},
-			},
-		},
-		{
 			name: "NotebookStatusReadyReplicas",
-			currentNb: nbv1beta1.Notebook{
+			currentNb: v1beta1.Notebook{
 				ObjectMeta: v1.ObjectMeta{
 					Name:      "test",
 					Namespace: "kubeflow-user",
 				},
-				Status: nbv1beta1.NotebookStatus{},
+				Status: v1beta1.NotebookStatus{},
 			},
-			pod: corev1.Pod{},
+			pod: corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: corev1.PodStatus{
+					ContainerStatuses: []corev1.ContainerStatus{
+						{
+							Name: "test",
+							State: corev1.ContainerState{
+								Running: &corev1.ContainerStateRunning{
+									StartedAt: v1.Time{},
+								},
+							},
+						},
+					},
+				},
+			},
 			sts: appsv1.StatefulSet{
 				ObjectMeta: v1.ObjectMeta{
 					Name:      "test",
@@ -135,20 +139,24 @@ func TestCreateNotebookStatus(t *testing.T) {
 					ReadyReplicas: int32(1),
 				},
 			},
-			expectedNbStatus: nbv1beta1.NotebookStatus{
-				Conditions:     []nbv1beta1.NotebookCondition{},
-				ReadyReplicas:  int32(1),
-				ContainerState: corev1.ContainerState{},
+			expectedNbStatus: v1beta1.NotebookStatus{
+				Conditions:    []v1beta1.NotebookCondition{},
+				ReadyReplicas: int32(1),
+				ContainerState: corev1.ContainerState{
+					Running: &corev1.ContainerStateRunning{
+						StartedAt: v1.Time{},
+					},
+				},
 			},
 		},
 		{
 			name: "NotebookContainerState",
-			currentNb: nbv1beta1.Notebook{
+			currentNb: v1beta1.Notebook{
 				ObjectMeta: v1.ObjectMeta{
 					Name:      "test",
 					Namespace: "kubeflow-user",
 				},
-				Status: nbv1beta1.NotebookStatus{},
+				Status: v1beta1.NotebookStatus{},
 			},
 			pod: corev1.Pod{
 				ObjectMeta: v1.ObjectMeta{
@@ -169,8 +177,8 @@ func TestCreateNotebookStatus(t *testing.T) {
 				},
 			},
 			sts: appsv1.StatefulSet{},
-			expectedNbStatus: nbv1beta1.NotebookStatus{
-				Conditions:    []nbv1beta1.NotebookCondition{},
+			expectedNbStatus: v1beta1.NotebookStatus{
+				Conditions:    []v1beta1.NotebookCondition{},
 				ReadyReplicas: int32(0),
 				ContainerState: corev1.ContainerState{
 					Running: &corev1.ContainerStateRunning{
@@ -211,8 +219,8 @@ func TestCreateNotebookStatus(t *testing.T) {
 					ReadyReplicas: int32(1),
 				},
 			},
-			expectedNbStatus: nbv1beta1.NotebookStatus{
-				Conditions: []nbv1beta1.NotebookCondition{
+			expectedNbStatus: v1beta1.NotebookStatus{
+				Conditions: []v1beta1.NotebookCondition{
 					{
 						Type:               "Running",
 						LastProbeTime:      v1.Date(2022, time.Month(8), 30, 1, 10, 30, 0, time.UTC),
@@ -256,8 +264,8 @@ func TestCreateNotebookStatus(t *testing.T) {
 				},
 				Status: appsv1.StatefulSetStatus{},
 			},
-			expectedNbStatus: nbv1beta1.NotebookStatus{
-				Conditions: []nbv1beta1.NotebookCondition{
+			expectedNbStatus: v1beta1.NotebookStatus{
+				Conditions: []v1beta1.NotebookCondition{
 					{
 						Type:               "PodScheduled",
 						LastProbeTime:      v1.Date(2022, time.Month(4), 21, 1, 10, 30, 0, time.UTC),
@@ -273,9 +281,16 @@ func TestCreateNotebookStatus(t *testing.T) {
 		},
 	}
 
+	// create fake client
+	fakeClient := fake.NewFakeClientWithScheme(scheme.Scheme)
+
+	r := &NotebookReconciler{
+		Client: fakeClient,
+		Log:    ctrl.Log.WithName("controllers").WithName("Notebook"),
+	}
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			r := createMockReconciler()
 			req := ctrl.Request{}
 			status, err := createNotebookStatus(r, &test.currentNb, &test.sts, &test.pod, req)
 			if err != nil {
@@ -289,10 +304,161 @@ func TestCreateNotebookStatus(t *testing.T) {
 
 }
 
-func createMockReconciler() *NotebookReconciler {
-	reconciler := &NotebookReconciler{
-		Scheme: runtime.NewScheme(),
-		Log:    ctrl.Log,
+func TestCheckWarningEvents(t *testing.T) {
+	// Define the test data
+	testCases := []struct {
+		name          string
+		annotations   map[string]string
+		events        []corev1.Event
+		expectedState v1beta1.NotebookStatus
+	}{
+		{
+			name: "No warning events",
+			events: []corev1.Event{
+				{
+					ObjectMeta:     metav1.ObjectMeta{Name: "test-event", Namespace: "test-namespace"},
+					InvolvedObject: corev1.ObjectReference{Kind: "Notebook", Name: "test-notebook", Namespace: "test-namespace"},
+					Reason:         "",
+					Message:        "",
+					FirstTimestamp: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					LastTimestamp:  v1.Time{},
+					Type:           "Normal",
+				},
+			},
+			expectedState: v1beta1.NotebookStatus{
+				Conditions:     []v1beta1.NotebookCondition{},
+				ReadyReplicas:  int32(0),
+				ContainerState: corev1.ContainerState{},
+			},
+		},
+		{
+			name: "1 warning event",
+			events: []corev1.Event{
+				{
+					ObjectMeta:     metav1.ObjectMeta{Name: "test-event-1", Namespace: "test-namespace"},
+					InvolvedObject: corev1.ObjectReference{Kind: "Notebook", Name: "test-notebook", Namespace: "test-namespace"},
+					Reason:         "FailedCreate",
+					Message:        "Failed to create StatefulSet due to invalid configuration.",
+					FirstTimestamp: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					LastTimestamp:  v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					Type:           "Warning",
+				},
+				{
+					ObjectMeta:     metav1.ObjectMeta{Name: "test-event-2", Namespace: "test-namespace"},
+					InvolvedObject: corev1.ObjectReference{Kind: "Notebook", Name: "test-notebook", Namespace: "test-namespace"},
+					Reason:         "",
+					Message:        "",
+					FirstTimestamp: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					LastTimestamp:  v1.Time{},
+					Type:           "Normal",
+				},
+			},
+			expectedState: v1beta1.NotebookStatus{
+				Conditions: []v1beta1.NotebookCondition{
+					{
+						Type:               "Warning",
+						LastProbeTime:      v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+						LastTransitionTime: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+						Message:            "Failed to create StatefulSet due to invalid configuration.",
+						Status:             "",
+						Reason:             "FailedCreate",
+					}},
+				ReadyReplicas:  int32(0),
+				ContainerState: corev1.ContainerState{},
+			},
+		},
+		{
+			name: "Duplicate warning events",
+			events: []corev1.Event{
+				{
+					ObjectMeta:     metav1.ObjectMeta{Name: "event1", Namespace: "test-namespace"},
+					InvolvedObject: corev1.ObjectReference{Kind: "Notebook", Name: "test-notebook", Namespace: "test-namespace"},
+					Reason:         "FailedCreate",
+					Message:        "Failed to create StatefulSet due to invalid configuration.",
+					FirstTimestamp: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					LastTimestamp:  v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					Type:           "Warning",
+				},
+				{
+					ObjectMeta:     metav1.ObjectMeta{Name: "event2", Namespace: "test-namespace"},
+					InvolvedObject: corev1.ObjectReference{Kind: "Notebook", Name: "test-notebook", Namespace: "test-namespace"},
+					Reason:         "FailedCreate",
+					Message:        "Failed to create StatefulSet due to invalid configuration.",
+					FirstTimestamp: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					LastTimestamp:  v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+					Type:           "Warning",
+				},
+			},
+			expectedState: v1beta1.NotebookStatus{
+				Conditions: []v1beta1.NotebookCondition{
+					{
+						Type:               "Warning",
+						LastProbeTime:      v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+						LastTransitionTime: v1.Date(2023, time.Month(3), 1, 1, 10, 30, 0, time.UTC),
+						Message:            "Failed to create StatefulSet due to invalid configuration.",
+						Status:             "",
+						Reason:             "FailedCreate",
+					}},
+				ReadyReplicas:  int32(0),
+				ContainerState: corev1.ContainerState{},
+			},
+		},
 	}
-	return reconciler
+
+	// Add the v1beta1 (Notebooks) and corev1 (Events) API groups to the scheme
+	// before creating the fake client
+	scheme := runtime.NewScheme()
+	v1beta1.AddToScheme(scheme)
+	corev1.AddToScheme(scheme)
+
+	// Loop over the test cases
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a fake client with the test data
+			builder := fake.NewClientBuilder().WithScheme(scheme)
+			nb := &v1beta1.Notebook{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: tt.annotations,
+					Name:        "test-notebook",
+					Namespace:   "test-namespace",
+				},
+			}
+			// Add the Notebook resource to the fake client
+			builder = builder.WithObjects(nb)
+
+			var objs []client.Object
+			// Convert each event item to a client.Object and append to objs slice
+			for _, e := range tt.events {
+				event := e // create a copy of event to avoid modifying the original
+				objs = append(objs, &event)
+			}
+			// Add events to the fake client
+			builder = builder.WithObjects(objs...)
+
+			r := &NotebookReconciler{
+				Client: builder.Build(),
+				Scheme: scheme,
+				Log:    ctrl.Log.WithName("controllers").WithName("Notebook"),
+			}
+			status := v1beta1.NotebookStatus{
+				Conditions:     make([]v1beta1.NotebookCondition, 0),
+				ReadyReplicas:  int32(0),
+				ContainerState: corev1.ContainerState{},
+			}
+			req := ctrl.Request{NamespacedName: types.NamespacedName{Namespace: "test-namespace", Name: "test-notebook"}}
+
+			// Call the checkWarningEvents function to test
+			status, err := checkWarningEvents(r, status, nb, req)
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+				return
+			}
+
+			// check if the status was updated with the correct condition
+			if diff := cmp.Diff(tt.expectedState, status); diff != "" {
+				t.Errorf("Unexpected status (-want +got):\n%s", diff)
+			}
+
+		})
+	}
 }
--- components/notebook-controller/go.mod
+++ components/notebook-controller/go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/go-logr/logr v1.2.0
+	github.com/google/go-cmp v0.5.5
 	github.com/kubeflow/kubeflow/components/common v0.0.0-20220218084159-4ad0158e955e
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
@@ -32,7 +33,6 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
