0a57535af3704a30c185786c6514681eb7b28d13
Tyler Kalbach <tyler.kalbach08@gmail.com>
2023-09-17 11:51:23 -0400

    Change log message to capacity

--- components/crud-web-apps/jupyter/backend/apps/common/routes/get.py
+++ components/crud-web-apps/jupyter/backend/apps/common/routes/get.py
@@ -117,7 +117,7 @@ def get_gpu_vendors():
         if node.status.capacity:
             installed_resources.update(status.capacity.keys())
         else:
-            log.warn("Status was not available for node {node.metadata.name}")
+            log.warn("Capacity was not available for node {node.metadata.name}")
 
     # Keep the vendors the key of which exists in at least one node
     available_vendors = installed_resources.intersection(config_vendor_keys)
