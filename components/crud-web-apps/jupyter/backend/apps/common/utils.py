f5207dcb779d1e4b1b4312a0736c586b5e9984b9
Kimonas Sotirchos <kimwnasptd@gmail.com>
2023-10-13 14:05:51 +0300

    Cherry-pick: Only load spawner_ui_config.yaml once every 10 minutes. closes #7259 (#7340)
    * Only load spawner_ui_config.yaml once every 10 minutes.
    
    * Update cache to 60 seconds
    
    Co-authored-by: boarder7395 <37314943+boarder7395@users.noreply.github.com>

--- components/crud-web-apps/jupyter/backend/apps/common/utils.py
+++ components/crud-web-apps/jupyter/backend/apps/common/utils.py
@@ -2,10 +2,10 @@ import os
 import random
 import string
 
+from cachetools.func import ttl_cache
+from kubeflow.kubeflow.crud_backend import helpers, logging
 from kubernetes import client
 from werkzeug import exceptions
-
-from kubeflow.kubeflow.crud_backend import helpers, logging
 
 from . import status
 
@@ -41,6 +41,7 @@ def load_notebook_template(**kwargs):
     return helpers.load_param_yaml(NOTEBOOK_TEMPLATE_YAML, **kwargs)
 
 
+@ttl_cache(ttl=60)
 def load_spawner_ui_config():
     for config in CONFIGS:
         config_dict = helpers.load_yaml(config)
@@ -90,13 +91,15 @@ def pvc_from_dict(vol, namespace):
         return None
 
     return client.V1PersistentVolumeClaim(
-        metadata=client.V1ObjectMeta(name=vol["name"], namespace=namespace,),
+        metadata=client.V1ObjectMeta(
+            name=vol["name"],
+            namespace=namespace,
+        ),
         spec=client.V1PersistentVolumeClaimSpec(
             access_modes=[vol["mode"]],
             storage_class_name=get_storage_class(vol),
             resources=client.V1ResourceRequirements(
-                requests={"storage": vol["size"]}
-            ),
+                requests={"storage": vol["size"]}),
         ),
     )
 
@@ -139,5 +142,5 @@ def notebook_dict_from_k8s_obj(notebook):
         "memory": cntr["resources"]["requests"]["memory"],
         "volumes": [v["name"] for v in cntr["volumeMounts"]],
         "status": status.process_status(notebook),
-        "metadata": notebook["metadata"]
+        "metadata": notebook["metadata"],
     }
