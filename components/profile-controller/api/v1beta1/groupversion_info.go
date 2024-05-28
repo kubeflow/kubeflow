428843ae96b186c1d71ecb35ae46e1b647aec8a6
Kimonas Sotirchos <kimwnasptd@gmail.com>
2024-05-28 06:59:42 +0000

    profiles: Add generated files from make test
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>

--- components/profile-controller/api/v1beta1/groupversion_info.go
+++ components/profile-controller/api/v1beta1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1beta1 contains API Schema definitions for the  v1beta1 API group
-//+kubebuilder:object:generate=true
-//+groupName=kubeflow.org
+// +kubebuilder:object:generate=true
+// +groupName=kubeflow.org
 package v1beta1
 
 import (
