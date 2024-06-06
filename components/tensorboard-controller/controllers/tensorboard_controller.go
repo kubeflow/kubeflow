fd4dcf2102157b8067eceb34fb56964707d48269
Denis Goupil <dgoupil@roblox.com>
2024-06-06 10:08:57 -0700

    Fix docker build
    Signed-off-by: Denis Goupil <dgoupil@roblox.com>

--- components/tensorboard-controller/controllers/tensorboard_controller.go
+++ components/tensorboard-controller/controllers/tensorboard_controller.go
@@ -24,7 +24,6 @@ import (
 	"strings"
 
 	"github.com/go-logr/logr"
-	"github.com/gogo/protobuf/proto"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
