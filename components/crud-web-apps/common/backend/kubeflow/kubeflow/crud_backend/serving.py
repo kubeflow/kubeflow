845e7cefef82545a29626c41c390a9d5c47c1799
lance-cl-lu <lance.cl.lu@gmail.com>
2024-09-03 23:50:23 +0800

    Modify serving.py

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/serving.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/serving.py
@@ -1,8 +1,8 @@
 import logging
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 
-from . import csrf, helpers
+from . import csrf, helpers, settings
 
 bp = Blueprint("serving", __name__)
 log = logging.getLogger(__name__)
@@ -14,6 +14,17 @@ log = logging.getLogger(__name__)
 # The JS/CSS files will have a hash in their name and thus we will want to
 # cache them for as long as possible
 
+def get_username():
+    if settings.USER_HEADER not in request.headers:
+        log.debug("User header not present!")
+        username = None
+    else:
+        user = request.headers[settings.USER_HEADER]
+        username = user.replace(settings.USER_PREFIX, "")
+        log.info("User: '%s' | Headers: '%s' '%s'",
+                  username, settings.USER_HEADER, settings.USER_PREFIX)
+
+    return username
 
 @bp.route("/index.html")
 @bp.route("/")
@@ -22,6 +33,9 @@ def serve_index(path="/"):
     # Serve the index file in all other cases
     log.info("Serving index.html for path: %s", path)
 
+    user = get_username()
+    log.info("Handling request for user: %s", user)
+    
     no_cache = "no-cache, no-store, must-revalidate, max-age=0"
     resp = Response(helpers.get_prefixed_index_html(), mimetype="text/html",
                     headers={"Cache-Control": no_cache})
