3ce5fdf3b23f4ecd5136333962a5225ca4dbb8b7
Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
2024-10-02 00:24:02 -0700

    feat: update notebook server images (#7647)
    * feat: update example notebook images
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Remove incorrect flow-chart png
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Update `kfp` python package to `2.9.0`
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Allow RStudio image to start outside Kubernetes
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Put note about why RStudio has WARN log-level
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Allow users to specify their own Reticulate python
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Fix installing packages on RStudio
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * Add `r-shiny` to base RStudio image
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>

--- components/example-notebook-servers/rstudio/rsession.sh
+++ components/example-notebook-servers/rstudio/rsession.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# use conda base environment with reticulate
+# activate "base" conda environment
 . /opt/conda/etc/profile.d/conda.sh
 conda activate base
-export RETICULATE_PYTHON=$CONDA_PREFIX/bin/python
+
+# set the default reticulate python to the one from the conda environment
+# https://rstudio.github.io/reticulate/articles/versions.html#order-of-discovery
+export RETICULATE_PYTHON_FALLBACK="${CONDA_PREFIX}/bin/python"
 
 /usr/lib/rstudio-server/bin/rsession "$@"
