9d563ce548334d2e06ce38e51958ed583b48e241
Tarek Abouzeid <tarek.abouzeid@teliacompany.com>
2024-10-22 19:19:59 +0200

    refactor logout
    Signed-off-by: Tarek Abouzeid <tarek.abouzeid@teliacompany.com>

--- components/centraldashboard/public/components/logout-button.js
+++ components/centraldashboard/public/components/logout-button.js
@@ -1,5 +1,6 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-button/paper-button.js';
+import css from './logout-button.css';
 
 /**
  * Logout button component.
@@ -7,65 +8,24 @@ import '@polymer/paper-button/paper-button.js';
  */
 export class LogoutButton extends PolymerElement {
     static get template() {
-        return html`
-            <a href="{{logoutUrl}}" on-click="logout">
+        return html([`
+            <style>${css.toString()}</style>
+            <a href$="{{logoutUrl}}" on-tap="logout">
                 <paper-button id="logout-button">
                     <iron-icon icon="kubeflow:logout" 
                                title="Logout">
                     </iron-icon>
                 </paper-button>
             </a>
-        `;
-    }
-
-    static get properties() {
-        return {
-            /**
-             * The URL to trigger the logout process.
-             */
-            logoutUrl: {
-                type: String,
-                value: '', // Default to avoid undefined URL errors.
-            },
-            /**
-             * The URL to redirect to after a successful logout.
-             */
-            afterLogoutURL: {
-                type: String,
-                value: '/', // Default redirect if none is provided.
-            },
-        };
+        `]);
+        ;
     }
 
     /**
-     * Handles logout by navigating to the logout URL.
-     * If the backend returns an 'afterLogoutURL', the user is redirected there.
-     * Otherwise, it defaults to the provided `afterLogoutURL`.
-     *
-     * @param {Event} event - The click event triggered by the logout link.
+     * Set current page to logoutURL.
      */
-    logout(event) {
-        event.preventDefault(); // Prevent default anchor behavior.
-
-        // Perform the logout request via a GET request.
-        fetch(this.logoutUrl, {
-            method: 'GET',
-            credentials: 'include',
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Logout failed');
-            })
-            .then((data) => {
-                const redirectUrl =
-                    data.afterLogoutURL || this.afterLogoutURL;
-                window.location.replace(redirectUrl);
-            })
-            .catch((error) => {
-                window.location.replace(this.afterLogoutURL);
-            });
+    logout() {
+        window.top.location.href = this.logoutUrl;
     }
 }
 
