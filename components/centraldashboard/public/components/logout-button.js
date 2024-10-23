7b7559a9c33d45eb615f0093fa264f121c676e0d
Raulian-Ionut Chiorescu <raulian-ionut.chiorescu@cern.ch>
2024-10-23 10:15:50 +0200

    feat(notebooks): add AMD ROCm example notebook images
    Signed-off-by: Raulian-Ionut Chiorescu <raulian-ionut.chiorescu@cern.ch>

--- components/centraldashboard/public/components/logout-button.js
+++ components/centraldashboard/public/components/logout-button.js
@@ -1,79 +1,31 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-
-import '@polymer/iron-ajax/iron-ajax.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-button/paper-button.js';
+import css from './logout-button.css';
 
 /**
  * Logout button component.
- * Handles the logout requests and post-logout redirects.
- *
+ * Triggers a GET request to the logout URL and handles post-logout redirection.
  */
-
 export class LogoutButton extends PolymerElement {
     static get template() {
-        return html`
-            <paper-button id="logout-button" on-tap="logout">
-                <iron-icon icon='kubeflow:logout' title="Logout">
-                </iron-icon>
-            </paper-button>
-            <iron-ajax
-                    id='logout'
-                    url$='{{logoutUrl}}'
-                    method='post'
-                    handle-as='json'
-                    headers='{{headers}}'
-                    on-response='_postLogout'>
-            </iron-ajax>
-        `;
-    }
-
-    static get properties() {
-        return {
-            headers: {
-                type: Object,
-                computed: '_setHeaders()',
-            },
-            logoutUrl: {
-                type: String,
-            },
-        };
+        return html([`
+            <style>${css.toString()}</style>
+            <a href$="{{logoutUrl}}" on-tap="logout">
+                <paper-button id="logout-button">
+                    <iron-icon icon="kubeflow:logout" 
+                               title="Logout">
+                    </iron-icon>
+                </paper-button>
+            </a>
+        `]);
+        ;
     }
 
     /**
-     * After successful logout, redirects user to `afterLogoutURL`,
-     * received from the backend.
-     *
-     * @param {{Event}} event
-     * @private
-     */
-    _postLogout(event) {
-        window.location.replace(event.detail.response['afterLogoutURL']);
-    }
-
-    /**
-     * Call logout endpoint.
+     * Set current page to logoutURL.
      */
     logout() {
-        // call iron-ajax
-        this.$.logout.generateRequest();
-    }
-
-    /**
-     * Set 'Authorization' header based on the existing cookie.
-     * Currently, the logout method only accepts authorization header, see:
-     * https://github.com/arrikto/oidc-authservice/blob/master/server.go#L386
-     *
-     * @return {{Object}} headers
-     * @private
-     */
-    _setHeaders() {
-        const cookie = ('; ' + document.cookie)
-            .split(`; authservice_session=`)
-            .pop()
-            .split(';')[0];
-        return {
-            'Authorization': `Bearer ${cookie}`,
-        };
+        window.top.location.href = this.logoutUrl;
     }
 }
 
