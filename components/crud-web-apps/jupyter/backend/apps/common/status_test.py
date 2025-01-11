272aef6134d359fc769b1d79510d5f7d192a1404
Roger Wang <ywang@roblox.com>
2025-01-10 19:04:20 -0800

    fix test and format
    Signed-off-by: Roger Wang <ywang@roblox.com>

--- components/crud-web-apps/jupyter/backend/apps/common/status_test.py
+++ components/crud-web-apps/jupyter/backend/apps/common/status_test.py
@@ -7,32 +7,18 @@ class TestStatusFromContainerState(unittest.TestCase):
     """Test the different cases of status from containerState"""
 
     def test_terminating_container_state(self):
-        container_state = {
-            "status": {
-                "containerState": {
-                    "terminating": {}
-                }
-            }
-        }
+        container_state = {"status": {"containerState": {"terminating": {}}}}
 
         self.assertEqual(
             status.get_status_from_container_state(container_state),
-            (None, None)
-        )
+            (None, None))
 
     def test_ready_container_state(self):
-        container_state = {
-            "status": {
-                "containerState": {
-                    "running": {}
-                }
-            }
-        }
+        container_state = {"status": {"containerState": {"running": {}}}}
 
         self.assertEqual(
             status.get_status_from_container_state(container_state),
-            (None, None)
-        )
+            (None, None))
 
     def test_no_message_container_state(self):
         container_state = {
@@ -48,54 +34,57 @@ class TestStatusFromContainerState(unittest.TestCase):
         self.assertEqual(
             status.get_status_from_container_state(container_state),
             ("warning",
-             "PodInitializing: No available message for container state.")
-        )
+             "PodInitializing: No available message for container state."))
+
 
 class TestStatusFromConditions(unittest.TestCase):
     """Test the different cases of status from conditions"""
 
     def test_no_conditions(self):
-        conditions = []
+        conditions = {}
 
-        self.assertEqual(
-            status.get_status_from_conditions(conditions),
-            (None, None)
-        )
+        self.assertEqual(status.get_status_from_conditions(conditions),
+                         (None, None))
 
     def test_no_reason_conditions(self):
-        conditions = [
-            {
-            "status": "True",
-            "type": "Initialized",
+        conditions = {
+            "status": {
+                "conditions": [{
+                    "status": "True",
+                    "type": "Initialized",
+                }]
             }
-        ]
+        }
 
-        self.assertEqual(
-            status.get_status_from_conditions(conditions),
-            (None, None)
-        )
+        self.assertEqual(status.get_status_from_conditions(conditions),
+                         (None, None))
 
     def test_conditions_with_reason_and_message(self):
         conditions = {
-            "status": "False",
-            "type": "Warning",
-            "reason": "FailedScheduling",
-            "message": "0/1 nodes are available."
+            "status": {
+                "conditions": [{
+                    "status": "False",
+                    "type": "Warning",
+                    "reason": "FailedScheduling",
+                    "message": "0/1 nodes are available."
+                }]
+            }
         }
 
         self.assertEqual(
             status.get_status_from_conditions(conditions),
-            ("warning", "FailedScheduling: 0/1 nodes are available.")
-        )
-    
+            ("warning", "FailedScheduling: 0/1 nodes are available."))
+
     def test_conditions_with_reason_no_message(self):
         conditions = {
-            "status": "False",
-            "type": "Ready",
-            "reason": "PodFailed",
+            "status": {
+                "conditions": [{
+                    "status": "False",
+                    "type": "Ready",
+                    "reason": "PodFailed",
+                }]
+            }
         }
 
-        self.assertEqual(
-            status.get_status_from_conditions(conditions),
-            ("warning", "PodFailed: No available message.")
-        )
+        self.assertEqual(status.get_status_from_conditions(conditions),
+                         ("warning", "PodFailed: No available message."))
